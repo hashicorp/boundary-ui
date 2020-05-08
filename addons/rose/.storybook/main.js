fdeca7012e6fdaf01113e7b287031175367402ee
Randall Morey <randall@randallmorey.com>
2020-05-08 16:28:15 -0400

    refactor: 💡 migrate typography to docs storybook

--- addons/rose/.storybook/main.js
+++ addons/rose/.storybook/main.js
@@ -1,5 +1,8 @@
 module.exports = {
-  stories: ['../addon/components/**/*.stories.(js|mdx)'],
+  stories: [
+    '../addon/components/**/*.stories.(js|mdx)',
+    '../stories/**/*.stories.mdx',
+  ],
   addons: [
     '@storybook/addon-knobs',
     '@storybook/addon-actions',
