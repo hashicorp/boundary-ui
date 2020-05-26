d7b044b2f76b53f94784e43026ec72c0a902703f
Randall Morey <randall@randallmorey.com>
2020-05-26 14:46:55 -0400

    refactor: 💡 simplify dropdown component and clean up styles

--- addons/rose/addon/components/rose/header/dropdown/content/button.js
+++
@@ -1,7 +0,0 @@
-import Component from '@ember/component';
-import layout from '../../../../../templates/components/rose/header/dropdown/content/button';
-
-export default Component.extend({
-  layout,
-  tagName: '',
-});
--- addons/rose/addon/modifiers/on-click-outside.js
+++ addons/rose/addon/modifiers/on-click-outside.js
@@ -1,5 +1,10 @@
 import { modifier } from 'ember-modifier';
 
+/**
+ * Used to detect clicks occuring _outside_ of the modified element.
+ * This is useful especially for dropdowns to toggle content visibility
+ * when the user clicks away.
+ */
 export default modifier((element, [callback]) => {
   function handleClick(event) {
     if (!element.contains(event.target)) {
