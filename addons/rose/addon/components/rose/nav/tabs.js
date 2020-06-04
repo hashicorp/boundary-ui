b03b31ff1add2ebec3ef79c131329397400aa415
Randall Morey <randall@randallmorey.com>
2020-06-04 15:33:24 -0400

    refactor: 💡 simplify tabs

--- addons/rose/addon/components/rose/nav/tabs.js
+++ addons/rose/addon/components/rose/nav/tabs.js
@@ -1,7 +1,6 @@
 import Component from '@ember/component';
-import layout from '../../../templates/components/rose/nav/tab';
+import layout from '../../../templates/components/rose/nav/tabs';
 
 export default Component.extend({
   layout,
-  tagName: '',
 });
