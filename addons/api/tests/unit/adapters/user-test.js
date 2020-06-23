d7967446d468a9622b947075fcffb4e84c3da9a6
Susmitha Girumala <susmitha@hashicorp.com>
2020-06-23 14:53:07 -0400

    Add user api adapter

---
+++ addons/api/tests/unit/adapters/user-test.js
@@ -0,0 +1,11 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Adapter | user', function(hooks) {
+  setupTest(hooks);
+
+  test('it exists', function(assert) {
+    let adapter = this.owner.lookup('adapter:user');
+    assert.ok(adapter);
+  });
+});
