e1c607d534c1affdc2a87e815105b20b67ed871d
Randall Morey <randall@randallmorey.com>
2020-06-26 15:06:09 -0400

    chore: add missing comments

--- ui/core/app/routes/index.js
+++ ui/core/app/routes/index.js
@@ -2,6 +2,11 @@ import Route from '@ember/routing/route';
 
 export default class IndexRoute extends Route {
 
+  // =methods
+
+  /**
+   * If arriving at index, redirect to authenticate for further processing.
+   */
   redirect() {
     this.transitionTo('orgs.authenticate');
   }
