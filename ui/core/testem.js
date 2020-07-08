998ce971447d0cc94c9c3f3555969deb032c16e3
Randall Morey <randall@randallmorey.com>
2020-07-08 16:35:54 -0400

    ci: 🎡 reduce test duration in CI

--- ui/core/testem.js
+++ ui/core/testem.js
@@ -1,7 +1,7 @@
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  launch_in_ci: ['Chrome', 'Firefox'],
+  launch_in_ci: ['Chrome'],
   launch_in_dev: ['Chrome', 'Firefox'],
   browser_start_timeout: 120,
   browser_args: {
