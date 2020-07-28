ee6c6626c990bbb235aa493dc79b3ff7350306f4
Randall Morey <randall@randallmorey.com>
2020-07-28 12:21:45 -0400

    chore: add new host catalog logic

--- ui/core/app/routes/scopes/scope/projects/project/host-catalogs/new.js
+++ ui/core/app/routes/scopes/scope/projects/project/host-catalogs/new.js
@@ -1,4 +1,16 @@
 import Route from '@ember/routing/route';
 
 export default class ScopesScopeProjectsProjectHostCatalogsNewRoute extends Route {
+
+  // =methods
+
+  /**
+   * Creates a new unsaved host catalog belonging to the current project scope.
+   * @return {HostCatalogModel}
+   */
+  model() {
+    const scopeID = this.modelFor('scopes.scope.projects.project').id;
+    return this.store.createRecord('host-catalog', { scopeID });
+  }
+
 }
