eb230d2f9bf2584791aa4fedea2bd8f81a028d2b
Susmitha Girumala <susmitha@hashicorp.com>
2020-08-11 18:29:50 -0400

    style: Format table updates

--- addons/rose/tests/integration/components/rose/table/section-test.js
+++ addons/rose/tests/integration/components/rose/table/section-test.js
@@ -3,7 +3,7 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | rose/table/section', function(hooks) {
+module('Integration | Component | rose/table/section', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
