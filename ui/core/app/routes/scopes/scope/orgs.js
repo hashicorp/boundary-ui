2a49d4601f23077354a4e751b841821ff94e87c0
Randall Morey <randall@randallmorey.com>
2020-08-17 12:25:07 -0400

    feat: 🎸 add new org functionality

--- ui/core/app/routes/scopes/scope/orgs.js
+++ ui/core/app/routes/scopes/scope/orgs.js
@@ -1,6 +1,13 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class ScopesScopeOrgsRoute extends Route {
+
+  // =services
+
+  @service intl;
+  @service notify;
 
   // =methods
 
@@ -10,6 +17,40 @@ export default class ScopesScopeOrgsRoute extends Route {
    */
   async model() {
     return this.store.query('scope', { scope_id: 'global' });
+  }
+
+  // =actions
+
+  /**
+   * Rollback changes on org.
+   * @param {ScopeModel} org
+   */
+  @action
+  cancel(org) {
+    const { isNew } = org;
+    org.rollbackAttributes();
+    if (isNew) this.transitionTo('scopes.scope.orgs');
+  }
+
+  /**
+   * Handle save org.
+   * @param {ScopeModel} org
+   * @param {Event} e
+   */
+  @action
+  async save(org) {
+    const { isNew } = org;
+    try {
+      await org.save();
+      this.refresh();
+      this.notify.success(
+        this.intl.t(isNew ? 'notify.create-success' : 'notify.save-success')
+      );
+      this.transitionTo('scopes.scope', org);
+    } catch (error) {
+      // TODO: replace with translated strings
+      this.notify.error(error.message, { closeAfter: null });
+    }
   }
 
 }
