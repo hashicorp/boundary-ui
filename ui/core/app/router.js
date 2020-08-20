4282a037836b24a5bde147143aaa47560359f70b
Susmitha Girumala <susmitha@hashicorp.com>
2020-08-20 13:13:11 -0400

    chore: Remove duplicated projects routes

--- ui/core/app/router.js
+++ ui/core/app/router.js
@@ -22,7 +22,12 @@ Router.map(function () {
         this.route('project', { path: ':project_id' }, function() {
           this.route('host-catalogs', function() {
             this.route('new');
-            this.route('host-catalog', { path: ':host_catalog_id' }, function() {});
+            this.route('host-catalog', { path: ':host_catalog_id' }, function() {
+              this.route('host-sets', function() {
+                this.route('host-set', { path: ':host_set_id' }, function() {});
+                this.route('new');
+              });
+            });
           });
         });
         this.route('new');
@@ -46,20 +51,6 @@ Router.map(function () {
         this.route('new');
       });
 
-      this.route('projects', function() {
-        this.route('project', { path: ':project_id' }, function() {
-          this.route('host-catalogs', function() {
-            this.route('new');
-            this.route('host-catalog', { path: ':host_catalog_id' }, function() {
-              this.route('host-sets', function() {
-                this.route('host-set', { path: ':host_set_id' }, function() {});
-                this.route('new');
-              });
-            });
-          });
-        });
-        this.route('new');
-      });
       this.route('auth-methods', function() {
         this.route('auth-method', { path: ':auth_method_id' }, function() {});
         this.route('new');
