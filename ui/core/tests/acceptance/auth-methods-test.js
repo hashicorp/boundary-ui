ea37ded4b3403eab609486b9dfe02075e17d31e6
Randall Morey <randall@randallmorey.com>
2020-08-20 15:07:59 -0400

    test: 💍 remove visit tests for defered acceptance suites

--- ui/core/tests/acceptance/auth-methods-test.js
+++ ui/core/tests/acceptance/auth-methods-test.js
@@ -1,55 +1,26 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+//import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
-import a11yAudit from 'ember-a11y-testing/test-support/audit';
-import {
-  authenticateSession,
-  // These are left here intentionally for future reference.
-  //currentSession,
-  //invalidateSession,
-} from 'ember-simple-auth/test-support';
+//import a11yAudit from 'ember-a11y-testing/test-support/audit';
+// import {
+//   authenticateSession,
+//   // These are left here intentionally for future reference.
+//   //currentSession,
+//   //invalidateSession,
+// } from 'ember-simple-auth/test-support';
 
 module('Acceptance | auth methods', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
 
-  const instances = {
-    scopes: {
-      global: null,
-      org: null
-    },
-    authMethod: null
-  };
-  const urls = {
-    orgScope: null,
-    authMethods: null
-  };
-
   hooks.beforeEach(function () {
     // Setup Mirage mock resources for this test
-    instances.scopes.global = this.server.create('scope', { id: 'global' });
-    instances.scopes.org = this.server.create('scope', { type: 'org',
-      scope: { id: instances.scopes.global.id, type: instances.scopes.global.type }
-    });
-    instances.authMethod = this.server.create('auth-method', {
-      scope: {
-        id: instances.scopes.org.id,
-        type: instances.scopes.org.type
-      }
-    });
-
     // Generate route URLs for resources
-    urls.orgScope = `/scopes/${instances.scopes.org.id}`;
-    urls.authMethods = `${urls.orgScope}/auth-methods`;
   });
 
   test('visiting auth methods', async function (assert) {
-    assert.expect(1);
-    authenticateSession();
-    await visit(urls.authMethods);
-    await a11yAudit();
-    assert.equal(currentURL(), urls.authMethods);
+    assert.expect(0);
   });
 
   test('can navigate to an auth method form', async function (assert) {
