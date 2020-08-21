786f8b884713f449a8f536429ee5f0eb740e21f4
Susmitha Girumala <susmitha@hashicorp.com>
2020-08-21 13:18:14 -0400

    chore: Add target breadcrumbs

---
+++ ui/core/app/controllers/scopes/scope/projects/project/targets.js
@@ -0,0 +1,20 @@
+import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+
+export default class ScopesScopeProjectsProjectTargetsController extends Controller {
+
+  // =services
+
+  @service intl;
+
+  // =attributes
+
+  /**
+   * Targets breadcrumb
+   * @type {string}
+   */
+  get breadCrumb() {
+    return this.intl.t('resources.targets');
+  }
+
+}
