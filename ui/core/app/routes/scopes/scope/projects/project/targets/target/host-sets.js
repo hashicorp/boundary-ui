63eabb8ba188719ac3ab78f6a78d91d1bfbadf0e
Susmitha Girumala <susmitha@hashicorp.com>
2020-08-26 16:43:04 -0400

    chore: Move target host-sets load to beforeModel

--- ui/core/app/routes/scopes/scope/projects/project/targets/target/host-sets.js
+++ ui/core/app/routes/scopes/scope/projects/project/targets/target/host-sets.js
@@ -5,22 +5,41 @@ export default class ScopesScopeProjectsProjectTargetsTargetHostSetsRoute extend
   // =methods
 
   /**
-   *
-   * Loads all host-sets under the current target and it's parent scope.
-   * @return {Promise{[HostSetModel]}}
+   * Loads all host-sets and associated host-catalogs under the current target
+   * and it's parent scope.
+   * @return {Promise{[HostSetModel, HostCatalogModel]}}
    */
-  async model() {
+  beforeModel() {
+    const hostSets = [], hostCatalogs = [];
     const target = this.modelFor('scopes.scope.projects.project.targets.target');
     const scopeID = this.modelFor('scopes.scope.projects.project').id;
 
-    const hostSets = target.host_sets.map((hostSet) =>
-      this.store.findRecord(
-        'host-set',
-        hostSet.host_set_id,
-        { adapterOptions : { scopeID, hostCatalogID: hostSet.host_catalog_id } }
-      )
-    );
+    target.host_sets.map((hostSet) => {
+      hostSets.push(
+        this.store.findRecord(
+          'host-set',
+          hostSet.host_set_id,
+          { adapterOptions : { scopeID, hostCatalogID: hostSet.host_catalog_id } }
+        )
+      );
 
-    return all(hostSets);
+      hostCatalogs.push(
+        this.store.findRecord(
+          'host-catalog',
+          hostSet.host_catalog_id,
+          { adapterOptions : { scopeID } }
+        )
+      );
+    });
+
+    return all(hostSets.concat(hostCatalogs));
+  }
+
+  /**
+   * Load current target and it's associated host-sets and host-catalogs
+   * @return {Promise{[TargetModel]}}
+   */
+  model() {
+    return this.modelFor('scopes.scope.projects.project.targets.target');
   }
 }
