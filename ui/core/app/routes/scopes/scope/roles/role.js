444a621bcfd0b6b4922976c207815fef263dd764
Randall Morey <randall@randallmorey.com>
2020-08-31 14:42:08 -0400

    refactor: 💡 role route should have multiple outlets

--- ui/core/app/routes/scopes/scope/roles/role.js
+++ ui/core/app/routes/scopes/scope/roles/role.js
@@ -9,10 +9,39 @@ export default class ScopesScopeRolesRoleRoute extends Route {
    * @param {string} params.role_id
    * @return {RoleModel}
    */
-  async model({ role_id }) {
+  model({ role_id }) {
     const { id: scopeID } = this.modelFor('scopes.scope');
     return this.store.findRecord('role', role_id, {
       adapterOptions: { scopeID },
+    });
+  }
+
+  /**
+   * Renders the route-specific templates for header, navigation,
+   * and actions page sections.
+   * @override
+   * @param {object} controller
+   * @param {object} model
+   */
+  renderTemplate(controller, model) {
+    super.renderTemplate(...arguments);
+
+    this.render('scopes/scope/roles/role/-header', {
+      into: 'scopes/scope/roles/role',
+      outlet: 'header',
+      model
+    });
+
+    this.render('scopes/scope/roles/role/-actions', {
+      into: 'scopes/scope/roles/role',
+      outlet: 'actions',
+      model
+    });
+
+    this.render('scopes/scope/roles/role/-navigation', {
+      into: 'scopes/scope/roles/role',
+      outlet: 'navigation',
+      model
     });
   }
 }
