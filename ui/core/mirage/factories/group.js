4ea8007c207dd5a4c3e4f43a7ea9832afa3bc303
Randall Morey <randall@randallmorey.com>
2020-09-01 12:03:36 -0400

    feat: 🎸 add mocks for group members

--- ui/core/mirage/factories/group.js
+++ ui/core/mirage/factories/group.js
@@ -1,5 +1,18 @@
 import factory from '../generated/factories/group';
+import { trait } from 'ember-cli-mirage';
 
 export default factory.extend({
-  id: (i) => `group-${i}`
+  id: (i) => `group-${i}`,
+
+  /**
+   * Generates some users for the same scope and assigns them to the group
+   * as members.
+   */
+  withMembers: trait({
+    afterCreate(group, server) {
+      const scope = group.scope;
+      const members = server.createList('user', 5, { scope });
+      group.update({ members });
+    }
+  })
 });
