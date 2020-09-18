4229658815795ed6b840a4e5c75159a598593be4
Randall Morey <randall@randallmorey.com>
2020-09-18 12:54:31 -0400

    feat: 🎸 add sessions list view

---
+++ ui/core/app/helpers/format-date-iso-human.js
@@ -0,0 +1,7 @@
+import { helper } from '@ember/component/helper';
+
+export default helper(function formatDateIsoHuman(params/*, hash*/) {
+  return params[0].toISOString()
+    .replace('T', ' ')
+    .replace(/\.\d*Z/, '');
+});
