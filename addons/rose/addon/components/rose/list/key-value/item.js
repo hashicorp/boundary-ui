8b772e06ab9bdf23b6fede3cfbf09f70463c9be2
Randall Morey <randall@randallmorey.com>
2020-09-24 16:54:27 -0400

    refactor: 💡 convert list components to nested structure

--- addons/rose/addon/components/rose/list/key-value/item.js
+++
@@ -1,8 +0,0 @@
-import Component from '@ember/component';
-import layout from '../../../../templates/components/rose/list/key-value/item';
-import ComponentAutoId from '../../../../mixins/component-auto-id';
-
-export default Component.extend(ComponentAutoId, {
-  layout,
-  tagName: '',
-});
---
+++ addons/rose/addon/components/rose/list/key-value/item/index.js
@@ -0,0 +1,10 @@
+import Component from '@glimmer/component';
+import { generateComponentID } from '../../../../../utilities/component-auto-id';
+
+export default class RoseListKeyValueItemComponent extends Component {
+
+  // =attributes
+
+  id = generateComponentID();
+
+}
