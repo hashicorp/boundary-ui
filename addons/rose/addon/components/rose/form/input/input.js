ed6a08ddd73e30b91531db984756b1eb4b2962fc
Randall Morey <randall@randallmorey.com>
2020-09-24 14:47:45 -0400

    refactor: 💡 convert form input to nested structure

--- addons/rose/addon/components/rose/form/input/input.js
+++
@@ -1,7 +0,0 @@
-import Component from '@ember/component';
-import layout from '../../../../templates/components/rose/form/input/input';
-
-export default Component.extend({
-  layout,
-  tagName: '',
-});
