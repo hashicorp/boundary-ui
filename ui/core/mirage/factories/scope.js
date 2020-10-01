5200f03417d561cf2a24e8caf42efab3833e4d14
Randall Morey <randall@randallmorey.com>
2020-10-01 16:31:39 -0400

    chore: generate more realistic scope IDs

--- ui/core/mirage/factories/scope.js
+++ ui/core/mirage/factories/scope.js
@@ -1,9 +1,15 @@
 import factory from '../generated/factories/scope';
 import { trait } from 'ember-cli-mirage';
+import { random } from 'faker';
 
 export default factory.extend({
 
   type: 'global',
+
+  id() {
+    const id = new Array(10).fill(0).map(() => random.alphaNumeric()).join('');
+    return `s_${id}`;
+  },
 
   withChildren: trait({
     afterCreate(record, server) {
