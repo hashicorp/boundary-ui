5254cfe228e5509412c567595a5ade50d8d2ba9c
Randall Morey <randall@randallmorey.com>
2020-10-03 12:43:12 -0400

    chore: remove branching

--- ui/core/app/controllers/scopes/scope/authenticate.js
+++ ui/core/app/controllers/scopes/scope/authenticate.js
@@ -1,19 +1,17 @@
 import Controller from '@ember/controller';
-import { sort } from '@ember/object/computed';
 
 export default class ScopesScopeAuthenticateController extends Controller {
   // =attributes
 
   /**
+   * Moves the global scope to index 0.
    * @type {Array}
    */
-  @sort('model.scopes', (a, b) => {
-    if (a.isGlobal) {
-      return -1;
-    } else if (b.isGlobal) {
-      return 1;
-    }
-    return 0;
-  })
-  sortedScopes;
+  get sortedScopes() {
+    return [
+      ...this.model.scopes.filter((scope) => scope.id === 'global'),
+      ...this.model.scopes.filter((scope) => scope.id !== 'global'),
+    ]
+  }
+
 }
