ee9bb281c1bf09255ca52e3414a80ee0540c29db
Randall Morey <randall@randallmorey.com>
2020-10-03 11:19:03 -0400

    chore: remove duplicate tests

--- ui/core/tests/acceptance/host-sets-test.js
+++ ui/core/tests/acceptance/host-sets-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit, currentURL, fillIn, click, find } from '@ember/test-helpers';
+import { visit, currentURL, fillIn, click, find, findAll } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import a11yAudit from 'ember-a11y-testing/test-support/audit';
@@ -32,6 +32,7 @@ module('Acceptance | host sets', function (hooks) {
     hostCatalog: null,
     hostSets: null,
     hostSet: null,
+    newHostSet: null
   };
 
   hooks.beforeEach(function () {
@@ -61,6 +62,7 @@ module('Acceptance | host sets', function (hooks) {
     urls.hostCatalog = `${urls.hostCatalogs}/${instances.hostCatalog.id}`;
     urls.hostSets = `${urls.hostCatalog}/host-sets`;
     urls.hostSet = `${urls.hostSets}/${instances.hostSet.id}`;
+    urls.newHostSet = `${urls.hostSets}/new`;
   });
 
   test('visiting a host set', async function (assert) {
@@ -147,5 +149,55 @@ module('Acceptance | host sets', function (hooks) {
     await click('.rose-layout-page-actions .rose-dropdown-button-danger');
     await a11yAudit();
     assert.ok(find('[role="alert"]'));
+  });
+
+  test('can create host set and save changes', async function (assert) {
+    assert.expect(1);
+    const hostSetsCount = this.server.db.hostSets.length;
+    await visit(urls.newHostSet);
+    await fillIn('[name="name"]', 'Test Name');
+    await fillIn('[name="description"]', 'description');
+    await click('form [type="submit"]:not(:disabled)');
+    await visit(urls.hostSets);
+    assert.equal(findAll('tbody tr').length, hostSetsCount + 1);
+  });
+
+  test('can create host set and cancel changes', async function (assert) {
+    assert.expect(2);
+    const hostSetsCount = this.server.db.hostSets.length;
+    await visit(urls.newHostSet);
+    await fillIn('[name="name"]', 'Test Name');
+    await click('form button:not([type="submit"])');
+    assert.equal(currentURL(), urls.hostSets);
+    assert.equal(findAll('tbody tr').length, hostSetsCount);
+  });
+
+  test('saving a new host set with invalid fields displays error messages', async function (assert) {
+    assert.expect(2);
+    this.server.post('/host-sets', () => {
+      return new Response(
+        400,
+        {},
+        {
+          status: 400,
+          code: 'invalid_argument',
+          message: 'The request was invalid.',
+          details: {
+            request_fields: [
+              {
+                name: 'name',
+                description: 'Name is required.',
+              },
+            ],
+          },
+        }
+      );
+    });
+    await visit(urls.newHostSet);
+    await fillIn('[name="name"]', 'new target');
+    await click('form [type="submit"]');
+    await a11yAudit();
+    assert.ok(find('[role="alert"]'));
+    assert.ok(find('.rose-form-error-message'));
   });
 });
