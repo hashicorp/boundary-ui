9e8b666204cb0cccfb4cce0c56c4091289bfb8f7
Susmitha Girumala <susmitha@hashicorp.com>
2020-11-05 17:05:48 -0500

    feat: Add local addons to desktop

--- ui/desktop/.eslintrc.js
+++ ui/desktop/.eslintrc.js
@@ -30,7 +30,6 @@ module.exports = {
         'ember-cli-build.js',
         'testem.js',
         'testem-electron.js',
-
         'blueprints/*/index.js',
         'config/**/*.js',
         'lib/*/index.js',
