66aeb805f00fc820d70b1aa909b766036efe4714
Randall Morey <randall@randallmorey.com>
2020-11-09 18:18:15 -0500

    test: 💍 add IPCRequest and service tests

--- ui/desktop/app/services/ipc.js
+++ ui/desktop/app/services/ipc.js
@@ -3,16 +3,33 @@ import { getOwner } from '@ember/application';
 import { Promise } from 'rsvp';
 
 /**
- * An IPC request is a promise-like object that invokes a method on the main
- * process and resolves to its response.
+ * An IPC request is a promise-like object that issues a request via
+ * `window.postMessage` and resolves to a response received via a response
+ * port.  IPC requests are used by an Electron renderer process to communicate
+ * with the main process when the renderer is fully isolated.
  *
- * Since the renderer cannot call into main or access Node modules directly,
- * it uses the window's `postMessage` interface.  Messages posted in this manner
- * are proxied by `preload.js` to the main process.  This ensures that main
- * and renderer remain isolated and frontend code does not rely on the presence
- * of a main process.
+ * Some of the design goals of this "safe IPC" are to respect the confines
+ * of process isolation and security, without using node integration, while
+ * retaining the ability to run the Ember application within a browser
+ * (sans main process) by using only APIs available in a standard
+ * (non-Electron) browser.  This is achieved by posting messages to the
+ * renderer's `window.postMessage` interface.  Thus, the renderer requires no
+ * knowledge of the main process or the Electron context.  In fact a main
+ * process and Electron context are not even required.
+ *
+ * In an Electron context, the `preload.js` script sets up a `window.postMessge`
+ * listener which acts as proxy layer between the main and renderer processes.
+ * It waits for incoming requests from the renderer and forwards them to main.
+ * Responses from the main process are forwarded back via a response port
+ * (sent with the original request).
+ *
+ * Local development in a non-Electron browser context or test context should
+ * mock up IPC requests.  See the ipc service test for an example of mocking.
+ *
+ * TODO: add utilities for quickly mocking IPC responses (like Mirage)
+ *
  */
-class IPCRequest {
+export class IPCRequest {
 
   // =attributes
 
@@ -122,10 +139,11 @@ export default class IpcService extends Service {
    * Creates a new IPC request and returns it.
    * @param {string} method
    * @param {object} payload
+   * @param {object} origin
    * @return {IPCRequest}
    */
-  invoke(method, payload) {
-    return new IPCRequest(method, payload, this.window);
+  invoke(method, payload, origin) {
+    return new IPCRequest(method, payload, this.window, origin);
   }
 
 }
