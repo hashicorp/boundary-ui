cc091a4cf6a5ecf5398e9be94019826f0f2e51e9
Randall Morey <randall@randallmorey.com>
2020-11-12 11:22:24 -0500

    fix: 🐛 postMessage origin should be the current origin

--- ui/desktop/config/environment.js
+++ ui/desktop/config/environment.js
@@ -31,12 +31,6 @@ module.exports = function (environment) {
       namespace: 'v1',
     },
 
-    ipc: {
-      defaultOrigin: process.env.EMBER_CLI_ELECTRON
-        ? 'serve://boundary'
-        : 'http://localhost:4200'
-    },
-
     appName: APP_NAME,
   };
 
