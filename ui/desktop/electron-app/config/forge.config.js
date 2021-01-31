86df6fc2c820053953f5f36f93330fea3cdcfbd9
Susmitha Girumala <susmitha@hashicorp.com>
2021-01-31 15:21:26 -0500

    chore: Download and extract boundary artifact into desktoppkg

--- ui/desktop/electron-app/config/forge.config.js
+++ ui/desktop/electron-app/config/forge.config.js
@@ -1,7 +1,44 @@
 const process = require('process');
+const fs = require('fs');
+const path = require('path');
+const https = require('https');
+const decompress = require('decompress');
+const os = require('os');
 
 module.exports = {
   hooks: {
+    prePackage: async () => {
+      const downloadArtifact = (url) => {
+        return new Promise((resolve, reject) => {
+          const tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), 'boundary-desktop-artifact-'))
+          console.log('\nCreate tmp artifact directory: ', tmpDir);
+
+          const artifactFileName = url.split('/').pop();
+          if(!artifactFileName) reject('Could not find artifact filename in: ', url);
+
+          const artifactPath = path.resolve(tmpDir, artifactFileName);
+          console.log('\nDownload artifact url: ', url);
+          https.get(url, (response) => {
+            const stream = response.pipe(fs.createWriteStream(artifactPath))
+            stream.on('close', () => resolve(artifactPath));
+            stream.on('error', reject);
+          });
+        });
+      }
+
+      // Download boundary
+      let artifactVersion = '0.1.4';
+      if (process.env.BOUNDARY_ARTIFACT_VERSION) artifactVersion = process.env.BOUNDARY_ARTIFACT_VERSION;
+
+      const url = `https://releases.hashicorp.com/boundary/${artifactVersion}/boundary_${artifactVersion}_darwin_amd64.zip`;
+      const artifactPath = await downloadArtifact(url);
+
+      // Extract artifact content
+      const binaryPath = path.resolve(__dirname, '..', 'ember-dist', 'binary');
+      if (!fs.existsSync(binaryPath)) fs.mkdirSync(binaryPath);
+      console.log('\nExtract artifact to: ', binaryPath);
+      await decompress(artifactPath, binaryPath);
+    },
     preMake: (forgeConfig) => {
       if (process.env.BOUNDARY_DESKTOP_SIGNING_IDENTITY) {
         forgeConfig.osxSign = {
