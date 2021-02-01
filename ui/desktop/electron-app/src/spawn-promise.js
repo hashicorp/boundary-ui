ab9e73df3ccd6371b97221a2b751ea54652850bc
Susmitha Girumala <susmitha@hashicorp.com>
2021-02-01 12:38:15 -0500

    chore: Remove cli path logging in electron-app

--- ui/desktop/electron-app/src/spawn-promise.js
+++ ui/desktop/electron-app/src/spawn-promise.js
@@ -28,8 +28,6 @@ const jsonify = (data) => {
 module.exports = function spawnPromise (command) {
   return new Promise((resolve, reject) => {
     const boundaryPath = path.resolve(__dirname, '..', 'cli', 'boundary');
-    // TODO:  remove path logging
-    console.log(boundaryPath);
     const childProcess = spawn(boundaryPath, command);
     let outputStream = '';
     let errorStream = '';
