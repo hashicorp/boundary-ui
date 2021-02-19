37168e8ea89d89993b3cfec6854e9b089030e1e9
Susmitha Girumala <susmitha@hashicorp.com>
2021-02-19 11:11:24 -0500

    test: Add connect tests for targets view

--- ui/desktop/tests/acceptance/scopes-test.js
+++ ui/desktop/tests/acceptance/scopes-test.js
@@ -5,7 +5,7 @@ import {
   //fillIn,
   click,
   find,
-  //findAll,
+  findAll,
   //getRootElement
   //setupOnerror,
 } from '@ember/test-helpers';
@@ -13,6 +13,7 @@ import { run, later } from '@ember/runloop';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import { Response } from 'miragejs';
+import { resolve, reject } from 'rsvp';
 import a11yAudit from 'ember-a11y-testing/test-support/audit';
 import sinon from 'sinon';
 import {
@@ -36,7 +37,9 @@ module('Acceptance | scopes', function (hooks) {
     },
     authMethods: {
       global: null,
-    }
+    },
+    target: null,
+    session: null,
   };
 
   const stubs = {
@@ -61,6 +64,8 @@ module('Acceptance | scopes', function (hooks) {
     globalProjects: null,
     targets: null,
     globalTargets: null,
+    target: null,
+    targetSessions: null,
   };
 
   const setDefaultOrigin = (test) => {
@@ -94,6 +99,26 @@ module('Acceptance | scopes', function (hooks) {
       scope: instances.scopes.global,
     });
 
+    instances.hostCatalog = this.server.create(
+      'host-catalog',
+      { scope: instances.scopes.project },
+      'withChildren'
+    );
+    instances.target = this.server.create(
+      'target',
+      { scope: instances.scopes.project },
+      'withRandomHostSets'
+    );
+
+    instances.session = this.server.create(
+      'session',
+      {
+        scope: instances.scopes.project,
+        status: 'active',
+      },
+      'withAssociations'
+    );
+
     urls.scopes.global = `/scopes/${instances.scopes.global.id}`;
     urls.scopes.org = `/scopes/${instances.scopes.org.id}`;
     urls.authenticate.global = `${urls.scopes.global}/authenticate`;
@@ -102,6 +127,8 @@ module('Acceptance | scopes', function (hooks) {
     urls.projects = `${urls.scopes.org}/projects`;
     urls.globalTargets = `${urls.globalProjects}/targets`;
     urls.targets = `${urls.projects}/targets`;
+    urls.target = `${urls.targets}/${instances.target.id}`;
+    urls.targetSessions = `${urls.target}/sessions`;
 
     class MockIPC {
       origin = null;
@@ -118,6 +145,10 @@ module('Acceptance | scopes', function (hooks) {
         this.origin = origin;
         return this.origin;
       }
+
+      cliExists(payload) {}
+
+      connect(payload) {}
     }
 
     mockIPC = new MockIPC();
@@ -186,6 +217,94 @@ module('Acceptance | scopes', function (hooks) {
       assert.equal(currentURL(), urls.targets);
       await click('.rose-header-nav .rose-dropdown a:nth-of-type(1)');
       assert.equal(currentURL(), urls.globalTargets);
+    }, 750);
+    await visit(urls.targets);
+  });
+
+  test('visiting index while unauthenticated redirects to global authenticate method', async function (assert) {
+    invalidateSession();
+    assert.expect(2);
+    await visit(urls.targets);
+    await a11yAudit();
+    assert.notOk(currentSession().isAuthenticated);
+    assert.equal(currentURL(), urls.authenticate.methods.global);
+  });
+
+  test('visiting index', async function (assert) {
+    assert.expect(2);
+    const targetsCount = this.server.schema.targets.all().models.length;
+    // This later/cancelTimers technique allows us to test a page with
+    // active polling.  Normally an acceptance test waits for all runloop timers
+    // to stop before returning from an awaited test, but polling means that
+    // runloop timers exist indefinitely.  We thus schedule a cancelation before
+    // proceeding with our tests.
+    later(async () => {
+      run.cancelTimers();
+      await a11yAudit();
+      assert.equal(currentURL(), urls.targets);
+      assert.equal(findAll('tbody tr').length, targetsCount);
+    }, 750);
+    await visit(urls.targets);
+  });
+
+  test('visiting a target', async function (assert) {
+    assert.expect(1);
+    later(async() => {
+      run.cancelTimers();
+      await click('tbody tr th a');
+      assert.equal(currentURL(), urls.targetSessions);
+    }, 750);
+    await visit(urls.targets);
+  });
+
+  test('can display message for empty targets', async function (assert) {
+    assert.expect(1);
+    this.server.get('/targets', () => {
+      return new Response(200);
+    });
+    later(async() => {
+      run.cancelTimers();
+      assert.ok(find('.rose-message-title').textContent.trim(), 'No Targets Available');
+    }, 750);
+    await visit(urls.targets);
+  });
+
+  test('can connect to a target', async function (assert) {
+    assert.expect(3);
+    sinon.stub(mockIPC, 'cliExists').returns(true);
+    sinon.stub(mockIPC, 'connect').returns({
+      session_id: instances.session.id,
+      address: 'a_123',
+      port: 'p_123',
+      protocol: 'tcp',
+    });
+    const confirmService = this.owner.lookup('service:confirm');
+    confirmService.enabled = true;
+
+    later(async() => {
+      run.cancelTimers();
+      await click('tbody tr:first-child td:last-child button', 'Activate connect mode');
+      assert.ok(find('.rose-dialog-success'), 'Success dialog');
+      assert.equal(findAll('.rose-dialog-footer button').length, 1);
+      assert.equal(find('.rose-dialog-footer button').textContent.trim(), 'OK', 'Cannot retry');
+    }, 750);
+    await visit(urls.targets);
+  });
+
+  test('handles cli error on connect', async function (assert) {
+    assert.expect(4);
+    sinon.stub(mockIPC, 'cliExists').returns(false);
+    const confirmService = this.owner.lookup('service:confirm');
+    confirmService.enabled = true;
+
+    later(async() => {
+      run.cancelTimers();
+      await click('tbody tr:first-child td:last-child button', 'Activate connect mode');
+      assert.ok(find('.rose-dialog-error'), 'Error dialog');
+      const dialogButtons = findAll('.rose-dialog-footer button');
+      assert.equal(dialogButtons.length, 2);
+      assert.equal(dialogButtons[0].textContent.trim(), 'Retry', 'Can retry');
+      assert.equal(dialogButtons[1].textContent.trim(), 'Cancel', 'Can cancel');
     }, 750);
     await visit(urls.scopes.global);
   });
