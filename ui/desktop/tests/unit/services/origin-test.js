0d8508cd4e73cfd3fce2a219a6d8ff8490bf6307
Randall Morey <randall@randallmorey.com>
2021-04-08 13:45:19 -0400

    fix: 🐛 silently drop trailing slashes from origin input

--- ui/desktop/tests/unit/services/origin-test.js
+++ ui/desktop/tests/unit/services/origin-test.js
@@ -24,4 +24,15 @@ module('Unit | Service | origin', function (hooks) {
       assert.equal(service.adapter.host, window.location.origin);
     });
   });
+
+  test('drops trailing slashes from origin on setOrigin', async function (assert) {
+    assert.expect(4);
+    sinon.stub(ipcService, 'invoke');
+    await service.setOrigin(`${window.location.origin}/`);
+    assert.equal(service.rendererOrigin, window.location.origin);
+    assert.equal(service.adapter.host, window.location.origin);
+    await service.setOrigin(`${window.location.origin}//////`);
+    assert.equal(service.rendererOrigin, window.location.origin);
+    assert.equal(service.adapter.host, window.location.origin);
+  });
 });
