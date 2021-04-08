f3bd8d0b6f88d45e341b34b3b340c4025e43b2e4
Randall Morey <randall@randallmorey.com>
2021-04-08 11:26:29 -0400

    test: add missing frame tests

--- addons/rose/tests/integration/components/rose/frame-test.js
+++ addons/rose/tests/integration/components/rose/frame-test.js
@@ -1,26 +1,20 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, find } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | rose/frame', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`<Rose::Frame />`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
     await render(hbs`
       <Rose::Frame>
-        <:body>template block text</:body>
+        <:header>Header</:header>
+        <:body>Body</:body>
       </Rose::Frame>
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.ok(find('section > header'));
+    assert.ok(find('section > div'));
   });
 });
