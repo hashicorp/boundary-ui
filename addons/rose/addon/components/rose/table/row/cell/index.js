cf40b53fd05042a456ac623fded54d1d175df792
Susmitha Girumala <susmitha@hashicorp.com>
2021-05-06 10:14:11 -0400

    chore: Format newly added files in rose

--- addons/rose/addon/components/rose/table/row/cell/index.js
+++ addons/rose/addon/components/rose/table/row/cell/index.js
@@ -4,12 +4,11 @@ import { computed } from '@ember/object';
 
 export default Component.extend({
   layout,
-  classNameBindings: [
-    'cellStyle',
-    'cellSizeStyle',
-  ],
+  classNameBindings: ['cellStyle', 'cellSizeStyle'],
   cellStyle: computed(function () {
-    return this.tagName.match('th') ? `rose-table-header-cell` : 'rose-table-cell';
+    return this.tagName.match('th')
+      ? `rose-table-header-cell`
+      : 'rose-table-cell';
   }),
   cellSizeStyle: computed(function () {
     return this.style ? `rose-table-cell-${this.style}` : '';
