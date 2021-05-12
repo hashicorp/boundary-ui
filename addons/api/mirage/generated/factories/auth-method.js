e91612b3136116890057dcd600b31660330552d6
Randall Morey <randall@randallmorey.com>
2021-05-12 17:12:59 -0400

    feat: 🎸 implement basic OIDC auth method attributes modeling

--- addons/api/mirage/generated/factories/auth-method.js
+++ addons/api/mirage/generated/factories/auth-method.js
@@ -11,4 +11,22 @@ export default Factory.extend({
   created_time: () => date.recent(),
   updated_time: () => date.recent(),
   disabled: () => datatype.boolean(),
+
+  attributes: function () {
+    let attrs;
+    switch (this.type) {
+      case 'oidc':
+        attrs = {
+          api_url_prefix: 'http://127.0.0.1:9200',
+          callback_url:
+            'http://127.0.0.1:9200/v1/auth-methods/oidc:authenticate:callback',
+          client_id: 'YTwdJBB55LpKnuXb9n65',
+          client_secret_hmac: 'GY7blb4ynM5TEPNi_qnphJ9aDWhW8FpmN3D-eP_L7Zk',
+          issuer: 'http://127.0.0.1:57070',
+          state: 'active-public',
+        };
+        break;
+    }
+    return attrs;
+  },
 });
