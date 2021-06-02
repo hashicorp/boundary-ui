269c56bf83591a29f2f46797d46388321619da3f
Randall Morey <randall@randallmorey.com>
2021-06-02 14:33:31 -0400

    fix: lint issue

--- ui/admin/tests/acceptance/auth-methods/oidc-test.js
+++ ui/admin/tests/acceptance/auth-methods/oidc-test.js
@@ -10,7 +10,7 @@ import {
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import a11yAudit from 'ember-a11y-testing/test-support/audit';
-import { Response } from 'miragejs';
+//import { Response } from 'miragejs';
 import {
   authenticateSession,
   // These are left here intentionally for future reference.
