3b4bb24eb7d13efb811829ceb7daa957518a0eed
Carlos Alcaide <me@calcaide.com>
2021-07-15 09:36:32 -0700

    feat: 🎸 Add credential store
    Add screen to create new credential store

---
+++ ui/admin/app/routes/scopes/scope/credential-stores/new.js
@@ -0,0 +1,14 @@
+import Route from '@ember/routing/route';
+
+export default class ScopesScopeCredentialStoresNewRoute extends Route {
+  // =methods
+  /**
+   * Creates a new unsaved credential-store
+   */
+  model() {
+    const scopeModel = this.modelFor('scopes.scope');
+    return this.store.createRecord('credential-store', {
+      scopeModel,
+    });
+  }
+}
