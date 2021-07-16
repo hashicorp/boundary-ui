9c93ff6c131d531c640da3e6f02e2ba28833841d
Carlos Alcaide <me@calcaide.com>
2021-07-16 13:55:33 -0700

    test: 💍 Add test cases to credential stores route
    Add new test cases to credential stores route

--- ui/admin/tests/acceptance/credential-stores-test.js
+++ ui/admin/tests/acceptance/credential-stores-test.js
@@ -1,12 +1,15 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { visit, currentURL, click, fillIn } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import a11yAudit from 'ember-a11y-testing/test-support/audit';
+import { authenticateSession } from 'ember-simple-auth/test-support';
 
 module('Acceptance | credential-stores', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
+
+  let getCredentialStoresCount;
 
   const instances = {
     scopes: {
@@ -32,17 +35,40 @@ module('Acceptance | credential-stores', function (hooks) {
       type: 'project',
       scope: { id: instances.scopes.org.id, type: 'org' },
     });
+    instances.credentialStore = this.server.create('credential-store', {
+      scope: instances.scopes.project,
+    });
     // Generate route URLs for resources
     urls.globalScope = `/scopes/global/scopes`;
     urls.orgScope = `/scopes/${instances.scopes.org.id}/scopes`;
     urls.projectScope = `/scopes/${instances.scopes.project.id}`;
     urls.credentialStores = `${urls.projectScope}/credential-stores`;
+    urls.credentialStore = `${urls.credentialStores}/${instances.credentialStore.id}`;
+    urls.unknownCredentialStore = `${urls.credentialStores}/foo`;
+    urls.newCredentialStore = `${urls.credentialStores}/new`;
+    // Generate resource counter
+    getCredentialStoresCount = () => {
+      return this.server.schema.credentialStores.all().models.length;
+    };
+    authenticateSession({});
   });
 
   test('visiting /credential-stores', async function (assert) {
-    assert.expect(1);
+    assert.expect(2);
     await visit(urls.credentialStores);
     await a11yAudit();
     assert.equal(currentURL(), urls.credentialStores);
+    await visit(urls.credentialStore);
+    await a11yAudit();
+    assert.equal(currentURL(), urls.credentialStore);
+  });
+
+  test('can create a new credential store', async function (assert) {
+    assert.expect(1);
+    const count = getCredentialStoresCount();
+    await visit(urls.newCredentialStore);
+    await fillIn('[name="name"]', 'random string');
+    await click('[type="submit"]');
+    assert.equal(getCredentialStoresCount(), count + 1);
   });
 });
