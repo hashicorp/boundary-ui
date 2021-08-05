74df9237753a61afddbf1a7eb03ae9c4d6415386
Randall Morey <randall@randallmorey.com>
2021-08-05 13:29:33 -0400

    feat: 🎸 apply base singular capabilities to account

--- ui/admin/tests/acceptance/accounts/update-test.js
+++ ui/admin/tests/acceptance/accounts/update-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit, currentURL, click, find, fillIn } from '@ember/test-helpers';
+import { visit, click, find, fillIn } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import a11yAudit from 'ember-a11y-testing/test-support/audit';
@@ -11,7 +11,7 @@ import {
   //invalidateSession,
 } from 'ember-simple-auth/test-support';
 
-module('Acceptance | accounts', function (hooks) {
+module('Acceptance | accounts | update', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
 
@@ -27,7 +27,6 @@ module('Acceptance | accounts', function (hooks) {
     orgScope: null,
     authMethods: null,
     accounts: null,
-    newAccount: null,
     account: null,
   };
 
@@ -50,7 +49,6 @@ module('Acceptance | accounts', function (hooks) {
     urls.authMethods = `${urls.orgScope}/auth-methods`;
     urls.authMethod = `${urls.authMethods}/${instances.authMethod.id}`;
     urls.accounts = `${urls.authMethod}/accounts`;
-    urls.newAccount = `${urls.accounts}/new`;
     urls.account = `${urls.accounts}/${instances.account.id}`;
   });
 
@@ -61,38 +59,52 @@ module('Acceptance | accounts', function (hooks) {
     }
   });
 
-  test('visiting accounts', async function (assert) {
+  test('can update an account and save changes', async function (assert) {
     assert.expect(1);
-    await visit(urls.accounts);
-    await a11yAudit();
-    assert.equal(currentURL(), urls.accounts);
-  });
-
-  test('can create a new account', async function (assert) {
-    assert.expect(1);
-    const accountsCount = this.server.db.accounts.length;
-    await visit(urls.newAccount);
-    await fillIn('[name="name"]', 'Account name');
-    await fillIn('[name="description"]', 'description');
-    await fillIn('[name="login_name"]', 'username');
-    await fillIn('[name="password"]', 'password');
+    await visit(urls.account);
+    await click('form [type="button"]', 'Activate edit mode');
+    await fillIn('[name="name"]', 'update name');
     await click('form [type="submit"]:not(:disabled)');
-    assert.equal(this.server.db.accounts.length, accountsCount + 1);
+    assert.equal(this.server.db.accounts[0].name, 'update name');
   });
 
-  test('can cancel a new account creation', async function (assert) {
-    assert.expect(2);
-    const accountsCount = this.server.db.accounts.length;
-    await visit(urls.newAccount);
-    await fillIn('[name="name"]', 'Account name');
+  test('can update an account and cancel changes', async function (assert) {
+    assert.expect(1);
+    await visit(urls.account);
+    await click('form [type="button"]', 'Activate edit mode');
+    await fillIn('[name="name"]', 'update name');
     await click('form button:not([type="submit"])');
-    assert.equal(this.server.db.accounts.length, accountsCount);
-    assert.equal(currentURL(), urls.accounts);
+    assert.notEqual(this.server.db.accounts[0].name, 'update name');
   });
 
-  test('saving a new account with invalid fields displays error messages', async function (assert) {
+  test('errors are displayed when save on account fails', async function (assert) {
+    assert.expect(1);
+    this.server.patch('/accounts/:id', () => {
+      return new Response(
+        490,
+        {},
+        {
+          status: 490,
+          code: 'error',
+          message: 'Oops.',
+        }
+      );
+    });
+    await visit(urls.account);
+    await click('form [type="button"]', 'Activate edit mode');
+    await fillIn('[name="name"]', 'save account');
+    await click('form [type="submit"]');
+    await a11yAudit();
+    assert.ok(
+      find('[role="alert"]').textContent.trim(),
+      'Oops.',
+      'Displays primary error message.'
+    );
+  });
+
+  test('saving an existing account with invalid fields displays error messages', async function (assert) {
     assert.expect(2);
-    this.server.post('/accounts', () => {
+    this.server.patch('/accounts/:id', () => {
       return new Response(
         400,
         {},
@@ -111,8 +123,9 @@ module('Acceptance | accounts', function (hooks) {
         }
       );
     });
-    await visit(urls.newAccount);
-    await fillIn('[name="name"]', 'new account');
+    await visit(urls.account);
+    await click('form [type="button"]', 'Activate edit mode');
+    await fillIn('[name="name"]', 'existing account');
     await click('form [type="submit"]');
     await a11yAudit();
     assert.ok(
