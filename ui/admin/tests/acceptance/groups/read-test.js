2cca6fe005af011a7e34dd042a14c21c6c005fa5
Dharini Jeeva <dharini@hashicorp.com>
2021-08-16 17:06:45 -0700

    Update read-test.js

--- ui/admin/tests/acceptance/groups/read-test.js
+++ ui/admin/tests/acceptance/groups/read-test.js
@@ -10,7 +10,7 @@ import {
   //invalidateSession,
 } from 'ember-simple-auth/test-support';
 
-module('Acceptance | groups', function (hooks) {
+module('Acceptance | groups | read', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
 
