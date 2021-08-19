4659b54e9e33905dbdb9d3e6c988767585e534c6
Randall Morey <randall@randallmorey.com>
2021-08-19 17:52:27 -0400

    feat: 🎸 add ui-level navigation ability

---
+++ ui/desktop/app/abilities/collection.js
@@ -0,0 +1,30 @@
+import CollectionAbility from 'api/abilities/collection';
+import { inject as service } from '@ember/service';
+
+export default class OverrideModelAbility extends CollectionAbility {
+  // =services
+
+  @service features;
+
+  // =permissions
+
+  /**
+   * Navigating to a resource is allowed if either list or create grants
+   * are present.
+   * @type {boolean}
+   */
+  get canNavigate() {
+    return this.canList || this.canCreate;
+  }
+
+  // =methods
+
+  /**
+   * If the capabilities feature flag is disabled, actions are always
+   * authorized.  This mimics pre-capabilities behavior.
+   */
+  hasAuthorizedCollectionAction() {
+    if (!this.features.isEnabled('capabilities')) return true;
+    return super.hasAuthorizedCollectionAction(...arguments);
+  }
+}
