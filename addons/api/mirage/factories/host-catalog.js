7c377506c8296a63054ecd4aa0b899eea404845d
dharini <dharini@hashicorp.com>
2021-08-19 16:06:47 -0700

    feat: 🎸 host catalog capabilities

--- addons/api/mirage/factories/host-catalog.js
+++ addons/api/mirage/factories/host-catalog.js
@@ -1,7 +1,15 @@
 import factory from '../generated/factories/host-catalog';
 import { trait } from 'ember-cli-mirage';
+import permissions from '../helpers/permissions';
 
 export default factory.extend({
+  authorized_actions: () =>
+    permissions.authorizedActionsFor('host-catalog') || [
+      'no-op',
+      'read',
+      'update',
+      'delete',
+    ],
   withChildren: trait({
     afterCreate(hostCatalog, server) {
       const { scope } = hostCatalog;
