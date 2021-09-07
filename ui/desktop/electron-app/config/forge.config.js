fe9ce25ed06796ea21c2895e5f1925fdabf9ac27
Susmitha Girumala <susmitha@hashicorp.com>
2021-09-07 18:33:13 -0400

    fix: Add linux executable name for linux binary name

--- ui/desktop/electron-app/config/forge.config.js
+++ ui/desktop/electron-app/config/forge.config.js
@@ -26,6 +26,7 @@ module.exports = {
     appVersion: config.releaseVersion,
     appBundleId: 'com.electron.boundary',
     appCopyright: config.copyright,
+    executableName: 'boundary-desktop',
     osxSign: {
       identity: process.env.BOUNDARY_DESKTOP_SIGNING_IDENTITY,
       'hardened-runtime': true,
