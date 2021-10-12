632e5cb5be3ff7e2545404b2c21f10bfddc0a9f3
dharini <dharini@hashicorp.com>
2021-10-12 11:55:45 -0700

    checkbox group updates

---
+++ addons/rose/tests/integration/components/rose/dropdown/checkbox-test.js
@@ -0,0 +1,24 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, find } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | rose/dropdown/link', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+    await render(hbs`<Rose::Dropdown::Checkbox />`);
+    assert.ok(find('input'));
+    assert.ok(find('.rose-dropdown-button'));
+  });
+
+  test('it is disabled when @disabled={{true}}', async function (assert) {
+    await render(hbs`<Rose::Dropdown::Checkbox @disabled={{true}} />`);
+    assert.equal(find('button').disabled, true);
+  });
+
+  test('it is of type="checkbox"', async function (assert) {
+    await render(hbs`<Rose::Dropdown::Checkbox />`);
+    assert.equal(find('checkbox').type, 'checkbox');
+  });
+});
