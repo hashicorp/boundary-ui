6bcbf2b2ba3fb35380990adefa58d2b64a1e7097
Carlos Alcaide Corvo <ccorvo@hashicorp.com>
2021-10-15 13:40:14 -0700

    Simplify conditionals with a switch

--- ui/desktop/tests/end2end/test-helpers.js
+++ ui/desktop/tests/end2end/test-helpers.js
@@ -25,21 +25,18 @@ exports.generateScreenshotPath = (screenshotTestDirectory, fileName) => {
  */
 exports.returnExecutablePath = (platform, arch) => {
   try {
-    // Just mac
-    if (platform === 'darwin') {
-      if (arch === 'x64') {
-        // Intel chips
-        return 'electron-app/out/Boundary-darwin-x64/Boundary.app/Contents/MacOS/Boundary';
-      } else if (arch === 'arm64') {
-        // M1 chips
-        return 'electron-app/out/Boundary-darwin-arm64/Boundary.app/Contents/MacOS/Boundary';
-      } else {
-        throw new Error('The test suite is not compatible with your arch.');
-      }
-    } else {
-      throw new Error(
-        'The test suite is not compatible with your Platform or Architecture.'
-      );
+    switch (platform) {
+      case 'darwin':
+        switch (arch) {
+          case 'x64': // Intel chips
+            return 'electron-app/out/Boundary-darwin-x64/Boundary.app/Contents/MacOS/Boundary';
+          case 'arm64': // M1 chips
+            return 'electron-app/out/Boundary-darwin-arm64/Boundary.app/Contents/MacOS/Boundary';
+          default:
+            throw new Error('The test suite is not compatible with your arch.');
+        }
+      default:
+        throw new Error('The test suite is not compatible with your platform.');
     }
   } catch (error) {
     console.error(error);
