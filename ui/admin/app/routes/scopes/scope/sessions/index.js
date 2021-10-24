093019ba33e3e694a5c12ede77f3a7da61ad1d09
dharini <dharini@hashicorp.com>
2021-10-23 22:18:20 -0700

    move store logic to route

--- ui/admin/app/routes/scopes/scope/sessions/index.js
+++ ui/admin/app/routes/scopes/scope/sessions/index.js
@@ -1,3 +1,9 @@
 import Route from '@ember/routing/route';
 
-export default class ScopesScopeSessionsIndexRoute extends Route {}
+export default class ScopesScopeSessionsIndexRoute extends Route {
+setupController(controller) {
+    const availableSessions = this.store.peekAll('session');
+    super.setupController(...arguments);
+    controller.setProperties({ availableSessions });
+    }
+}
