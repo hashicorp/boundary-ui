bc46fe5d688c9b10e9daf6cccd09aced8fcb4ef3
dharini <dharini@hashicorp.com>
2021-10-31 01:21:22 -0700

    updates

--- ui/admin/app/routes/scopes/scope/sessions/index.js
+++ ui/admin/app/routes/scopes/scope/sessions/index.js
@@ -2,8 +2,6 @@ import Route from '@ember/routing/route';
 export default class ScopesScopeSessionsIndexRoute extends Route {
   setupController(controller) {
     const availableSessions = this.store.peekAll('session');
-    console.log(availableSessions, 'avalalallal');
-
     super.setupController(...arguments);
     controller.setProperties({ availableSessions });
   }
