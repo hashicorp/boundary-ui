0f95ee730393fbb6f8ebc36693841c736756eb02
dharini <dharini@hashicorp.com>
2021-11-04 10:23:55 -0700

    feat: 🎸 updates to checkbox

--- ui/admin/app/controllers/scopes/scope/sessions/index.js
+++
@@ -1,9 +0,0 @@
-import Controller from '@ember/controller';
-import { inject as service } from '@ember/service';
-import { tracked } from '@glimmer/tracking';
-export default class ScopesScopeSessionsIndexController extends Controller {
-  // =services
-
-  @service intl;
-  @tracked selectedItems;
-}
--- ui/admin/app/routes/scopes/scope/sessions/index.js
+++ ui/admin/app/routes/scopes/scope/sessions/index.js
@@ -1,8 +1,19 @@
 import Route from '@ember/routing/route';
+import { setupFilters } from '../../../utils/setupFilters';
+
 export default class ScopesScopeSessionsIndexRoute extends Route {
   setupController(controller) {
-    const availableSessions = this.store.peekAll('session');
     super.setupController(...arguments);
-    controller.setProperties({ availableSessions });
+    const scopeModel = this.modelFor('scopes.scope');
+    const route = 'scopes.scope.sessions';
+    controller.setProperties({
+      scopeModel,
+      filters: {
+        status: {
+          items: ['active', 'pending', 'terminated', 'cancelling'],
+          selectedItems: setupFilters(this, route, 'status'),
+        },
+      },
+    });
   }
 }
