5911c6f0d9e5dd9674daf9c1d4f1003703498ac8
Randall Morey <randall@randallmorey.com>
2021-11-08 14:29:08 -0500

    remove unused setupcontroller code

--- ui/admin/app/routes/scopes/scope/auth-methods/index.js
+++ ui/admin/app/routes/scopes/scope/auth-methods/index.js
@@ -10,22 +10,6 @@ export default class ScopesScopeAuthMethodsIndexRoute extends Route {
   setupController(controller) {
     super.setupController(...arguments);
     const scopeModel = this.modelFor('scopes.scope');
-    const type = this.controllerFor('scopes.scope.auth-methods')['filter-type'];
-    const is_primary = this.controllerFor('scopes.scope.auth-methods')[
-      'filter-is_primary'
-    ];
-    controller.setProperties({
-      scopeModel,
-      filters: {
-        type: {
-          items: ['password', 'oidc'],
-          selectedItems: type ? JSON.parse(type) : null,
-        },
-        is_primary: {
-          items: [true, false],
-          selectedItems: is_primary ? JSON.parse(is_primary) : null,
-        },
-      },
-    });
+    controller.setProperties({ scopeModel });
   }
 }
