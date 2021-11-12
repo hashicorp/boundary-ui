b3809b20515648034d714cd516865335c519f05f
Carlos Alcaide Corvo <ccorvo@hashicorp.com>
2021-11-12 13:21:46 -0800

    Refactor members controller location

--- ui/admin/tests/unit/controllers/scopes/scope/auth-methods/auth-method/managed-groups/managed-group/members-test.js
+++ ui/admin/tests/unit/controllers/scopes/scope/auth-methods/auth-method/managed-groups/managed-group/members-test.js
@@ -2,14 +2,14 @@ import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
 module(
-  'Unit | Controller | scopes/scope/auth-methods/auth-method/managed-groups/members',
+  'Unit | Controller | scopes/scope/auth-methods/auth-method/managed-groups/managed-group/members',
   function (hooks) {
     setupTest(hooks);
 
     // TODO: Replace this with your real tests.
     test('it exists', function (assert) {
       let controller = this.owner.lookup(
-        'controller:scopes/scope/auth-methods/auth-method/managed-groups/members'
+        'controller:scopes/scope/auth-methods/auth-method/managed-groups/managed-group/members'
       );
       assert.ok(controller);
     });
