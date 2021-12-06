6c2694999c921eb46b64c7e5ddd6043302e17a57
Carlos Alcaide Corvo <ccorvo@hashicorp.com>
2021-12-06 15:00:48 -0800

    feat: 🎸 Host catalogs only supports host type static
    Host Catalogs will just show the hosts with type: static

--- addons/api/addon/models/host-catalog.js
+++ addons/api/addon/models/host-catalog.js
@@ -1,3 +1,10 @@
 import GeneratedHostCatalogModel from '../generated/models/host-catalog';
 
-export default class HostCatalogModel extends GeneratedHostCatalogModel {}
+export default class HostCatalogModel extends GeneratedHostCatalogModel {
+  /**
+   * @type {boolean}
+   */
+  get isStatic() {
+    return this.type === 'static';
+  }
+}
--- addons/api/tests/unit/models/host-catalog-test.js
+++ addons/api/tests/unit/models/host-catalog-test.js
@@ -9,4 +9,14 @@ module('Unit | Model | host catalog', function (hooks) {
     let model = store.createRecord('host-catalog', {});
     assert.ok(model);
   });
+
+  test('it has isStatic function and returns the expected values', async function (assert) {
+    assert.expect(3);
+    const store = this.owner.lookup('service:store');
+    const modelA = store.createRecord('host-catalog', { type: 'static' });
+    const modelB = store.createRecord('host-catalog', { type: 'plugin' });
+    assert.equal(typeof modelA.isStatic, 'boolean');
+    assert.true(modelA.isStatic);
+    assert.false(modelB.isStatic);
+  });
 });
