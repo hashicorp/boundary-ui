9a8c47024e2885ab107dda0105cb1815afe0803e
Randall Morey <randall@randallmorey.com>
2022-01-03 16:17:56 -0500

    refactor: 💡 remove deprecated renderTemplate usage in desktop

--- ui/desktop/app/routes/scopes/scope/projects/targets/target.js
+++ ui/desktop/app/routes/scopes/scope/projects/targets/target.js
@@ -12,28 +12,4 @@ export default class ScopesScopeProjectsTargetsTargetRoute extends Route {
   model({ target_id }) {
     return this.store.findRecord('target', target_id, { reload: true });
   }
-
-  /**
-   * Renders the target-specific templates for actions, header and navigation
-   * page sections.
-   * @override
-   */
-  renderTemplate() {
-    super.renderTemplate(...arguments);
-
-    this.render('scopes/scope/projects/targets/target/-actions', {
-      into: 'scopes/scope/projects/targets/target',
-      outlet: 'actions',
-    });
-
-    this.render('scopes/scope/projects/targets/target/-header', {
-      into: 'scopes/scope/projects/targets/target',
-      outlet: 'header',
-    });
-
-    this.render('scopes/scope/projects/targets/target/-navigation', {
-      into: 'scopes/scope/projects/targets/target',
-      outlet: 'navigation',
-    });
-  }
 }
