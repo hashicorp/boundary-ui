eb36be3688704b8416c6cf3362d76ffc197e2879
Carlos Alcaide Corvo <ccorvo@hashicorp.com>
2022-01-11 15:34:11 -0800

    feat: 🎸 Add host catalog type basic support
    Add host catalog type basic support. Delete previous conditional
    rendering to just render static types

--- addons/api/addon/models/host-catalog.js
+++ addons/api/addon/models/host-catalog.js
@@ -2,9 +2,26 @@ import GeneratedHostCatalogModel from '../generated/models/host-catalog';
 
 export default class HostCatalogModel extends GeneratedHostCatalogModel {
   /**
+   * Returns if the host-catalog is a plugin or not.
    * @type {boolean}
    */
-  get isStatic() {
-    return this.type === 'static';
+  get isPlugin() {
+    return this.type === 'plugin';
+  }
+
+  /**
+   * Returns if host-catalog plugin is AWS or not
+   * @type {boolean}
+   */
+  get isAWS() {
+    return this.plugin.name === 'aws';
+  }
+
+  /**
+   * Return if host-catalog plugin is Azure or not.
+   * @type {boolean}
+   */
+  get isAzure() {
+    return this.plugin.name === 'azure';
   }
 }
