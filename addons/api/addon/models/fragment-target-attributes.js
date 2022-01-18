ac14e8786d63a4a29aa51a481ff0ef9f09f82520
Randall Morey <randall@randallmorey.com>
2022-01-18 13:44:27 -0500

    feat: 🎸 support isNestedAttribute in models

--- addons/api/addon/models/fragment-target-attributes.js
+++
@@ -1,9 +0,0 @@
-import Fragment from 'ember-data-model-fragments/fragment';
-import { attr } from '@ember-data/model';
-
-export default class FragmentTargetAttributesModel extends Fragment {
-  @attr('number', {
-    description: 'The  default port a target should use if present.',
-  })
-  default_port;
-}
