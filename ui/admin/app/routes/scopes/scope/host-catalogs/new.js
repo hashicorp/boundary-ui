c1d71e51663889d2dec2818497db7df181b12abf
Susmitha Girumala <susmitha@hashicorp.com>
2022-01-26 10:38:04 -0500

    feat: Add type param support to creation form of host catalog

--- ui/admin/app/routes/scopes/scope/host-catalogs/new.js
+++ ui/admin/app/routes/scopes/scope/host-catalogs/new.js
@@ -1,16 +1,27 @@
 import Route from '@ember/routing/route';
 
 export default class ScopesScopeHostCatalogsNewRoute extends Route {
+  // =attributes
+
+  queryParams = {
+    type: {
+      refreshModel: true,
+    },
+  };
+
   // =methods
 
   /**
    * Creates a new unsaved host catalog belonging to the current scope.
    * @return {HostCatalogModel}
    */
-  model() {
+  model(params) {
     const scopeModel = this.modelFor('scopes.scope');
+    // TODO Use model provider type method
+    //  to set params.type to 'static' if type is not provided
+    params.type = 'static';
     return this.store.createRecord('host-catalog', {
-      type: 'static',
+      type: params.type,
       scopeModel,
     });
   }
