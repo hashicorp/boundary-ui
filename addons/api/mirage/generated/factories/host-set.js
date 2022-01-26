f0b21521303e5561130c003f2362182593b90d40
Carlos Alcaide Corvo <ccorvo@hashicorp.com>
2022-01-26 12:34:31 -0800

    Inherit type from host-catalog

--- addons/api/mirage/generated/factories/host-set.js
+++ addons/api/mirage/generated/factories/host-set.js
@@ -5,6 +5,7 @@ import { random, date, datatype } from 'faker';
  * GeneratedHostSetModelFactory
  */
 export default Factory.extend({
+  type: () => this.hostCatalog.type,
   name: () => random.words(),
   description: () => random.words(),
   created_time: () => date.recent(),
