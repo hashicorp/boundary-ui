e150b15f0035cfed1a40884501fbeede2c18f983
Carlos Alcaide Corvo <ccorvo@hashicorp.com>
2022-01-27 11:36:29 -0800

    Add default value for type in mirage

--- addons/api/mirage/generated/factories/host.js
+++ addons/api/mirage/generated/factories/host.js
@@ -5,6 +5,7 @@ import { random, date, internet, datatype } from 'faker';
  * GeneratedHostModelFactory
  */
 export default Factory.extend({
+  type: 'static',
   name: () => random.words(),
   description: () => random.words(),
   created_time: () => date.recent(),
