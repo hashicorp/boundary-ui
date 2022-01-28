043b760cd67397c42cc668b4fb2edf3f5431f485
dharini <dharini@hashicorp.com>
2022-01-28 00:01:17 -0800

    more updates

--- ui/admin/app/routes/scopes/scope/host-catalogs/new.js
+++ ui/admin/app/routes/scopes/scope/host-catalogs/new.js
@@ -24,7 +24,7 @@ export default class ScopesScopeHostCatalogsNewRoute extends Route {
    */
   model(params) {
     const scopeModel = this.modelFor('scopes.scope');
-    if (!params.type) params.type = 'static';
+    if (!params.type) params.type = 'plugin';
     return this.store.createRecord('host-catalog', {
       compositeType: params.type, //static or aws or azure
       scopeModel,
