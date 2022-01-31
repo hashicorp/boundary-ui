9e74c137b7018e7928f43bcc79701786bf629a3e
Randall Morey <randall@randallmorey.com>
2022-01-31 10:40:16 -0500

    refactor: 💡 remove capabilities feature flag

--- ui/admin/app/abilities/model.js
+++ ui/admin/app/abilities/model.js
@@ -1,19 +1,3 @@
 import ModelAbility from 'api/abilities/model';
-import { inject as service } from '@ember/service';
 
-export default class OverrideModelAbility extends ModelAbility {
-  // =services
-
-  @service features;
-
-  // =methods
-
-  /**
-   * If the capabilities feature flag is disabled, actions are always
-   * authorized.  This mimics pre-capabilities behavior.
-   */
-  hasAuthorizedAction() {
-    if (!this.features.isEnabled('capabilities')) return true;
-    return super.hasAuthorizedAction(...arguments);
-  }
-}
+export default class OverrideModelAbility extends ModelAbility {}
--- ui/desktop/app/abilities/collection.js
+++ ui/desktop/app/abilities/collection.js
@@ -1,11 +1,6 @@
 import CollectionAbility from 'api/abilities/collection';
-import { inject as service } from '@ember/service';
 
 export default class OverrideCollectionAbility extends CollectionAbility {
-  // =services
-
-  @service features;
-
   // =permissions
 
   /**
@@ -15,16 +10,5 @@ export default class OverrideCollectionAbility extends CollectionAbility {
    */
   get canNavigate() {
     return this.canList || this.canCreate;
-  }
-
-  // =methods
-
-  /**
-   * If the capabilities feature flag is disabled, actions are always
-   * authorized.  This mimics pre-capabilities behavior.
-   */
-  hasAuthorizedCollectionAction() {
-    if (!this.features.isEnabled('capabilities')) return true;
-    return super.hasAuthorizedCollectionAction(...arguments);
   }
 }
