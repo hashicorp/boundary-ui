361bdf02a4869f82f0eb0817df5d37e0f4ba7f55
dharini <dharini@hashicorp.com>
2022-02-13 22:42:34 -0800

    updates to host-set plugin filters

--- ui/admin/app/routes/scopes/scope/host-catalogs/host-catalog/host-sets/index.js
+++ ui/admin/app/routes/scopes/scope/host-catalogs/host-catalog/host-sets/index.js
@@ -1,4 +1,5 @@
 import Route from '@ember/routing/route';
+
 export default class ScopesScopeHostCatalogsHostCatalogHostSetsIndexRoute extends Route {
   // =methods
   setupController(controller) {
