5b245a3ce19c107416d33ec8b1da69f85937289b
dharini <dharini@hashicorp.com>
2022-02-14 10:37:58 -0800

    lint

--- ui/admin/app/routes/scopes/scope/host-catalogs/host-catalog/host-sets/index.js
+++ ui/admin/app/routes/scopes/scope/host-catalogs/host-catalog/host-sets/index.js
@@ -1,5 +1,4 @@
 import Route from '@ember/routing/route';
-
 export default class ScopesScopeHostCatalogsHostCatalogHostSetsIndexRoute extends Route {
   // =methods
   setupController(controller) {
