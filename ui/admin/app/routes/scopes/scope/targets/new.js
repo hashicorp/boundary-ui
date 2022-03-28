eff5f90394d4f9e260a95fee8a2767f0fe4afdfd
dharini <dharini@hashicorp.com>
2022-03-28 14:02:27 -0700

    feat: 🎸 update target route to support ssh type

--- ui/admin/app/routes/scopes/scope/targets/new.js
+++ ui/admin/app/routes/scopes/scope/targets/new.js
@@ -3,14 +3,24 @@ import Route from '@ember/routing/route';
 export default class ScopesScopeTargetsNewRoute extends Route {
   // =methods
 
+  // =attributes
+  queryParams = {
+    type: {
+      refreshModel: true,
+    },
+  };
   /**
    * Creates a new unsaved target in current scope.
    * @return {TargetModel}
    */
-  model() {
+
+  model(params) {
     const scopeModel = this.modelFor('scopes.scope');
+    if (!params.type) {
+      params.type = 'tcp'; //tcp is the default type
+    }
     return this.store.createRecord('target', {
-      type: 'tcp',
+      type: params.type,
       scopeModel,
     });
   }
