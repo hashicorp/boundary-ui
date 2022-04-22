a68d1149c2a3badb41a93ddbbbea81609a658fc8
dharini <dharini@hashicorp.com>
2022-04-22 15:06:22 -0700

    test: 💍 add more test

--- addons/rose/addon/components/rose/loading-button/index.js
+++ addons/rose/addon/components/rose/loading-button/index.js
@@ -8,19 +8,12 @@ export default class LoadingButton extends button {
   @tracked isLoading = false;
 
   @action
-  //on click of refresh btn, make a call to api..
-  //show the loading icon
-  //after it is done loading the results, show the refresh back and
-  //let users to refresh again..
-
-  //NOTE: do not let users to keep clicking the btn
   async toggleRefresh() {
     this.isLoading = true;
     await this.args.onClick();
     try {
       this.isLoading = false;
     } catch (e) {
-      //todo: what goes here, really?
       throw new Error('error refreshing sessions');
     }
   }
--- addons/rose/tests/integration/components/rose/loading-button-test.js
+++ addons/rose/tests/integration/components/rose/loading-button-test.js
@@ -1,12 +1,20 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, find } from '@ember/test-helpers';
-import { hbs } from 'ember-cli-htmlbars';
+import { render, find, click } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | rose/loading-button', function (hooks) {
   setupRenderingTest(hooks);
   test('it renders', async function (assert) {
-    await render(hbs`<Rose::LoadingButton />`);
+    this.onClick = () => {};
+    await render(hbs`<Rose::LoadingButton @onClick={{fn this.onClick}}/>`);
     assert.ok(find('.refresh-button'));
+  });
+
+  test('it executes a function on refresh button click', async function (assert) {
+    assert.expect(1);
+    this.onClick = () => assert.ok(true, 'refresh was clicked');
+    await render(hbs`{{rose/loading-button onClick=this.onClick}}`);
+    await click('button');
   });
 });
