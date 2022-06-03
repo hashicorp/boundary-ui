4fe4c24953a7ec96c8a551c8188d3b009e28cfd7
Randall Morey <randall@randallmorey.com>
2022-06-03 12:24:56 -0400

    chore: initial routing logic and resource creation

--- ui/admin/app/routes/scopes/scope/onboarding/quick-setup/create-resources.js
+++ ui/admin/app/routes/scopes/scope/onboarding/quick-setup/create-resources.js
@@ -8,18 +8,41 @@ export default class ScopesScopeOnboardingQuickSetupCreateResourcesRoute extends
   // =services
 
   @service router;
+  @service intl;
 
   // =methods
 
-  model() {
-    // const scopeModel = this.modelFor('scopes.scope');
-    // TODO:  locally create resources necessary for onboarding:
-    //  - org
-    //  - project
-    //  - host catalog
-    //  - host set
-    //  - host <- this is the one that will be edited in the form
-    //  - target
+  async model() {
+    const scopeModel = this.modelFor('scopes.scope');
+    return {
+      org: this.store.createRecord('scope', {
+        type: 'org',
+        scopeID: 'global',
+        name: 'Marketing Department',
+        description: 'Sample org created by quick setup',
+      }),
+      project: this.store.createRecord('scope', {
+        type: 'project',
+        name: 'New widget ad campaign',
+        description: 'Sample project created by quick setup',
+      }),
+      hostCatalog: this.store.createRecord('host-catalog', {
+        name: 'All Databases',
+        description: 'Sample host catalog created by quick setup',
+      }),
+      hostSet: this.store.createRecord('host-set', {
+        name: 'QA Databases',
+        description: 'Sample host set created by quick setup',
+      }),
+      host: this.store.createRecord('host', {
+        name: 'CRM Database',
+        description: 'Sample host created by quick setup',
+      }),
+      target: this.store.createRecord('target', {
+        name: 'CRM Database target',
+        description: 'Sample target created by quick setup',
+      }),
+    };
   }
 
   // =actions
@@ -27,16 +50,45 @@ export default class ScopesScopeOnboardingQuickSetupCreateResourcesRoute extends
   @action
   @loading
   @notifyError(({ message }) => message)
-  submit(createHost = true) {
-    if (createHost) {
-      // TODO:  save all created resources
+  async submit(event, hostAddress = '1234', targetPort = '42') {
+    try {
+      await this.createOnboardingResourcesAndRedirect(hostAddress, targetPort);
+    } catch (e) {
+      const message = this.intl.t('errors.quick-setup-failed.description');
+      this.router.replaceWith('scopes.scope', 'global');
+      throw new Error(message);
+    }
+  }
+
+  async createOnboardingResourcesAndRedirect(hostAddress, targetPort) {
+    const { org, project, hostCatalog, hostSet, host, target } =
+      this.currentModel;
+    // The Procedure
+    await org.save();
+    project.scopeID = org.id;
+    await project.save();
+    hostCatalog.scopeID = project.id;
+    await hostCatalog.save();
+    hostSet.host_catalog_id = hostCatalog.id;
+    await hostSet.save();
+    host.host_catalog_id = hostCatalog.id;
+    if (hostAddress) {
+      host.address = hostAddress;
+      await host.save();
+      await hostSet.addHosts([host.id]);
+      target.scopeID = project.id;
+      target.default_port = targetPort;
+      await target.save();
+      await target.addHostSources([hostSet.id]);
       this.router.transitionTo(
         'scopes.scope.onboarding.quick-setup.create-resources.success'
       );
+    } else {
+      this.router.transitionTo(
+        'scopes.scope.host-catalogs.host-catalog.hosts',
+        org.id,
+        hostCatalog.id
+      );
     }
-    // else {
-    //   // TODO:  save resources up to host catalog/host set?
-    //   this.router.transitionTo('scopes.scope.host-catalogs.host-catalog.host-sets')
-    // }
   }
 }
