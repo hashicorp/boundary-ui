ef0fc67afa18b8f1b209bc408fca0dcc78a03d2d
Cameron Perera <cameron.perera@hashicorp.com>
2022-06-14 11:17:44 -0500

    feat: 🎸 Add Controller URL component to Success screen
    ✅ Closes: ICU-4785

--- ui/admin/app/routes/onboarding/quick-setup/create-resources/success.js
+++ ui/admin/app/routes/onboarding/quick-setup/create-resources/success.js
@@ -1,9 +1,26 @@
 import Route from '@ember/routing/route';
+import { getOwner } from '@ember/application';
 
 export default class OnboardingQuickSetupCreateResourcesSuccessRoute extends Route {
+  // =attributes
+
+  /**
+   * Looks up the window object indirectly.
+   * @type {Window}
+   */
+  get window() {
+    // The Ember way of accessing globals...
+    const document = getOwner(this).lookup('service:-document').documentElement;
+    // defaultView === window, but without using globals directly
+    return document.parentNode.defaultView;
+  }
   // =methods
 
   model() {
-    return this.modelFor('onboarding.quick-setup.create-resources');
+    const model = this.modelFor('onboarding.quick-setup.create-resources');
+    return {
+      ...model,
+      origin: this.window.location.origin,
+    };
   }
 }
