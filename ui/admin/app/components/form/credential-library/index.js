890cfe51687eb9ec044b846e852953ea0eed0fd6
Randall Morey <randall@randallmorey.com>
2022-06-28 19:30:26 -0400

    Refactor attributes fragments (#1156)
    * refactor: 💡 migrate managed-group off fragments
    
    * refactor: 💡 migrate credential-store off fragments
    
    * refactor: 💡 migrate credential-library off fragments
    
    * refactor: 💡 migrate account off fragments

--- ui/admin/app/components/form/credential-library/index.js
+++ ui/admin/app/components/form/credential-library/index.js
@@ -13,8 +13,8 @@ export default class FormCredentialLibraryIndexComponent extends Component {
    * Only allow HTTP request body field if http_method is set to POST.
    * @type {boolean}
    */
-  @computed('args.model.attributes.http_method')
+  @computed('args.model.http_method')
   get isHttpRequestBodyAllowed() {
-    return this.args.model.attributes.http_method?.match(/post/i);
+    return this.args.model.http_method?.match(/post/i);
   }
 }
