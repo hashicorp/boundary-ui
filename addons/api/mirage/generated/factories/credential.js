b3b165a8fb6a979b40e3b385fda72580a1f6ee79
Lisbet Alvarez <lisbet.alvarez@hashicorp.com>
2022-07-20 13:11:08 -0700

    feat: 🎸 Added mirage mocks for credential
    ✅ Closes: ICU-5232

--- addons/api/mirage/generated/factories/credential.js
+++ addons/api/mirage/generated/factories/credential.js
@@ -1,12 +1,13 @@
 import { Factory } from 'ember-cli-mirage';
-import { random, date } from 'faker';
+import { random, date, datatype } from 'faker';
 
 /**
- * GeneratedCredentialStoreModelFactory
+ * GeneratedCredentialModelFactory
  */
- export default Factory.extend({
+export default Factory.extend({
   name: () => random.words(),
   description: () => random.words(),
   created_time: () => date.recent(),
   updated_time: () => date.recent(),
+  version: () => datatype.number(),
 });
