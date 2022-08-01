424ee716fcf3db664f37f4f9a7ba3ec66454463f
dvonanderson <devon.anderson@hashicorp.com>
2022-08-01 15:02:03 -0400

    refactor: 💡 refactored `models/worker.js`
    Because `canonical_tags` and `config_tags` are `readOnly` attributes, we
    could use `attr` decorator instead of `fragment`.
    
    ✅ Closes: ICU-5241

--- addons/api/addon/models/worker.js
+++ addons/api/addon/models/worker.js
@@ -1,13 +1,10 @@
 import GeneratedWorkerModel from '../generated/models/worker';
-import { fragment } from 'ember-data-model-fragments/attributes';
+import { attr } from '@ember-data/model';
 
 export default class WorkerModel extends GeneratedWorkerModel {
   // =attributes
 
-  /**
-   * @type {[FragmentTagModel]}
-   */
-  @fragment('fragment-tag', {
+  @attr({
     description:
       'The deduplicated union of the tags reported by the worker ' +
       'from its configuration and any tags added through other means.\nOutput only.',
@@ -15,13 +12,24 @@ export default class WorkerModel extends GeneratedWorkerModel {
   })
   canonical_tags;
 
-  /**
-   * @type {[FragmentTagModel]}
-   */
-  @fragment('fragment-tag', {
+  @attr({
     description:
       "The tags set in the worker's configuration file.\nOutput only.",
     readOnly: true,
   })
   config_tags;
+
+  addWorker(workerGeneratedAuthToken, options = { adapterOptions: {} }) {
+    const defaultAdapterOptions = {
+      method: 'add-worker',
+      workerGeneratedAuthToken,
+    };
+    return this.save({
+      ...options,
+      adapterOpions: {
+        ...defaultAdapterOptions,
+        ...options.adapterOptions,
+      },
+    });
+  }
 }
