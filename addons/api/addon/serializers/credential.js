c5836b3ba72b0a900f275e0f58d9631a6494abcb
Cameron Perera <cameron.perera@hashicorp.com>
2022-08-15 12:37:40 -0500

    feat: 🎸 Update Credential Serializer
    Add support for SSH Private Key credentials
    
    ✅ Closes: ICU-5646

--- addons/api/addon/serializers/credential.js
+++ addons/api/addon/serializers/credential.js
@@ -2,13 +2,35 @@ import ApplicationSerializer from './application';
 import { copy } from 'ember-copy';
 
 export default class CredentialSerializer extends ApplicationSerializer {
-  serialize() {
-    const serialized = super.serialize(...arguments);
-    delete serialized['attributes']['password_hmac'];
-    if (serialized.type === 'username_password') {
-      delete serialized['attributes']['private_key'];
-      delete serialized['attributes']['passphrase'];
+  /**
+   * @override
+   * @method serialize
+   * @param {Snapshot} snapshot
+   */
+  serialize(snapshot) {
+    switch (snapshot.record.type) {
+      case 'username_password':
+        return this.serializeUsernamePassword(...arguments);
+      case 'ssh_private_key':
+      default:
+        return this.serializeSSHPrivateKey(...arguments);
     }
+  }
+
+  serializeUsernamePassword() {
+    const serialized = super.serialize(...arguments);
+    // Remove password_hmac and non-username_password type attributes
+    delete serialized['attributes']['password_hmac'];
+    delete serialized['attributes']['private_key'];
+    delete serialized['attributes']['passphrase'];
+    return serialized;
+  }
+
+  serializeSSHPrivateKey() {
+    const serialized = super.serialize(...arguments);
+    // Remove private_key_hmac and non-ssh_private_key type attributes
+    delete serialized['attributes']['private_key_hmac'];
+    delete serialized['attributes']['password'];
     return serialized;
   }
 
--- addons/api/tests/unit/serializers/credential-test.js
+++ addons/api/tests/unit/serializers/credential-test.js
@@ -4,17 +4,18 @@ import { setupTest } from 'ember-qunit';
 module('Unit | Serializer | credential', function (hooks) {
   setupTest(hooks);
 
-  test('it serializes correctly on create', function (assert) {
+  test('it serializes username_password type correctly on create', function (assert) {
     assert.expect(1);
     const store = this.owner.lookup('service:store');
     const serializer = store.serializerFor('credential');
     const record = store.createRecord('credential', {
       password: 'pass',
       username: 'user',
-      credential_store_id: 'cs_i7p1eu0Nw8',
+      credential_store_id: 'csst_i7p1eu0Nw8',
       type: 'username_password',
       name: 'Name',
       description: 'Description',
+      version: 1,
     });
     const snapshot = record._createSnapshot();
     const serializedRecord = serializer.serialize(snapshot);
@@ -24,10 +25,42 @@ module('Unit | Serializer | credential', function (hooks) {
         password: 'pass',
         username: 'user',
       },
-      credential_store_id: 'cs_i7p1eu0Nw8',
+      credential_store_id: 'csst_i7p1eu0Nw8',
       type: 'username_password',
       name: 'Name',
       description: 'Description',
+      version: 1,
+    });
+  });
+
+  test('it serializes ssh_private_key type correctly on create', function (assert) {
+    assert.expect(1);
+    const store = this.owner.lookup('service:store');
+    const serializer = store.serializerFor('credential');
+    const record = store.createRecord('credential', {
+      passphrase: 'passphrasesaresosecure',
+      private_key: 'superPriveKey',
+      username: 'user',
+      credential_store_id: 'csst_i7p1eu0Nw8',
+      type: 'ssh_private_key',
+      name: 'Name',
+      description: 'Description',
+      version: 1,
+    });
+    const snapshot = record._createSnapshot();
+    const serializedRecord = serializer.serialize(snapshot);
+
+    assert.deepEqual(serializedRecord, {
+      attributes: {
+        passphrase: 'passphrasesaresosecure',
+        private_key: 'superPriveKey',
+        username: 'user',
+      },
+      credential_store_id: 'csst_i7p1eu0Nw8',
+      type: 'ssh_private_key',
+      name: 'Name',
+      description: 'Description',
+      version: 1,
     });
   });
 
@@ -42,7 +75,7 @@ module('Unit | Serializer | credential', function (hooks) {
       type: 'username_password',
       attributes: {
         username: 'username',
-        password: '12345678',
+        password_hmac: 'completenonsense',
       },
     };
     const normalized = serializer.normalize(credentialModelClass, payload);
@@ -55,9 +88,9 @@ module('Unit | Serializer | credential', function (hooks) {
           username: 'username',
           version: 1,
         },
+        type: 'credential',
         id: 'cred_123',
         relationships: {},
-        type: 'credential',
       },
     });
   });
