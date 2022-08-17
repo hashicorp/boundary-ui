dae5174d2ff7ef42c417e6bddf24f88af29f40c8
lisbet-alvarez <107949262+lisbet-alvarez@users.noreply.github.com>
2022-08-17 14:48:52 -0700

    feat: 🎸 Update the credentials route to support both types (#1263)
    Add query param to the credential route in order to support the new ssh
    private key type
    
    ✅ Closes: ICU-5649
    
    Co-authored-by: Cameron Perera <cameron.perera@hashicorp.com>

--- ui/admin/app/routes/scopes/scope/credential-stores/credential-store/credentials/new.js
+++ ui/admin/app/routes/scopes/scope/credential-stores/credential-store/credentials/new.js
@@ -1,19 +1,41 @@
 import Route from '@ember/routing/route';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class ScopesScopeCredentialStoresCredentialStoreCredentialsNewRoute extends Route {
+  // =services
+
+  @service router;
+
+  // =attributes
+
+  queryParams = {
+    type: {
+      refreshModel: true,
+    },
+  };
+
   // =methods
 
   /**
    * Creates a new unsaved credential in current credential store.
    * @return {CredentialModel}
    */
-  model() {
-    const { id: credential_store_id, type } = this.modelFor(
+  model({ type = 'username_password' }) {
+    const { id: credential_store_id } = this.modelFor(
       'scopes.scope.credential-stores.credential-store'
     );
     return this.store.createRecord('credential', {
       type,
       credential_store_id,
     });
+  }
+  /**
+   * Update type of credential
+   * @param {string} type
+   */
+  @action
+  changeType(type) {
+    this.router.replaceWith({ queryParams: { type } });
   }
 }
