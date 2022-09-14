6817c92d2d2d4fd19cd9d7f2a7044dd8c70aabed
Carlos Alcaide Corvo <ccorvo@hashicorp.com>
2022-09-14 14:33:36 +0200

    Refactor host-catalog/hosts/new template to accomodate renderTemplate deprecation

--- ui/admin/app/routes/scopes/scope/host-catalogs/host-catalog/hosts/new.js
+++ ui/admin/app/routes/scopes/scope/host-catalogs/host-catalog/hosts/new.js
@@ -16,33 +16,4 @@ export default class ScopesScopeHostCatalogsHostCatalogHostsNewRoute extends Rou
       host_catalog_id,
     });
   }
-
-  /**
-   * Renders new host specific templates for header, navigation,
-   * and action page sections.
-   * @override
-   * @param {object} controller
-   * @param {object} model
-   */
-  renderTemplate(controller, model) {
-    super.renderTemplate(...arguments);
-
-    this.render('scopes/scope/host-catalogs/host-catalog/hosts/new/-header', {
-      into: 'scopes/scope/host-catalogs/host-catalog',
-      outlet: 'header',
-      model: model,
-    });
-
-    this.render('_empty', {
-      into: 'scopes/scope/host-catalogs/host-catalog',
-      outlet: 'navigation',
-      model: model,
-    });
-
-    this.render('_empty', {
-      into: 'scopes/scope/host-catalogs/host-catalog',
-      outlet: 'actions',
-      model: model,
-    });
-  }
 }
