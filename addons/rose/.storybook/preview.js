0b7ddb23c45bbc5aec979e9704b0caa4d84499af
Zhihe Li <zhihe.li@hashicorp.com>
2022-10-21 14:35:53 -0400

    style: 💄 Cleanup styles mdx

--- addons/rose/.storybook/preview.js
+++ addons/rose/.storybook/preview.js
@@ -1,5 +1,7 @@
 import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
 import handlebars from 'react-syntax-highlighter/dist/esm/languages/prism/handlebars';
+import scss from 'react-syntax-highlighter/dist/esm/languages/prism/scss';
 
-// Registers and enables handlebars language support
+// Registers and enables language support
 SyntaxHighlighter.registerLanguage('handlebars', handlebars);
+SyntaxHighlighter.registerLanguage('scss', scss);
