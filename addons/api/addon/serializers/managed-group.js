0e08dfc2791a70716258905e1d2108dbf34aca3e
Zhihe Li <zhihe.li@hashicorp.com>
2022-11-23 18:29:39 -0500

    refactor: 💡 Rename attributes called filter

--- addons/api/addon/serializers/managed-group.js
+++ addons/api/addon/serializers/managed-group.js
@@ -1,4 +1,5 @@
 import ApplicationSerializer from './application';
+import { copy } from 'ember-copy';
 
 export default class ManagedGroupSerializer extends ApplicationSerializer {
   // =properties
@@ -7,4 +8,29 @@ export default class ManagedGroupSerializer extends ApplicationSerializer {
    * @type {boolean}
    */
   serializeScopeID = false;
+
+  serialize() {
+    const serialized = super.serialize(...arguments);
+
+    if (serialized.attributes.filter_string) {
+      serialized.attributes.filter = serialized.attributes.filter_string;
+    } else {
+      serialized.attributes.filter = null;
+    }
+    delete serialized.attributes.filter_string;
+
+    return serialized;
+  }
+
+  normalize(typeClass, hash, ...rest) {
+    const normalizedHash = copy(hash, true);
+
+    if (normalizedHash?.attributes?.filter) {
+      normalizedHash.attributes.filter_string =
+        normalizedHash.attributes.filter;
+      delete normalizedHash.attributes.filter;
+    }
+
+    return super.normalize(typeClass, normalizedHash, ...rest);
+  }
 }
