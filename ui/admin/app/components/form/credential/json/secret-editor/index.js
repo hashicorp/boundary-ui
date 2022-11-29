3238f73f377636d7ff403f723343843f94303c67
Cameron Perera <cameron.perera@hashicorp.com>
2022-11-29 10:53:12 -0600

    refactor: 💡 remove computed decorators for Secret Editor

--- ui/admin/app/components/form/credential/json/secret-editor/index.js
+++ ui/admin/app/components/form/credential/json/secret-editor/index.js
@@ -1,7 +1,6 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import { computed } from '@ember/object';
 
 export default class FormCredentialJsonSecretEditorComponent extends Component {
   // =actions
@@ -12,17 +11,14 @@ export default class FormCredentialJsonSecretEditorComponent extends Component {
     this.allowEditing = true;
   }
 
-  @computed('args.{isNew,isEditing}')
   get isDetailView() {
     return !this.args.isNew && !this.args.isEditing;
   }
 
-  @computed('args.{isNew,isEditing}', 'allowEditing')
   get isEditView() {
-    return !this.args.isNew && (this.args.isEditing && !this.allowEditing);
+    return !this.args.isNew && this.args.isEditing && !this.allowEditing;
   }
 
-  @computed('args.{isNew,isEditing}', 'allowEditing')
   get canEdit() {
     return this.args.isNew || (this.args.isEditing && this.allowEditing);
   }
