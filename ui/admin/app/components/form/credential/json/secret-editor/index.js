e506aaabb380b0590dbb7945c1a22ebac5dfe2c7
Cameron Perera <cameron.perera@hashicorp.com>
2022-11-30 14:09:42 -0600

    refactor: 💡 add comments to SecretEditor component class

--- ui/admin/app/components/form/credential/json/secret-editor/index.js
+++ ui/admin/app/components/form/credential/json/secret-editor/index.js
@@ -3,23 +3,52 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class FormCredentialJsonSecretEditorComponent extends Component {
-  // =actions
+  // =properties
+
+  /**
+   * Tracks when editor should be displayed
+   * @type {boolean}
+   */
   @tracked allowEditing = false;
 
-  @action
-  allowEdit() {
-    this.allowEditing = true;
-  }
-
+  /**
+   * True if viewing the resource after it has been created but not editing it
+   * and displays as a read only state. Since this is a secret editor the secret
+   * value is not displayed and a skeleton view is show.
+   * @type {boolean}
+   */
   get isDetailView() {
     return !this.args.isNew && !this.args.isEditing;
   }
 
+  /**
+   * True if editing the resource after it has been created but not editing
+   * the secret value in the Secret Editor. This will still present the Secret
+   * Editor as a read only state even when the form is editing the resource.
+   * @type {boolean}
+   */
   get isEditView() {
     return !this.args.isNew && this.args.isEditing && !this.allowEditing;
   }
 
+  /**
+   * True if creating a new resource or editing the resource after it has been 
+   * created and the user has clicked the Secret Editor button while in
+   * EditView.
+   * @type {boolean}
+   */
   get canEdit() {
     return this.args.isNew || (this.args.isEditing && this.allowEditing);
+  }
+
+  // =actions
+
+  /**
+   * Sets allowEditing to true when Secret Editor
+   * button is clicked in Edit view
+   */
+  @action
+  allowEdit() {
+    this.allowEditing = true;
   }
 }
