b084f0acde80ae99e7ff8d98f69d1f6e60132e26
Lisbet Alvarez <lisbet.alvarez@hashicorp.com>
2022-11-30 18:01:49 -0800

    refactor: 💡 add new info field

--- addons/api/addon/models/credential.js
+++ addons/api/addon/models/credential.js
@@ -1,12 +1,20 @@
 import GeneratedCredentialModel from '../generated/models/credential';
+// import { inject as service } from '@ember/service';
 
 /**
  * Supported Credential types.
  */
-export const types = ['username_password', 'ssh_private_key', 'json'];
+export const types = ['username_password', 'ssh_private_key'];
 
 export default class CredentialModel extends GeneratedCredentialModel {
+  // =services
+  // @service features;
+
   // =attributes
+  // types = this.features.isEnabled('json-credentials')
+  //   ? ['username_password', 'ssh_private_key', 'json']
+  //   : ['username_password', 'ssh_private_key'];
+
   /**
    * All Credentials are prefixed with "cred" and are considered
    * static due to their relation with Static Credential Stores.
