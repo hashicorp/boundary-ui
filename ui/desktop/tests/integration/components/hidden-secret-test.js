3b084b09723645bec6581bcb5f122123299e8001
dvonanderson <devon.anderson@hashicorp.com>
2023-01-04 12:57:32 -0500

    refactor: 💡 test
    Changed test to check for secret output after clicking on reveal

--- ui/desktop/tests/integration/components/hidden-secret-test.js
+++ ui/desktop/tests/integration/components/hidden-secret-test.js
@@ -1,35 +1,49 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, find } from '@ember/test-helpers';
+import { render, find, click } from '@ember/test-helpers';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import { hbs } from 'ember-cli-htmlbars';
 import { setupIntl } from 'ember-intl/test-support';
-
 module('Integration | Component | hidden-secret', function (hooks) {
   setupRenderingTest(hooks);
   setupIntl(hooks);
+  setupMirage(hooks);
+
+  // Global Secret for mocking
+  this.secret = `{
+    "secret_key": "QWERTYUIOP",
+    "secret_access_key": "QWERT.YUIOP234567890",
+    "nested_secret": {
+      "session_token": "ZXCVBNMLKJHGFDSAQWERTYUIOP0987654321",
+      "complex_nest": {
+        "hash": "qazxswedcvfrtgbnjhyujm.1234567890"
+      }
+    }
+  }`;
+
+  async function renderComponent() {
+    return render(hbs`
+      <HiddenSecret
+        @secret={{this.secret}}
+      />
+    `);
+  }
 
   test('it renders with a secret', async function (assert) {
-    assert.expect(2);
-
-    this.secret = {
-      secret_key: 'QWERTYUIOP',
-      secret_access_key: 'QWERT.YUIOP234567890',
-      nested_secret: {
-        session_token: 'ZXCVBNMLKJHGFDSAQWERTYUIOP0987654321',
-        complex_nest: {
-          hash: 'qazxswedcvfrtgbnjhyujm.1234567890',
-        },
-      },
-    };
-
-    await render(hbs`
-      <HiddenSecret @secret={{secret}} />
-    `);
-
+    await renderComponent();
     assert.ok(find('.hidden-secret'));
     assert.strictEqual(
       find('.hidden-secret > span > span').textContent.trim(),
       '■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■'
+    );
+  });
+
+  test('it reveals secret when button is clicked', async function (assert) {
+    await renderComponent();
+    await click('.hidden-secret > button');
+    assert.strictEqual(
+      find('.hidden-secret > span > span').textContent.trim(),
+      this.secret
     );
   });
 });
