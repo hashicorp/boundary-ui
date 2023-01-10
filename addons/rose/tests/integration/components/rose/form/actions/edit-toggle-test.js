9ebdd35a0fba2cb9b32f27960f8b33a75b6b4c9d
Carlos Alcaide Corvo <ccorvo@hashicorp.com>
2023-01-10 13:56:49 -0800

    refactor: 💡 Deprecate htmlbars-inline-precompile within addons
    Deprecate htmlbars-inline-precompile within addons

--- addons/rose/tests/integration/components/rose/form/actions/edit-toggle-test.js
+++ addons/rose/tests/integration/components/rose/form/actions/edit-toggle-test.js
@@ -1,7 +1,7 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render, find, click } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
+import { hbs } from 'ember-cli-htmlbars';
 
 module(
   'Integration | Component | rose/form/actions/edit-toggle',
