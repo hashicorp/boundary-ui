faa68125d3125db8ae616937c8be3cf61ad6ce9c
Cameron Perera <cameron.perera@hashicorp.com>
2023-01-15 17:45:56 -0600

    feat: 🎸 create Worker Diagram for OSS
    ✅ Closes: ICU-7579

---
+++ ui/admin/tests/integration/components/worker-diagram/index-test.js
@@ -0,0 +1,24 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'admin/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import { setupIntl } from 'ember-intl/test-support';
+
+module('Integration | Component | worker-diagram/index', function (hooks) {
+  setupRenderingTest(hooks);
+  setupIntl(hooks);
+
+  let featuresService;
+  const targetWorkerFilterIngress = 'target-worker-filters-v2-ingress';
+  const targetWorkerFilterHCP = 'target-worker-filters-v2-hcp';
+
+  test('it renders a single filter diagram', async function (assert) {
+    featuresService = this.owner.lookup('service:features');
+    featuresService.disable(targetWorkerFilterIngress);
+    featuresService.disable(targetWorkerFilterHCP);
+    assert.expect(1);
+    await render(hbs`<WorkerDiagram />`);
+
+    assert.dom('[data-test-single-filter]').isVisible();
+  });
+});
---
+++ ui/admin/tests/integration/components/worker-diagram/single-filter/index-test.js
@@ -0,0 +1,31 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'admin/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import { setupIntl } from 'ember-intl/test-support';
+
+module(
+  'Integration | Component | worker-diagram/single-filter',
+  function (hooks) {
+    setupRenderingTest(hooks);
+    setupIntl(hooks);
+
+    test('it renders the correct diagram when egressFilter is false', async function (assert) {
+      assert.expect(2);
+      await render(
+        hbs`<WorkerDiagram::SingleFilter @egressFilter={{false}} />`
+      );
+
+      assert.dom('[data-test-egress-off]').isVisible();
+      assert.dom('[data-test-egress-on]').doesNotExist();
+    });
+
+    test('it renders the correct diagram when egressFilter is true', async function (assert) {
+      assert.expect(2);
+      await render(hbs`<WorkerDiagram::SingleFilter @egressFilter={{true}} />`);
+
+      assert.dom('[data-test-egress-on]').isVisible();
+      assert.dom('[data-test-egress-off]').doesNotExist();
+    });
+  }
+);
