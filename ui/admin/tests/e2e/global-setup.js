6c892cc384a94c8f3f4192f44a9f4cf1831664ae
Michael Li <michael.li@hashicorp.com>
2023-02-21 12:02:50 -0500

    test(admin/e2e): Add playwright testing library

---
+++ ui/admin/tests/e2e/global-setup.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-undef */
+const { chromium } = require('@playwright/test');
+const { checkEnv } = require('./helpers/general');
+
+module.exports = async () => {
+  await checkEnv([
+    'BOUNDARY_ADDR',
+    'E2E_PASSWORD_ADMIN_LOGIN_NAME',
+    'E2E_PASSWORD_ADMIN_PASSWORD',
+    'E2E_PASSWORD_AUTH_METHOD_ID',
+  ]);
+
+  // Log in and save the authenticated state to reuse in tests
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  await page.goto(process.env.BOUNDARY_ADDR);
+  await page
+    .getByLabel('Login Name')
+    .fill(process.env.E2E_PASSWORD_ADMIN_LOGIN_NAME);
+  await page
+    .getByLabel('Password')
+    .fill(process.env.E2E_PASSWORD_ADMIN_PASSWORD);
+  await page.getByRole('button', { name: 'Sign In' }).click();
+  await page.getByRole('navigation', { name: 'General' }).waitFor();
+  await page.getByText(process.env.E2E_PASSWORD_ADMIN_LOGIN_NAME).waitFor();
+  await page
+    .context()
+    .storageState({ path: './tests/e2e/artifacts/authenticated-state.json' });
+  await browser.close();
+};
