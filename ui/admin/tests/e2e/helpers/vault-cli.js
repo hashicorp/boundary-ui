d3102a17e4e3c37f8ff8fb75d05f0db01dc1a455
Michael Li <michael.li@hashicorp.com>
2023-02-22 11:31:54 -0500

    test(admin/e2e): Add a vault test

---
+++ ui/admin/tests/e2e/helpers/vault-cli.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+const { execSync } = require('child_process');
+
+/**
+ * Checks that the vault cli is available
+ */
+exports.checkVaultCli = async () => {
+  try {
+    execSync('which vault');
+  } catch (e) {
+    throw new Error('vault does not exist on the path');
+  }
+};
