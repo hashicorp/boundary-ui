7816772ef2e1e57125fa905c12450b0f17a39b2e
Michael Li <michael.li@hashicorp.com>
2023-02-23 18:30:51 -0500

    test(admin/e2e): Add first playwright test (#1637)
    Adds an end-to-end test for the Admin UI using Playwright. This test uses the Admin UI to create the necessary resources to connect to a target. The test then uses the CLI to start a session and checks in the Admin UI that a session is created

---
+++ ui/admin/tests/e2e/helpers/general.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+exports.authenticatedState = './tests/e2e/artifacts/authenticated-state.json';
+
+/**
+ * Checks that the provided environment variables are set
+ * @param {string[]} envs List of environment variables
+ */
+exports.checkEnv = async (envs) => {
+  let missing = [];
+  for (let env in envs) {
+    if (!process.env[envs[env]]) {
+      missing.push(envs[env]);
+    }
+  }
+
+  if (missing.length > 0) {
+    throw new Error('Missing Environment Variables -- ' + missing);
+  }
+};
