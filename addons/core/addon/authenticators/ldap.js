2e88c020401406571255e32820aab999cdbd0ae7
Randall Morey <randall@randallmorey.com>
2023-02-24 14:38:58 -0500

    feat: 🎸 initial LDAP authentication support
    does not include CRUD for LDAP yet

---
+++ addons/core/addon/authenticators/ldap.js
@@ -0,0 +1,8 @@
+import PasswordAuthenticator from './password';
+
+/**
+ * A simple login_name/password authenticator that authenticates with
+ * a scope/auth-method endpoint.  Authentication occurs on an auth method
+ * endpoint, while deauthentication occurs on a scope endpoint.
+ */
+export default class LDAPAuthenticator extends PasswordAuthenticator {}
