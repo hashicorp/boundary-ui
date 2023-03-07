449ea383c9b195cfadcb3f1588f10b6aa8a1fa5c
Cameron Perera <cameron.perera@hashicorp.com>
2023-03-07 11:04:51 -0600

    Icu 8329 create custom boundary wrapper for ember asciinema player (#103)
    * refactor: 💡 fix broken page title translations
    
    * feat: 🎸 create SessionRecording::Player component
    
    ✅ Closes: ICU-8329

---
+++ ui/admin/tests/integration/components/session-recording/player-test.js
@@ -0,0 +1,34 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'admin/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | session-recording/player', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+    const castConfig = {
+      version: 2,
+      width: 124,
+      height: 36,
+      timestamp: 1670352594,
+      env: { SHELL: '/bin/zsh', TERM: 'xterm-256color' },
+    };
+    this.cast = `${JSON.stringify(castConfig)}
+[2.676444, "o", "y"]
+[2.788653, "o", "a"]
+[2.868652, "o", "r"]
+[3.001764, "o", "n"]
+[3.167636, "o", " "]
+[3.313668, "o", "t"]
+[3.460527, "o", "e"]
+[3.561983, "o", "s"]
+[3.63678, "o", "t"]`;
+
+    await render(hbs`
+      <SessionRecording::Player @asciiCast={{this.cast}} />
+    `);
+
+    assert.dom(this.element).hasText('Back to channels --:----:--');
+  });
+});
