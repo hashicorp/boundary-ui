59850651b88180c74cba7ab7c4eea855da96031d
Carlos Alcaide <ccorvo@hashicorp.com>
2023-03-14 09:56:47 -0700

    feat: 🎸 Add format-time-duration helper
    Add format-time-duration helper to provide human-readable duration
    string
    
    ✅ Closes: ICU-8233

---
+++ addons/core/addon/helpers/format-time-duration.js
@@ -0,0 +1,25 @@
+import Helper from '@ember/component/helper';
+import { inject as service } from '@ember/service';
+import DurationUnitFormat from 'intl-unofficial-duration-unit-format';
+
+export default class extends Helper {
+  // =services
+  @service intl;
+
+  // =methods
+  /**
+   *
+   * @param {number} durationInMs Duration in milliseconds
+   * @returns localized human-readable duration string. For example, in English "1 hr 23 min 20 sec"
+   */
+  compute([durationInMs]) {
+    const durationInSeconds = durationInMs / 1000;
+    const durationFormat = new DurationUnitFormat(this.intl.primaryLocale, {
+      style: 'short',
+      format: '{days} {hours} {minutes} {seconds}',
+      round: true,
+    });
+
+    return durationFormat.format(durationInSeconds);
+  }
+}
---
+++ addons/core/tests/integration/helpers/format-time-duration-test.js
@@ -0,0 +1,30 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'dummy/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import { setupIntl } from 'ember-intl/test-support';
+
+module('Integration | Helper | format-time-duration', function (hooks) {
+  setupRenderingTest(hooks);
+  setupIntl(hooks);
+
+  test('It renders in human-readable duration strings given different durations in milliseconds', async function (assert) {
+    // Set mock input (in milliseconds) durations
+    this.set('inputDurationSeconds', 3000);
+    this.set('inputDurationMinutes', 2700000);
+    this.set('inputDurationHours', 5000000);
+    this.set('inputDurationDays', 177800000);
+
+    await render(hbs`{{format-time-duration this.inputDurationSeconds}}`);
+    assert.dom(this.element).hasText('3 sec');
+
+    await render(hbs`{{format-time-duration this.inputDurationMinutes}}`);
+    assert.dom(this.element).hasText('45 min');
+
+    await render(hbs`{{format-time-duration this.inputDurationHours}}`);
+    assert.dom(this.element).hasText('1 hr 23 min 20 sec');
+
+    await render(hbs`{{format-time-duration this.inputDurationDays}}`);
+    assert.dom(this.element).hasText('2 days 1 hr 23 min 20 sec');
+  });
+});
