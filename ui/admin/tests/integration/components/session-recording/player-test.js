9fc4be7b61dbd2a9004d3e16c20c2d293f985971
Cameron Perera <cameron.perera@hashicorp.com>
2023-04-05 10:02:19 -0500

    refactor: 💡 fix active link style issue with SR Player (#183)
    * refactor: 💡 fix active link style issue with SR Player
    
    ✅ Closes: ICU-8774
    
    * refactor: 💡 update player hds link to hds button and styles
    
    * refactor: 💡 add translation for "Back to channels" text
    
    * test: 💍 add Intl setup hook to player test

--- ui/admin/tests/integration/components/session-recording/player-test.js
+++ ui/admin/tests/integration/components/session-recording/player-test.js
@@ -2,9 +2,11 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'admin/tests/helpers';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import { setupIntl } from 'ember-intl/test-support';
 
 module('Integration | Component | session-recording/player', function (hooks) {
   setupRenderingTest(hooks);
+  setupIntl(hooks);
 
   test('it renders', async function (assert) {
     const castConfig = {
