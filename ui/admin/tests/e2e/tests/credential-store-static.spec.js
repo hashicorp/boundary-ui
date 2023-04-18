78490c9d1b29c990a65ec147b5e83da5f1847b92
Stan Ryzhov <60649800+stasryzhov@users.noreply.github.com>
2023-04-18 16:49:03 -0400

    test: 💍 (admin-e2e): Add test to create user/password creds
    ✅ Closes: ICU-8564

--- ui/admin/tests/e2e/tests/credential-store-static.spec.js
+++ ui/admin/tests/e2e/tests/credential-store-static.spec.js
@@ -146,3 +146,113 @@ test('Static Credential Store (User & Key Pair)', async ({ page }) => {
     throw new Error('Stored Key does not match');
   }
 });
+
+test('Static Credential Store (Username & Password)', async ({ page }) => {
+  const orgName = await createNewOrg(page);
+  const projectName = await createNewProject(page);
+  await createNewHostCatalog(page);
+  const hostSetName = await createNewHostSet(page);
+  await createNewHostInHostSet(page);
+  const targetName = await createNewTarget(page);
+  await addHostSourceToTarget(page, hostSetName);
+
+  const credentialStoreName = 'Credential Store ' + nanoid();
+  await page
+    .getByRole('navigation', { name: 'Resources' })
+    .getByRole('link', { name: 'Credential Stores' })
+    .click();
+  await page.getByRole('link', { name: 'New' }).click();
+  await page.getByLabel('Name', { exact: true }).fill(credentialStoreName);
+  await page.getByLabel('Description').fill('This is an automated test');
+  await page.getByRole('group', { name: 'Type' }).getByLabel('Static').click();
+  await page.getByRole('button', { name: 'Save' }).click();
+  await expect(
+    page.getByRole('alert').getByText('Success', { exact: true })
+  ).toBeVisible();
+  await page.getByRole('button', { name: 'Dismiss' }).click();
+  await expect(
+    page.getByRole('link', { name: credentialStoreName })
+  ).toBeVisible();
+
+  const credentialName = 'Credential ' + nanoid();
+  await page.getByRole('link', { name: 'Credentials', exact: true }).click();
+  await page.getByRole('link', { name: 'New', exact: true }).click();
+  await page.getByLabel('Name', { exact: true }).fill(credentialName);
+  await page.getByLabel('Description').fill('This is an automated test');
+
+  // adds username & password
+  await page
+    .getByRole('group', { name: 'Type' })
+    .getByLabel('Username & Password')
+    .click();
+  await page
+    .getByLabel('Username', { exact: true })
+    .fill(process.env.E2E_SSH_USER);
+  // const keyData = await readFile(process.env.E2E_SSH_KEY_PATH, {
+  //   encoding: 'utf-8',
+  // });
+  const testPassword = 'password';
+  await page.getByLabel('Password', { exact: true }).fill(testPassword);
+  await page.getByRole('button', { name: 'Save' }).click();
+  await expect(
+    page.getByRole('alert').getByText('Success', { exact: true })
+  ).toBeVisible();
+  await page.getByRole('button', { name: 'Dismiss' }).click();
+  await expect(page.getByRole('link', { name: credentialName })).toBeVisible();
+
+  await page
+    .getByRole('navigation', { name: 'Resources' })
+    .getByRole('link', { name: 'Targets' })
+    .click();
+  await page.getByRole('link', { name: targetName }).click();
+  await page
+    .getByRole('link', { name: 'Brokered Credentials', exact: true })
+    .click();
+  await page
+    .getByRole('article')
+    .getByRole('link', { name: 'Add Brokered Credentials', exact: true })
+    .click();
+  await page
+    .getByRole('cell', { name: credentialName })
+    .locator('..')
+    .getByRole('checkbox')
+    .click({ force: true });
+  await page
+    .getByRole('button', { name: 'Add Brokered Credentials', exact: true })
+    .click();
+  await expect(
+    page.getByRole('alert').getByText('Success', { exact: true })
+  ).toBeVisible();
+  await page.getByRole('button', { name: 'Dismiss' }).click();
+  await expect(page.getByRole('link', { name: credentialName })).toBeVisible();
+
+  await authenticateBoundaryCli();
+  const orgs = JSON.parse(execSync('boundary scopes list -format json'));
+  const org = orgs.items.filter((obj) => obj.name == orgName)[0];
+  const projects = JSON.parse(
+    execSync(`boundary scopes list -format json -scope-id ${org.id}`)
+  );
+  const project = projects.items.filter((obj) => obj.name == projectName)[0];
+  const targets = JSON.parse(
+    execSync(`boundary targets list -format json -scope-id ${project.id}`)
+  );
+  const target = targets.items.filter((obj) => obj.name == targetName)[0];
+
+  const session = JSON.parse(
+    execSync(`boundary targets authorize-session -id ${target.id} -format json`)
+  );
+  const retrievedUser = session.item.credentials[0].credential.username;
+  const retrievedPassword = session.item.credentials[0].credential.password;
+
+  if (process.env.E2E_SSH_USER != retrievedUser) {
+    throw new Error(
+      'Stored User does not match. EXPECTED: ' +
+        process.env.E2E_SSH_USER +
+        ', ACTUAL: ' +
+        retrievedUser
+    );
+  }
+  if (testPassword != retrievedPassword) {
+    throw new Error('Stored Password does not match');
+  }
+});
