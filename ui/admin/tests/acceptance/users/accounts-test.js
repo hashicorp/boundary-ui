696f0fe04aa546af71e5758f79d2a221ea2e41d3
Lisbet Alvarez <lisbet.alvarez@hashicorp.com>
2023-05-15 10:44:56 -0700

    test: 💍 add tests and refactor existing tests

--- ui/admin/tests/acceptance/users/accounts-test.js
+++ ui/admin/tests/acceptance/users/accounts-test.js
@@ -4,7 +4,7 @@
  */
 
 import { module, test } from 'qunit';
-import { visit, currentURL, click, find, findAll } from '@ember/test-helpers';
+import { visit, currentURL, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import a11yAudit from 'ember-a11y-testing/test-support/audit';
@@ -15,10 +15,26 @@ import {
   //currentSession,
   //invalidateSession,
 } from 'ember-simple-auth/test-support';
+import { TYPE_AUTH_METHOD_LDAP } from 'api/models/auth-method';
 
 module('Acceptance | users | accounts', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
+
+  let accountsCount;
+
+  const ACCOUNTS_ACTION_SELECTOR =
+    'tbody .rose-table-row:nth-child(1) .rose-table-cell:last-child .rose-dropdown';
+  const ACCOUNTS_TYPE_SELECTOR =
+    'tbody .rose-table-row:nth-child(1) .rose-table-cell';
+  const ADD_ACCOUNTS_ACTION_SELECTOR = '.rose-layout-page-actions a';
+  const ERROR_MSG_SELECTOR = '[role="alert"]';
+  const TABLE_SELECTOR = '.rose-table';
+  const TABLE_ROWS_SELECTOR = 'tbody tr';
+  const CHECKBOX_SELECTOR = 'tbody label';
+  const SUBMIT_BTN_SELECTOR = 'form [type="submit"]';
+  const CANCEL_BTN_SELECTOR = 'form [type="button"]';
+  const REMOVE_ACTION_SELECTOR = 'tbody tr .rose-dropdown-button-danger';
 
   const instances = {
     scopes: {
@@ -26,15 +42,18 @@ module('Acceptance | users | accounts', function (hooks) {
       org: null,
     },
     user: null,
+    globalUser: null,
   };
+
   const urls = {
-    orgScope: null,
+    globalScope: null,
     users: null,
     user: null,
+    globalUser: null,
     accounts: null,
+    ldapAccounts: null,
     addAccounts: null,
   };
-  let accountsCount;
 
   hooks.beforeEach(function () {
     authenticateSession({});
@@ -50,28 +69,47 @@ module('Acceptance | users | accounts', function (hooks) {
       },
       'withAccountsAndUsersAndManagedGroups'
     );
-    instances.user = this.server.schema.users.all().models[0];
+    this.server.create(
+      'auth-method',
+      {
+        scope: instances.scopes.global,
+        type: TYPE_AUTH_METHOD_LDAP,
+      },
+      'withAccountsAndUsersAndManagedGroups'
+    );
+    instances.user = this.server.schema.users.first();
+    instances.globalUser = this.server.schema.users.findBy({
+      scopeId: 'global',
+    });
     accountsCount = instances.user.accountIds.length;
+    urls.globalScope = `/scopes/global`;
     urls.users = `/scopes/${instances.scopes.org.id}/users`;
     urls.user = `${urls.users}/${instances.user.id}`;
+    urls.globalUser = `${urls.globalScope}/users/${instances.globalUser.id}`;
     urls.accounts = `${urls.user}/accounts`;
+    urls.ldapAccounts = `${urls.globalUser}/accounts`;
     urls.addAccounts = `${urls.user}/add-accounts`;
   });
 
   test('visiting user accounts', async function (assert) {
     assert.expect(2);
-    await visit(urls.accounts);
+    await visit(urls.user);
+
+    await click(`[href="${urls.accounts}"]`);
     await a11yAudit();
+
     assert.strictEqual(currentURL(), urls.accounts);
-    assert.strictEqual(findAll('tbody tr').length, accountsCount);
+    assert.dom(TABLE_ROWS_SELECTOR).exists({ count: accountsCount });
   });
 
   test('can remove an account', async function (assert) {
     assert.expect(2);
-    await visit(urls.accounts);
-    assert.strictEqual(findAll('tbody tr').length, accountsCount);
-    await click('tbody tr .rose-dropdown-button-danger');
-    assert.strictEqual(findAll('tbody tr').length, accountsCount - 1);
+    await visit(urls.user);
+
+    await click(`[href="${urls.accounts}"]`);
+    assert.dom(TABLE_ROWS_SELECTOR).exists({ count: accountsCount });
+    await click(REMOVE_ACTION_SELECTOR);
+    assert.dom(TABLE_ROWS_SELECTOR).exists({ count: accountsCount - 1 });
   });
 
   test('cannot remove an account without proper authorization', async function (assert) {
@@ -80,8 +118,21 @@ module('Acceptance | users | accounts', function (hooks) {
       (item) => item !== 'remove-accounts'
     );
     instances.user.update({ authorized_actions });
-    await visit(urls.accounts);
-    assert.notOk(find('tbody tr .rose-dropdown-button-danger'));
+    await visit(urls.user);
+
+    await click(`[href="${urls.accounts}"]`);
+
+    assert.dom(REMOVE_ACTION_SELECTOR).doesNotExist();
+  });
+
+  test('cannot remove an ldap account', async function (assert) {
+    assert.expect(2);
+    await visit(urls.globalUser);
+
+    await click(`[href="${urls.ldapAccounts}"]`);
+
+    assert.dom(ACCOUNTS_ACTION_SELECTOR).doesNotExist();
+    assert.dom(ACCOUNTS_TYPE_SELECTOR).hasText('LDAP');
   });
 
   test('shows error message on account remove', async function (assert) {
@@ -98,24 +149,31 @@ module('Acceptance | users | accounts', function (hooks) {
         }
       );
     });
-    await visit(urls.accounts);
-    assert.strictEqual(findAll('tbody tr').length, accountsCount);
-    await click('tbody tr .rose-dropdown-button-danger');
-    assert.ok(find('[role="alert"]'));
+    await visit(urls.user);
+
+    await click(`[href="${urls.accounts}"]`);
+    assert.dom(TABLE_ROWS_SELECTOR).exists({ count: accountsCount });
+    await click(REMOVE_ACTION_SELECTOR);
+    assert.dom(ERROR_MSG_SELECTOR).isVisible();
   });
 
   test('visiting account add accounts', async function (assert) {
     assert.expect(1);
-    await visit(urls.addAccounts);
-    await visit(urls.addAccounts);
+    await visit(urls.accounts);
+
+    await click(ADD_ACCOUNTS_ACTION_SELECTOR);
     await a11yAudit();
+
     assert.strictEqual(currentURL(), urls.addAccounts);
   });
 
   test('can navigate to add accounts with proper authorization', async function (assert) {
     assert.expect(1);
-    await visit(urls.accounts);
-    assert.ok(find(`[href="${urls.addAccounts}"]`));
+    await visit(urls.user);
+
+    await click(`[href="${urls.accounts}"]`);
+
+    assert.dom(`[href="${urls.addAccounts}"]`).exists();
   });
 
   test('cannot navigate to add accounts without proper authorization', async function (assert) {
@@ -124,38 +182,56 @@ module('Acceptance | users | accounts', function (hooks) {
       (item) => item !== 'add-accounts'
     );
     instances.user.update({ authorized_actions });
-    await visit(urls.accounts);
-    assert.notOk(find(`[href="${urls.addAccounts}"]`));
+    await visit(urls.user);
+
+    await click(`[href="${urls.accounts}"]`);
+    assert.dom(ADD_ACCOUNTS_ACTION_SELECTOR).doesNotExist();
+  });
+
+  test('cannot add ldap accounts to user since they were filtered out', async function (assert) {
+    assert.expect(1);
+    await visit(urls.globalUser);
+
+    await click(`[href="${urls.ldapAccounts}"]`);
+    await click(ADD_ACCOUNTS_ACTION_SELECTOR);
+
+    assert.dom(TABLE_SELECTOR).doesNotExist();
   });
 
   test('select and save accounts to add', async function (assert) {
-    assert.expect(3);
+    assert.expect(4);
     instances.user.update({ accountIds: [] });
-    await visit(urls.accounts);
-    assert.strictEqual(findAll('tbody tr').length, 0);
-    await click('.rose-layout-page-actions a');
+    await visit(urls.user);
+
+    await click(`[href="${urls.accounts}"]`);
+    assert.dom(TABLE_ROWS_SELECTOR).exists({ count: 0 });
+    await click(ADD_ACCOUNTS_ACTION_SELECTOR);
     assert.strictEqual(currentURL(), urls.addAccounts);
     // Click three times to select, unselect, then reselect (for coverage)
-    await click('tbody label');
-    await click('tbody label');
-    await click('tbody label');
-    await click('form [type="submit"]');
+    await click(CHECKBOX_SELECTOR);
+    await click(CHECKBOX_SELECTOR);
+    await click(CHECKBOX_SELECTOR);
+    await click(SUBMIT_BTN_SELECTOR);
     await visit(urls.accounts);
-    assert.strictEqual(findAll('tbody tr').length, 1);
+
+    assert.dom(TABLE_ROWS_SELECTOR).exists({ count: 1 });
+    assert.strictEqual(instances.user.accountIds.length, 1);
   });
 
   test('select and cancel accounts to add', async function (assert) {
     assert.expect(4);
-    await visit(urls.accounts);
-    assert.strictEqual(findAll('tbody tr').length, accountsCount);
-    await click('tbody tr .rose-dropdown-button-danger');
-    assert.strictEqual(findAll('tbody tr').length, accountsCount - 1);
-    await click('.rose-layout-page-actions a');
+    await visit(urls.user);
+
+    await click(`[href="${urls.accounts}"]`);
+    assert.dom(TABLE_ROWS_SELECTOR).exists({ count: accountsCount });
+    await click(REMOVE_ACTION_SELECTOR);
+    assert.dom(TABLE_ROWS_SELECTOR).exists({ count: accountsCount - 1 });
+    await click(ADD_ACCOUNTS_ACTION_SELECTOR);
     assert.strictEqual(currentURL(), urls.addAccounts);
-    await click('tbody label');
-    await click('form [type="button"]');
+    await click(CHECKBOX_SELECTOR);
+    await click(CANCEL_BTN_SELECTOR);
     await visit(urls.accounts);
-    assert.strictEqual(findAll('tbody tr').length, accountsCount - 1);
+    assert.dom(TABLE_ROWS_SELECTOR).exists({ count: accountsCount - 1 });
   });
 
   test('shows error message on account add', async function (assert) {
@@ -174,8 +250,8 @@ module('Acceptance | users | accounts', function (hooks) {
     });
     instances.user.update({ accountIds: [] });
     await visit(urls.addAccounts);
-    await click('tbody label');
-    await click('form [type="submit"]');
-    assert.ok(find('[role="alert"]'));
+    await click(CHECKBOX_SELECTOR);
+    await click(SUBMIT_BTN_SELECTOR);
+    assert.dom(ERROR_MSG_SELECTOR).isVisible();
   });
 });
