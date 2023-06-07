b01d5f0271768e3bafa4c839af2c54d4a56c42ab
Cameron Perera <cameron.perera@hashicorp.com>
2023-06-07 14:15:55 -0500

    refactor: 💡 Show View instead of Play if channel not asciicast (#325)
    * refactor: 💡 Show View instead of Play if channel not asciicast
    
    ✅ Closes: ICU-9524
    
    * refactor: 💡 every third channel is missing mime_types
    
    * refactor: 💡 create new abilities for channel-recording
    
    * refactor: 💡 use abilities for button logic for channels
    
    * test: 💍 update channel-recording ability tests
    
    * refactor: 💡 fix ChannelRecording ability import name
    
    * refactor: 💡 change ability name to canPlay from canPlayChannel
    
    * refactor: 💡 update OverrideChannelRecording ability methods

--- addons/api/mirage/factories/channel-recording.js
+++ addons/api/mirage/factories/channel-recording.js
@@ -6,6 +6,7 @@
 import factory from '../generated/factories/channel-recording';
 import generateId from '../helpers/id';
 import { faker } from '@faker-js/faker';
+import { MIME_TYPE_ASCIICAST } from 'api/models/channel-recording';
 
 export default factory.extend({
   id: () => generateId('chr_'),
@@ -14,4 +15,5 @@ export default factory.extend({
   },
   end_time: (i) => (i % 3 === 2 ? null : faker.date.recent()),
   duration: (i) => (i % 3 === 2 ? null : `${faker.datatype.number()}s`),
+  mime_types: (i) => (i % 3 === 2 ? null : [MIME_TYPE_ASCIICAST]),
 });
--- addons/api/mirage/generated/factories/channel-recording.js
+++ addons/api/mirage/generated/factories/channel-recording.js
@@ -5,7 +5,6 @@
 
 import { Factory } from 'miragejs';
 import { faker } from '@faker-js/faker';
-import { MIME_TYPE_ASCIICAST } from 'api/models/channel-recording';
 
 /**
  * GeneratedChannelRecordingModelFactory
@@ -16,5 +15,4 @@ export default Factory.extend({
   bytes_down: () =>
     faker.random.numeric(faker.datatype.number({ min: 1, max: 12 })),
   errors: () => faker.datatype.number({ max: 10 }),
-  mime_types: () => [MIME_TYPE_ASCIICAST],
 });
