1732da21fa2c712f9d73362cc2727f81fbe592fa
Devon Anderson <devon.anderson@hashicorp.com>
2023-07-13 13:23:57 -0400

    Update auth-method.js
    removed curly braces from values

--- addons/api/mirage/generated/factories/auth-method.js
+++ addons/api/mirage/generated/factories/auth-method.js
@@ -65,10 +65,10 @@ export default Factory.extend({
           anon_group_search: false,
           user_dn: 'dc=example,dc=com',
           user_attr: 'uid',
-          user_filter: 'uid={username}',
+          user_filter: 'uid=username',
           group_dn: 'dc=example,dc=com',
           group_attr: 'cn',
-          group_filter: 'member={dn}',
+          group_filter: 'member=dn',
           account_attribute_maps: [
             'preferredName=fullName',
             'preferredEmail=email',
