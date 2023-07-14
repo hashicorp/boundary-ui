56df65bf8e60f8481483932d5bc4ca067311e66b
Devon Anderson <devon.anderson@hashicorp.com>
2023-07-14 13:12:27 -0400

    refactor: 💡 refactor managed grouped mirage data for LDAP (#1775)
    * refactor: 💡 refactor managed grouped mirage data for LDAP
    
    ✅ Closes: ICU-10055
    
    * refactor: 💡 refactor mirage data factor
    
    Refactor mirage data factory for managed-groups
    
    * Update addons/api/mirage/factories/managed-group.js
    
    Co-authored-by: Zhihe Li <zhihe.li@hashicorp.com>
    
    ---------
    
    Co-authored-by: Zhihe Li <zhihe.li@hashicorp.com>

--- addons/api/mirage/factories/managed-group.js
+++ addons/api/mirage/factories/managed-group.js
@@ -7,12 +7,13 @@ import factory from '../generated/factories/managed-group';
 import { faker } from '@faker-js/faker';
 import permissions from '../helpers/permissions';
 import generateId from '../helpers/id';
-
-const types = ['oidc'];
+import {
+  TYPE_AUTH_METHOD_OIDC,
+  TYPE_AUTH_METHOD_LDAP,
+  TYPES_AUTH_METHOD,
+} from 'api/models/auth-method';
 
 export default factory.extend({
-  type: (i) => types[i % types.length],
-
   authorized_actions: () =>
     permissions.authorizedActionsFor('managed-group') || [
       'no-op',
@@ -23,12 +24,24 @@ export default factory.extend({
 
   id: () => generateId('mg_'),
 
+  type: (i) => TYPES_AUTH_METHOD[i % TYPES_AUTH_METHOD.length],
+
   attributes() {
     switch (this.type) {
-      case 'oidc':
+      case TYPE_AUTH_METHOD_OIDC:
         return {
           filter: faker.random.words(),
         };
+      case TYPE_AUTH_METHOD_LDAP: {
+        const num = faker.datatype.number({ min: 2, max: 4 });
+        const names = [];
+        for (let i = 0; i < num; i++) {
+          names.push(faker.random.word());
+        }
+        return {
+          group_names: names,
+        };
+      }
     }
   },
 });
