2cbfe622a81fe55224179285489e1d6dd9c9802e
dvonanderson <devon.anderson@hashicorp.com>
2023-07-18 12:08:57 -0400

    refactor: 💡 refactor test and serializer
    Refactor `auth-method` mirage serializer to delete secret attributes.
    Refactor `auth-method` mirage serializer unit test for LDAP
    
    ✅ Closes: ICU-10025

--- addons/api/tests/unit/serializers/auth-method-test.js
+++ addons/api/tests/unit/serializers/auth-method-test.js
@@ -10,6 +10,7 @@ import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import {
   TYPE_AUTH_METHOD_PASSWORD,
   TYPE_AUTH_METHOD_OIDC,
+  TYPE_AUTH_METHOD_LDAP,
 } from 'api/models/auth-method';
 
 module('Unit | Serializer | auth method', function (hooks) {
@@ -89,7 +90,66 @@ module('Unit | Serializer | auth method', function (hooks) {
     });
   });
 
-  test("it doesn't serialize client secrets when not set", function (assert) {
+  test('it serializes LDAP records without state', function (assert) {
+    assert.expect(1);
+    let store = this.owner.lookup('service:store');
+    let record = store.createRecord('auth-method', {
+      type: TYPE_AUTH_METHOD_LDAP,
+      name: 'LDAP Auth Method',
+      state: 'baz',
+      account_attribute_maps: [{ from: 'foo', to: 'bar' }],
+      urls: [{ value: 'ldap://ldap.example.com' }],
+      start_tls: true,
+      insecrure_tls: false,
+      discover_dn: true,
+      anon_group_search: true,
+      bind_dn: 'cn=users,dc=example,dc=com',
+      upn_domain: 'example.com',
+      user_dn: 'cn=users,dc=example,dc=com',
+      user_attr: 'uid',
+      user_filter: '({{.UserAttr}}={{.Username}})',
+      enable_groups: true,
+      group_dn: 'cn=groups,dc=example,dc=com',
+      group_attr: 'cn',
+      group_filter: '(member={{.UserDN}})',
+      certificates: [
+        { value: 'certificate-1234' },
+        { value: 'certificate-5678' },
+      ],
+      client_certificate: 'certificate-1234',
+      client_certificate_key: 'secret456',
+    });
+
+    let serializedRecord = record.serialize();
+
+    assert.deepEqual(serializedRecord, {
+      type: TYPE_AUTH_METHOD_LDAP,
+      name: 'LDAP Auth Method',
+      description: null,
+      attributes: {
+        account_attribute_maps: ['foo=bar'],
+        urls: ['ldap://ldap.example.com'],
+        start_tls: true,
+        insecure_tls: false,
+        discover_dn: true,
+        anon_group_search: true,
+        bind_dn: 'cn=users,dc=example,dc=com',
+        upn_domain: 'example.com',
+        user_dn: 'cn=users,dc=example,dc=com',
+        user_attr: 'uid',
+        user_filter: '({{.UserAttr}}={{.Username}})',
+        enable_groups: true,
+        group_dn: 'cn=groups,dc=example,dc=com',
+        group_attr: 'cn',
+        group_filter: '(member={{.UserDN}})',
+        certificates: ['certificate-1234', 'certificate-5678'],
+        client_certificate: 'certificate-1234',
+        client_certificate_key: 'secret456',
+      },
+    });
+  });
+
+  test("OIDC auth-method doesn't serialize client secrets when not set", function (assert) {
     assert.expect(1);
     let store = this.owner.lookup('service:store');
     let record = store.createRecord('auth-method', {
@@ -138,12 +198,94 @@ module('Unit | Serializer | auth method', function (hooks) {
     });
   });
 
+  test("LDAP auth-method doesn't serialize client secrets when not set", function (assert) {
+    assert.expect(1);
+    let store = this.owner.lookup('service:store');
+    let record = store.createRecord('auth-method', {
+      type: TYPE_AUTH_METHOD_LDAP,
+      name: 'LDAP Auth Method',
+      state: 'baz',
+      account_attribute_maps: [{ from: 'foo', to: 'bar' }],
+      urls: [{ value: 'ldap://ldap.example.com' }],
+      start_tls: true,
+      insecrure_tls: false,
+      discover_dn: true,
+      anon_group_search: true,
+      bind_dn: 'cn=users,dc=example,dc=com',
+      upn_domain: 'example.com',
+      user_dn: 'cn=users,dc=example,dc=com',
+      user_attr: 'uid',
+      user_filter: '({{.UserAttr}}={{.Username}})',
+      enable_groups: true,
+      group_dn: 'cn=groups,dc=example,dc=com',
+      group_attr: 'cn',
+      group_filter: '(member={{.UserDN}})',
+      certificates: [
+        { value: 'certificate-1234' },
+        { value: 'certificate-5678' },
+      ],
+      client_certificate: 'certificate-1234',
+      client_certificate_key: null,
+    });
+
+    let serializedRecord = record.serialize();
+
+    assert.deepEqual(serializedRecord, {
+      type: TYPE_AUTH_METHOD_LDAP,
+      name: 'LDAP Auth Method',
+      description: null,
+      attributes: {
+        account_attribute_maps: ['foo=bar'],
+        urls: ['ldap://ldap.example.com'],
+        start_tls: true,
+        insecure_tls: false,
+        discover_dn: true,
+        anon_group_search: true,
+        bind_dn: 'cn=users,dc=example,dc=com',
+        upn_domain: 'example.com',
+        user_dn: 'cn=users,dc=example,dc=com',
+        user_attr: 'uid',
+        user_filter: '({{.UserAttr}}={{.Username}})',
+        enable_groups: true,
+        group_dn: 'cn=groups,dc=example,dc=com',
+        group_attr: 'cn',
+        group_filter: '(member={{.UserDN}})',
+        certificates: ['certificate-1234', 'certificate-5678'],
+        client_certificate: 'certificate-1234',
+      },
+    });
+  });
+
   test('it serializes OIDC records with only state and version when `adapterOptions.state` is passed', function (assert) {
     assert.expect(1);
     const store = this.owner.lookup('service:store');
     const serializer = store.serializerFor('auth-method');
     const record = store.createRecord('auth-method', {
       type: TYPE_AUTH_METHOD_OIDC,
+      attributes: {
+        state: 'foo',
+      },
+      version: 1,
+    });
+    const snapshot = record._createSnapshot();
+    snapshot.adapterOptions = {
+      state: 'bar',
+    };
+    const serializedRecord = serializer.serialize(snapshot);
+    assert.deepEqual(serializedRecord, {
+      attributes: {
+        state: 'bar',
+      },
+      version: 1,
+    });
+  });
+
+  test('it serializes LDAP records with only state and version when `adapterOptions.state` is passed', function (assert) {
+    assert.expect(1);
+    const store = this.owner.lookup('service:store');
+    const serializer = store.serializerFor('auth-method');
+    const record = store.createRecord('auth-method', {
+      type: TYPE_AUTH_METHOD_LDAP,
       attributes: {
         state: 'foo',
       },
@@ -270,5 +412,72 @@ module('Unit | Serializer | auth method', function (hooks) {
     assert.strictEqual(record.dry_run, dryRun);
     assert.strictEqual(record.issuer, issuer);
     assert.strictEqual(record.max_age, maxAge);
+  });
+
+  test('it normalizes LDAP records', async function (assert) {
+    const store = this.owner.lookup('service:store');
+
+    const ldapUrl = 'ldap://ldap.example.com';
+    const certificates = ['certificate-1234', 'certificate-5678'];
+    const clientCerticate = 'certificate-1234';
+    const clientCertificateKey = 'secret456';
+    const startTls = true;
+    const insecureTls = false;
+    this.server.get('/v1/auth-methods/ldap123', () => ({
+      attributes: {
+        account_attribute_maps: ['from=to', 'foo=bar'],
+        urls: [ldapUrl],
+        start_tls: startTls,
+        insecure_tls: insecureTls,
+        discover_dn: true,
+        anon_group_search: true,
+        bind_dn: 'cn=users,dc=example,dc=com',
+        upn_domain: 'example.com',
+        user_dn: 'cn=users,dc=example,dc=com',
+        user_attr: 'uid',
+        user_filter: '({{.UserAttr}}={{.Username}})',
+        enable_groups: true,
+        group_dn: 'cn=groups,dc=example,dc=com',
+        group_attr: 'cn',
+        group_filter: '(member={{.UserDN}})',
+        certificates: certificates,
+        client_certificate: clientCerticate,
+        client_certificate_key: clientCertificateKey,
+      },
+      version: 1,
+      type: TYPE_AUTH_METHOD_LDAP,
+      id: 'ldap123',
+    }));
+
+    const record = await store.findRecord('auth-method', 'ldap123');
+    const {
+      account_attribute_maps,
+      urls,
+      certificates: recordCertificates,
+    } = record;
+    assert.deepEqual(account_attribute_maps, [
+      { from: 'from', to: 'to' },
+      { from: 'foo', to: 'bar' },
+    ]);
+    assert.deepEqual(urls, [{ value: ldapUrl }]);
+    assert.deepEqual(recordCertificates, [
+      { value: 'certificate-1234' },
+      { value: 'certificate-5678' },
+    ]);
+    assert.strictEqual(record.start_tls, startTls);
+    assert.strictEqual(record.insecure_tls, insecureTls);
+    assert.true(record.discover_dn);
+    assert.true(record.anon_group_search);
+    assert.strictEqual(record.bind_dn, 'cn=users,dc=example,dc=com');
+    assert.strictEqual(record.upn_domain, 'example.com');
+    assert.strictEqual(record.user_dn, 'cn=users,dc=example,dc=com');
+    assert.strictEqual(record.user_attr, 'uid');
+    assert.strictEqual(record.user_filter, '({{.UserAttr}}={{.Username}})');
+    assert.true(record.enable_groups);
+    assert.strictEqual(record.group_dn, 'cn=groups,dc=example,dc=com');
+    assert.strictEqual(record.group_attr, 'cn');
+    assert.strictEqual(record.group_filter, '(member={{.UserDN}})');
+    assert.strictEqual(record.client_certificate, clientCerticate);
+    assert.strictEqual(record.client_certificate_key, clientCertificateKey);
   });
 });
