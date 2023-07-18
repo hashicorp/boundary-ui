a67290d3e2999e987efb67341b7eee5bf837ed17
dvonanderson <devon.anderson@hashicorp.com>
2023-07-18 16:29:17 -0400

    refactor: 💡 merge main
    merge main

--- ui/admin/tests/acceptance/auth-methods/create-test.js
+++ ui/admin/tests/acceptance/auth-methods/create-test.js
@@ -127,7 +127,6 @@ module('Acceptance | auth-methods | create', function (hooks) {
     assert.strictEqual(authMethod.description, 'description');
     assert.strictEqual(authMethod.attributes.issuer, 'issuer');
     assert.strictEqual(authMethod.attributes.client_id, 'client_id');
-    assert.strictEqual(authMethod.attributes.client_secret, 'client_secret');
     assert.deepEqual(authMethod.attributes.signing_algorithms, ['RS384']);
     assert.deepEqual(authMethod.attributes.allowed_audiences, [
       'allowed_audiences',
