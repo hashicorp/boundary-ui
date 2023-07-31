1fc7f52c27feef311171926e7ebdc4338ea25f5d
cameronperera <cameron.perera@hashicorp.com>
2023-07-31 14:15:14 -0500

    refactor: 💡 Remove targets/target-id/hosts route and tests
    ✅ Closes: ICU-10316

--- ui/desktop/app/router.js
+++ ui/desktop/app/router.js
@@ -22,7 +22,6 @@ Router.map(function () {
       this.route('projects', function () {
         this.route('targets', function () {
           this.route('target', { path: ':target_id' }, function () {
-            this.route('hosts');
             this.route('sessions');
           });
         });
