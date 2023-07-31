6dee7cad620c6e58ea0c05a7e550085818760f25
Cameron Perera <cameron.perera@hashicorp.com>
2023-07-31 11:48:38 -0500

    Icu 10439 refactor layout sidebar into rose layout body content (#1807)
    * feat: 🎸 Add BodyContent component to Rose::Layout
    
    * refactor: 💡 remove Layout Sidebar and use Rose Layout Sidebar
    
    ✅ Closes: ICU-10439
    
    * fix: 🐛 add media specific styles to rose body content styles
    
    * refactor: 💡 update media rule max width to xlarge

---
+++ addons/rose/tests/integration/components/rose/layout/body-content-test.js
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | rose/layout/body-content', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+    await render(hbs`<Rose::Layout::BodyContent />`);
+
+    assert.dom('div').exists();
+    assert.dom('.rose-layout-body-content').isVisible();
+  });
+
+  test('it renders with attributes', async function (assert) {
+    await render(hbs`<Rose::Layout::BodyContent id="body-content"/>`);
+
+    assert.dom('#body-content').isVisible();
+  });
+
+  test('it renders with content', async function (assert) {
+    await render(hbs`<Rose::Layout::BodyContent as |bc|>
+      <bc.Body><button id="button" /></bc.Body>
+      <bc.Sidebar><p id="details">Resource Details</p></bc.Sidebar>
+    </Rose::Layout::BodyContent>`);
+
+    assert.dom('.rose-layout-body-content-body #button').isVisible();
+    assert.dom('.rose-layout-body-content-sidebar #details').isVisible();
+    assert
+      .dom('.rose-layout-body-content-sidebar #details')
+      .hasText('Resource Details');
+  });
+});
