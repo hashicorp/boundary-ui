9e0546635618d58da4fc4b0f1abd1a1bdc3526f9
Cameron Perera <cameron.perera@hashicorp.com>
2023-08-04 13:43:53 -0500

    Icu 10491 create route for session/session-id (#1825)
    * fix: 🐛 call displayName on model
    
    * feat: 🎸 add sessions/session-id route and base layout
    
    ✅ Closes: ICU-10491

---
+++ ui/desktop/app/routes/scopes/scope/projects/sessions/session.js
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+
+export default class ScopesScopeProjectsSessionsSessionRoute extends Route {
+  // =services
+
+  @service store;
+
+  // =methods
+
+  /**
+   * Load a session
+   * @param {object} params
+   * @param {string} params.session_id
+   * @return {TargetModel}
+   */
+  model({ session_id }) {
+    return this.store.findRecord('session', session_id, { reload: true });
+  }
+}
