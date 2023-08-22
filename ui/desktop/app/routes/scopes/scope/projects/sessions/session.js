bf0d3f6100ecb13f222c5ee82e937eec1fe6eb5d
cameronperera <cameron.perera@hashicorp.com>
2023-08-22 14:31:39 -0500

    refactor: 💡 add comment to resetController on session route

--- ui/desktop/app/routes/scopes/scope/projects/sessions/session.js
+++ ui/desktop/app/routes/scopes/scope/projects/sessions/session.js
@@ -12,6 +12,15 @@ export default class ScopesScopeProjectsSessionsSessionRoute extends Route {
   @service store;
 
   // =methods
+  /**
+   * Controllers are singletons and all state maintained by
+   * the controller will persist even if the user transitions
+   * away from the route. This will reset "isRawApiVisible"
+   * back to false when a user leaves this route.
+   * @param {*} controller
+   * @param {*} isExiting
+   * @param {*} transition
+   */
   resetController(controller, isExiting, transition) {
     if (isExiting && transition.targetName !== 'error') {
       controller.set('isRawApiVisible', false);
