d6074b33f92fb809c98935765ebcf7ac8319d9e5
cameronperera <cameron.perera@hashicorp.com>
2023-08-23 15:00:47 -0500

    feat: 🎸 create CredentialsPanel component

--- ui/desktop/app/components/credentials-panel/index.js
+++ ui/desktop/app/components/credentials-panel/index.js
@@ -1,8 +1,13 @@
-import Controller from '@ember/controller';
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
-export default class ScopesScopeProjectsSessionsSessionController extends Controller {
+export default class CredentialsPanelIndexComponent extends Component {
   @tracked isRawApiVisible = false;
 
   @action
