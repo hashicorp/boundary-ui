fe2d7c4284d51e3bafed41182f3149f92c7ff1de
cameronperera <cameron.perera@hashicorp.com>
2023-08-29 12:30:27 -0500

    refactor: 💡 display proxy info based on target type

--- ui/desktop/app/components/proxy-url/index.js
+++ ui/desktop/app/components/proxy-url/index.js
@@ -7,13 +7,17 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { TYPE_TARGET_SSH } from 'api/models/target';
 
 export default class ProxyUrlComponent extends Component {
   @service intl;
 
-  @tracked selectedValue = this.intl.t('proxy-url.options.address-port');
+  @tracked selectedValue =
+    this.args.targetType === TYPE_TARGET_SSH
+      ? this.intl.t('proxy-url.options.ssh')
+      : this.intl.t('proxy-url.options.address-port');
 
-  get isDefault() {
+  get isAddressAndPort() {
     return this.selectedValue === this.intl.t('proxy-url.options.address-port');
   }
 
--- ui/desktop/tests/integration/components/proxy-url-test.js
+++ ui/desktop/tests/integration/components/proxy-url-test.js
@@ -8,6 +8,7 @@ import { setupRenderingTest } from 'desktop/tests/helpers';
 import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import { setupIntl } from 'ember-intl/test-support';
+import { TYPE_TARGET_TCP, TYPE_TARGET_SSH } from 'api/models/target';
 
 module('Integration | Component | proxy-url', function (hooks) {
   setupRenderingTest(hooks);
@@ -17,9 +18,16 @@ module('Integration | Component | proxy-url', function (hooks) {
     assert.expect(2);
     this.set('proxyAddress', 'http://localhost');
     this.set('proxyPort', '1234');
+    this.set('targetType', TYPE_TARGET_TCP);
 
     await render(
-      hbs`<ProxyUrl @proxyAddress={{this.proxyAddress}} @proxyPort={{this.proxyPort}} />`
+      hbs`
+        <ProxyUrl
+          @proxyAddress={{this.proxyAddress}}
+          @proxyPort={{this.proxyPort}}
+          @targetType={{this.targetType}}
+        />
+      `
     );
 
     assert.dom('.copy-address').hasText(this.proxyAddress);
@@ -30,12 +38,61 @@ module('Integration | Component | proxy-url', function (hooks) {
     assert.expect(2);
     this.set('proxyAddress', 'http://localhost');
     this.set('proxyPort', '1234');
+    this.set('targetType', TYPE_TARGET_TCP);
 
     await render(
-      hbs`<ProxyUrl @proxyAddress={{this.proxyAddress}} @proxyPort={{this.proxyPort}} />`
+      hbs`
+        <ProxyUrl
+          @proxyAddress={{this.proxyAddress}}
+          @proxyPort={{this.proxyPort}}
+          @targetType={{this.targetType}}
+        />
+      `
     );
     await click('.proxy-url-container .hds-dropdown-toggle-button');
     await click('[data-test-ssh-option]');
+
+    assert.dom('.copy-ssh-command').isVisible();
+    assert
+      .dom('.copy-ssh-command')
+      .hasText(`ssh ${this.proxyAddress} -p ${this.proxyPort}`);
+  });
+
+  test('shows address & port info when target type is TCP', async function (assert) {
+    assert.expect(2);
+    this.set('proxyAddress', 'http://localhost');
+    this.set('proxyPort', '1234');
+    this.set('targetType', TYPE_TARGET_TCP);
+
+    await render(
+      hbs`
+        <ProxyUrl
+          @proxyAddress={{this.proxyAddress}}
+          @proxyPort={{this.proxyPort}}
+          @targetType={{this.targetType}}
+        />
+      `
+    );
+
+    assert.dom('.copy-address').hasText(this.proxyAddress);
+    assert.dom('.copy-port').hasText(this.proxyPort);
+  });
+
+  test('shows ssh command when target type is SSH', async function (assert) {
+    assert.expect(2);
+    this.set('proxyAddress', 'http://localhost');
+    this.set('proxyPort', '1234');
+    this.set('targetType', TYPE_TARGET_SSH);
+
+    await render(
+      hbs`
+        <ProxyUrl
+          @proxyAddress={{this.proxyAddress}}
+          @proxyPort={{this.proxyPort}}
+          @targetType={{this.targetType}}
+        />
+      `
+    );
 
     assert.dom('.copy-ssh-command').isVisible();
     assert
