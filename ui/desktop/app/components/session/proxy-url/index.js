bcc3c8c032950aa668849e05d94dfeccec2c35f6
cameronperera <cameron.perera@hashicorp.com>
2023-08-30 11:25:48 -0500

    refactor: 💡 update how proxy-url determins default proxy info

--- ui/desktop/app/components/session/proxy-url/index.js
+++ ui/desktop/app/components/session/proxy-url/index.js
@@ -7,15 +7,13 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-import { TYPE_TARGET_SSH } from 'api/models/target';
 
 export default class SessionProxyUrlComponent extends Component {
   @service intl;
 
-  @tracked selectedValue =
-    this.args.targetType === TYPE_TARGET_SSH
-      ? this.intl.t('resources.session.proxy-url.ssh')
-      : this.intl.t('resources.session.proxy-url.address-port');
+  @tracked selectedValue = this.args.isSSHTarget
+    ? this.intl.t('resources.session.proxy-url.ssh')
+    : this.intl.t('resources.session.proxy-url.address-port');
 
   get isAddressAndPort() {
     return (
