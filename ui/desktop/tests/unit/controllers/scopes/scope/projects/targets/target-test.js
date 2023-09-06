1a54dbfcd77bfb4c54bbd28ace744f9fb28cc1fc
Cameron Perera <cameron.perera@hashicorp.com>
2023-09-06 10:56:08 -0500

    feat: 🎸 update connect logic for targets with static addresses (#1874)
    * feat: 🎸 update connect logic for targets with static addresses
    
    ✅ Closes: ICU-10430
    
    * refactor: 💡 remove console logs
    
    * refactor: 💡 add missing awaits

---
+++ ui/desktop/tests/unit/controllers/scopes/scope/projects/targets/target-test.js
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import { module, test } from 'qunit';
+import { setupTest } from 'desktop/tests/helpers';
+
+module(
+  'Unit | Controller | scopes/scope/projects/targets/target',
+  function (hooks) {
+    setupTest(hooks);
+
+    test('it exists', function (assert) {
+      let controller = this.owner.lookup(
+        'controller:scopes/scope/projects/targets/target'
+      );
+      assert.ok(controller);
+    });
+  }
+);
