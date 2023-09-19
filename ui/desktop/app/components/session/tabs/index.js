fa34958dcf2deebfd4240b0c8f361c3c7cbf08a1
Zhihe Li <zhihe.li@hashicorp.com>
2023-09-19 16:47:20 -0400

    feat: 🎸 Prevent automatically sending SSH command on windows
    ✅ Closes: ICU-11109

--- ui/desktop/app/components/session/tabs/index.js
+++ ui/desktop/app/components/session/tabs/index.js
@@ -1,5 +1,6 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 import { Terminal } from 'xterm';
 import { FitAddon } from 'xterm-addon-fit';
 import { v4 as uuidv4 } from 'uuid';
@@ -10,6 +11,10 @@ const terminalOptions = {
 };
 
 export default class SessionTerminalTabsComponent extends Component {
+  // =services
+
+  @service ipc;
+
   // =attributes
 
   id;
@@ -19,7 +24,7 @@ export default class SessionTerminalTabsComponent extends Component {
   // =actions
 
   @action
-  openTerminal() {
+  async openTerminal() {
     // Don't re-initialize the terminal if it's already been opened
     if (this.terminal) {
       return;
@@ -39,8 +44,11 @@ export default class SessionTerminalTabsComponent extends Component {
     this.id = uuidv4();
     this.#setupTerminal(fitAddon, xterm);
 
+    const { isWindows } = await this.ipc.invoke('checkOS');
     const { model } = this.args;
-    if (model.target?.isSSH) {
+
+    // Don't send the command on windows as most users won't have an openSSH client installed
+    if (model.target?.isSSH && !isWindows) {
       const { proxy_address, proxy_port } = model;
 
       // Send an SSH command immediately
@@ -69,13 +77,14 @@ export default class SessionTerminalTabsComponent extends Component {
     // Terminal is exposed by contextBridge within the preload script
     window.terminal.create({ id: this.id, cols: xterm.cols, rows: xterm.rows });
     xterm.onData((data) => window.terminal.send(data, this.id));
+
+    // Save the handler to cleanup the listener on the renderer process later
     this.removeListener = window.terminal.receive((event, value) => {
       xterm.write(value);
     }, this.id);
 
-    // Handle resizing terminal windows. We debounce the resizing as we
-    // don't want to resize xterm and the pty process before the previous
-    // one has finished
+    // Handle resizing terminal windows. We debounce the resizing as we don't want
+    // to resize xterm and the pty process before the previous one has finished
     const debouncedFit = debounce(() => {
       fitAddon.fit();
     }, 150);
