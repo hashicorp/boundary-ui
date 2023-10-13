d10382dbabe8ac235d63706e0408aedf2652da4c
Lisbet Alvarez <lisbet.alvarez@hashicorp.com>
2023-10-13 10:24:34 -0700

    test: 💍 update service unit test

--- ui/admin/tests/unit/services/breadcrumbs-test.js
+++ ui/admin/tests/unit/services/breadcrumbs-test.js
@@ -4,9 +4,30 @@ import { setupTest } from 'admin/tests/helpers';
 module('Unit | Service | breadcrumbs', function (hooks) {
   setupTest(hooks);
 
-  // TODO: Replace this with your real tests.
   test('it exists', function (assert) {
-    let service = this.owner.lookup('service:breadcrumbs');
+    assert.expect(1);
+    const service = this.owner.lookup('service:breadcrumbs');
     assert.ok(service);
+  });
+
+  test('registerContainer properly adds a new container', function (assert) {
+    assert.expect(1);
+    const container = { element: 'element' };
+    const service = this.owner.lookup('service:breadcrumbs');
+
+    service.registerContainer(container);
+
+    assert.strictEqual(service.containers[0].element, container.element);
+  });
+
+  test('unregisterContainer properly removes an existing container', function (assert) {
+    assert.expect(1);
+    const container = { element: 'element' };
+    const service = this.owner.lookup('service:breadcrumbs');
+
+    service.registerContainer(container);
+    service.unregisterContainer(container);
+
+    assert.strictEqual(service.containers.length, 0);
   });
 });
