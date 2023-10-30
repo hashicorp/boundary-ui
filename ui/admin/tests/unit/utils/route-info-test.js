05bd5092386ae81fece27c30cef13a763aa04da1
Lisbet Alvarez <lisbet.alvarez@hashicorp.com>
2023-10-30 16:23:22 -0700

    test: 💍 add test cases and rename util file

--- ui/admin/tests/unit/utils/route-info-test.js
+++
@@ -1,9 +0,0 @@
-import { paramValueFinder } from 'admin/utils/route-info';
-import { module, test } from 'qunit';
-
-module('Unit | Utility | route-info', function () {
-  test('it works', function (assert) {
-    let result = paramValueFinder('scope', null);
-    assert.strictEqual(result.length, 0);
-  });
-});
