b80f92ecb77a5dce8fadfdf3ccccd5c1030c7019
Devon Anderson <devon.anderson@hashicorp.com>
2023-10-31 14:07:06 -0400

    Update ember-try.js
    change scenarios to match 4.12.2

--- addons/rose/tests/dummy/config/ember-try.js
+++ addons/rose/tests/dummy/config/ember-try.js
@@ -10,20 +10,20 @@ const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
 
 module.exports = async function () {
   return {
-    scenarios: [
-      {
-        name: 'ember-lts-4.4',
-        npm: {
-          devDependencies: {
-            'ember-source': '~4.4.0',
-          },
-        },
-      },
+   scenarios: [
       {
         name: 'ember-lts-4.8',
         npm: {
           devDependencies: {
             'ember-source': '~4.8.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-4.12',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.12.0',
           },
         },
       },
