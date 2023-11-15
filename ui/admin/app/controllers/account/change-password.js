4205a42dbec8c411feae1751c49e087b53657edd
Kristin Bradley <kristin.bradley@hashicorp.com>
2023-11-15 15:06:13 -0800

    HDS-2805 Add dark and light mode functionality to AppFooter components

---
+++ ui/admin/app/controllers/account/change-password.js
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+
+export default class AccountChangePasswordController extends Controller {
+  // =services
+
+  @service session;
+}
