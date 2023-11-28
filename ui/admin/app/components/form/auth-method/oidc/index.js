9a35e46110168a16c6faad1fcc7ebe4cb7cf8674
Dharini Jeeva <dharini@hashicorp.com>
2023-11-28 09:27:10 -0800

    Update ui/admin/app/components/form/auth-method/oidc/index.js
    Co-authored-by: Zhihe Li <zhihe.li@hashicorp.com>

--- ui/admin/app/components/form/auth-method/oidc/index.js
+++ ui/admin/app/components/form/auth-method/oidc/index.js
@@ -52,14 +52,14 @@ export default class FormAuthMethodOidcComponent extends Component {
    */
   @action
   updatePrompt(value, event) {
-    const currentSelection = [{ value }];
+    const currentSelection = { value };
     const previousSelection = this.args.model.prompts || [];
 
-    //add the selected option to the model, only when the checkbox is in checked state
+    // Add the selected option to the model, only when the checkbox is in checked state
     if (event.target.checked) {
-      this.args.model.prompts = [...previousSelection, ...currentSelection];
+      this.args.model.prompts = [...previousSelection, currentSelection];
     } else {
-      //when the checkbox is unchecked, remove the option form the exisiting list
+      // When the checkbox is unchecked, remove the option form the exisiting list
       const removeSelection = previousSelection.filter(
         (prompt) => prompt.value !== value,
       );
