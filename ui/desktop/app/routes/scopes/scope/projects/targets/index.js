96d513456a04c2f07aec948a26c0e26c2506fc2f
cameronperera <cameron.perera@hashicorp.com>
2023-12-08 14:28:28 -0600

    refactor: 💡 add replace: true to scopes query param
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-11802

--- ui/desktop/app/routes/scopes/scope/projects/targets/index.js
+++ ui/desktop/app/routes/scopes/scope/projects/targets/index.js
@@ -25,6 +25,7 @@ export default class ScopesScopeProjectsTargetsIndexRoute extends Route {
     },
     scopes: {
       refreshModel: true,
+      replace: true,
     },
     page: {
       refreshModel: true,
