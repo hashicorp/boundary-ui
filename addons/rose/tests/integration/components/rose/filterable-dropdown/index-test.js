253def78bd348eaa2ea49ef74fb9f72f4dcffbf7
cameronperera <cameron.perera@hashicorp.com>
2023-12-12 15:13:30 -0600

    refactor: 💡 render Rose::FilterableDropdown correctly in test
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-11802

--- addons/rose/tests/integration/components/rose/filterable-dropdown/index-test.js
+++ addons/rose/tests/integration/components/rose/filterable-dropdown/index-test.js
@@ -12,18 +12,18 @@ module(
       // Set any properties with this.set('myProperty', 'value');
       // Handle any actions with this.set('myAction', function(val) { ... });
 
-      await render(hbs`<Rose::FilterableDropdown::Index />`);
+      await render(hbs`<Rose::FilterableDropdown />`);
 
       assert.dom(this.element).hasText('');
 
       // Template block usage:
       await render(hbs`
-      <Rose::FilterableDropdown::Index>
+      <Rose::FilterableDropdown>
         template block text
-      </Rose::FilterableDropdown::Index>
+      </Rose::FilterableDropdown>
     `);
 
       assert.dom(this.element).hasText('template block text');
     });
-  }
+  },
 );
