7ecb9fbea0e2cf118185639bf5e980374d2656e2
cameronperera <cameron.perera@hashicorp.com>
2023-12-12 15:41:26 -0600

    test: 💍 remove generated comments from tests
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-11802

--- addons/rose/tests/integration/components/rose/filterable-dropdown/index-test.js
+++ addons/rose/tests/integration/components/rose/filterable-dropdown/index-test.js
@@ -9,14 +9,10 @@ module(
     setupRenderingTest(hooks);
 
     test('it renders', async function (assert) {
-      // Set any properties with this.set('myProperty', 'value');
-      // Handle any actions with this.set('myAction', function(val) { ... });
-
       await render(hbs`<Rose::FilterableDropdown />`);
 
       assert.dom(this.element).hasText('');
 
-      // Template block usage:
       await render(hbs`
       <Rose::FilterableDropdown>
         template block text
