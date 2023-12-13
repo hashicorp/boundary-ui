b0fd03b1e38a68a6f578e38dbade2b3cad191112
cameronperera <cameron.perera@hashicorp.com>
2023-12-13 11:16:02 -0600

    feat: 🎸 generate new dropdown component in core
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12060

---
+++ addons/core/tests/integration/components/dropdown/index-test.js
@@ -0,0 +1,24 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'dummy/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | dropdown/index', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`<Dropdown />`);
+
+    assert.dom(this.element).hasText('');
+
+    // Template block usage:
+    await render(hbs`<Dropdown>
+  template block text
+</Dropdown>`);
+
+    assert.dom(this.element).hasText('template block text');
+  });
+});
