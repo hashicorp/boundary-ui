1ab77272572564ffd84063256214e2f6c0feb274
Lisbet Alvarez <107949262+lisbet-alvarez@users.noreply.github.com>
2023-12-12 17:38:18 -0800

    Icu 11915 add session in progress field to the table and populate it (#2028)
    * initial support for sessions in targets list view

--- ui/desktop/app/routes/scopes/scope/projects/targets/index.js
+++ ui/desktop/app/routes/scopes/scope/projects/targets/index.js
@@ -5,6 +5,11 @@
 
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import orderBy from 'lodash/orderBy';
+import {
+  STATUS_SESSION_ACTIVE,
+  STATUS_SESSION_PENDING,
+} from 'api/models/session';
 
 export default class ScopesScopeProjectsTargetsIndexRoute extends Route {
   // =services
@@ -73,6 +78,24 @@ export default class ScopesScopeProjectsTargetsIndexRoute extends Route {
       this.can.can('connect target', target),
     );
 
+    // Retrieve all the 'active' and 'pending' sessions associated with each target.
+    let targetsWithSessions = [];
+    for (const target of targets) {
+      let sessions = await this.store.query('session', {
+        query: {
+          filters: {
+            target_id: { equals: target.id },
+            status: [
+              { equals: STATUS_SESSION_ACTIVE },
+              { equals: STATUS_SESSION_PENDING },
+            ],
+          },
+        },
+      });
+      sessions = orderBy(sessions, 'created_time', 'desc');
+      targetsWithSessions.push({ target, sessions });
+    }
+
     // Query all targets for defining filtering values
     const options = { pushToStore: false };
     let allTargets = await this.store.query('target', {}, options);
@@ -82,6 +105,6 @@ export default class ScopesScopeProjectsTargetsIndexRoute extends Route {
       target.authorized_actions.includes('authorize-session'),
     );
 
-    return { targets, projects, allTargets, totalItems };
+    return { targets: targetsWithSessions, projects, allTargets, totalItems };
   }
 }
