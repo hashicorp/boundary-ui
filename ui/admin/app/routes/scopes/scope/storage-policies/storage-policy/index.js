bfe7747fc3f3d1451b058af1efbd25dbd9cf1123
dharini <dharini@hashicorp.com>
2023-12-19 13:58:05 -0800

    feat: 🎸 route updates to storage policies
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12081

---
+++ ui/admin/app/routes/scopes/scope/storage-policies/storage-policy/index.js
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Route from '@ember/routing/route';
+
+export default class ScopesScopeStoragePoliciesStoragePolicyIndexRoute extends Route {
+  // =services
+}
