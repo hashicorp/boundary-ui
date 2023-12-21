25a04fe39c6f99c51bf7199060598f6067422cca
cameronperera <cameron.perera@hashicorp.com>
2023-12-21 11:29:28 -0600

    feat: 🎸 create filter-tags component in core
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12069

---
+++ addons/core/addon/components/filter-tags/index.js
@@ -0,0 +1,89 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+
+export default class FilterTagsIndexComponent extends Component {
+  // =services
+
+  @service router;
+
+  // =attributes
+
+  get tags() {
+    const tags = [];
+
+    for (const key in this.args.tags) {
+      if (Array.isArray(this.args.tags[key])) {
+        const names = this.args.tags[key].map((item) => item.name);
+        tags.push(...names);
+      }
+    }
+
+    return tags;
+  }
+
+  // =methods
+
+  /**
+   * Clears a single filter from queryParams for current route
+   * @param {string} filterName
+   */
+  @action
+  removeFilter(filterName) {
+    const matchedTag = this.findTagByName(filterName);
+    const queryParams = { ...this.router.currentRoute.queryParams };
+
+    for (const key in queryParams) {
+      const paramValue = queryParams[key];
+
+      if (typeof paramValue === 'string') {
+        queryParams[key] = JSON.parse(paramValue);
+      }
+    }
+
+    for (const key in matchedTag) {
+      if (queryParams[key] !== undefined && matchedTag[key]?.id) {
+        const matchedId = matchedTag[key].id;
+
+        if (Array.isArray(queryParams[key])) {
+          queryParams[key] = queryParams[key].filter(
+            (item) => item !== matchedId,
+          );
+        }
+      }
+    }
+
+    this.router.replaceWith({ queryParams });
+  }
+
+  /**
+   * Clears all filters based on queryParams from tags keys
+   */
+  @action
+  clearAllFilters() {
+    const allowList = Object.keys(this.args.tags);
+
+    const queryParams = { ...this.router.currentRoute.queryParams };
+
+    for (const key in queryParams) {
+      if (allowList.includes(key)) {
+        queryParams[key] = [];
+      }
+    }
+
+    this.router.replaceWith({ queryParams });
+  }
+
+  findTagByName(filterName) {
+    const matchedTag = {};
+
+    Object.entries(this.args.tags).forEach(([key, value]) => {
+      const foundTag = value.find((tag) => tag.name === filterName);
+      if (foundTag) {
+        matchedTag[key] = foundTag;
+      }
+    });
+
+    return matchedTag;
+  }
+}
---
+++ addons/core/tests/integration/components/filter-tags/index-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { setupIntl } from 'ember-intl/test-support';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | filter-tags/index', function (hooks) {
+  setupRenderingTest(hooks);
+  setupIntl(hooks);
+
+  test('it renders', async function (assert) {
+    this.set('tag', { scopes: [{ id: '1', name: 'Project 1' }] });
+    await render(hbs`<FilterTags @tags={{this.tag}} />`);
+
+    assert.dom('.hds-tag__text').hasText('Project 1');
+  });
+});
