ef2744f0662644e05ea85e0cc92c08023847094c
Dharini Jeeva <dharini@hashicorp.com>
2023-12-20 19:39:41 -0800

    Update new.js

--- ui/admin/app/routes/scopes/scope/policies/new.js
+++ ui/admin/app/routes/scopes/scope/policies/new.js
@@ -4,23 +4,6 @@
  */
 
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
 
 export default class ScopesScopePoliciesNewRoute extends Route {
-  // =services
-
-  @service store;
-
-  // =methods
-
-  /**
-   * Creates a new unsaved storage policy.
-   * @return {StorageBucket}
-   */
-  model() {
-    const scopeModel = this.modelFor('scopes.scope');
-    const record = this.store.createRecord('policy');
-    record.scopeModel = scopeModel;
-    return record;
-  }
 }
