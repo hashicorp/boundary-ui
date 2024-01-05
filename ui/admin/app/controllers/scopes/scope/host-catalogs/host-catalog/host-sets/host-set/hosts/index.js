2df3d1b8001d345ea93960cc539f675f461154c3
dvonanderson <devon.anderson@hashicorp.com>
2024-01-05 14:47:14 -0500

    refactor: 💡 Quick refactor for columns getter
    Quick refactor to clean up getter that conditionally renders column
    header label based on `isStatic` boolean
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-11728

--- ui/admin/app/controllers/scopes/scope/host-catalogs/host-catalog/host-sets/host-set/hosts/index.js
+++ ui/admin/app/controllers/scopes/scope/host-catalogs/host-catalog/host-sets/host-set/hosts/index.js
@@ -11,33 +11,39 @@ export default class ScopesScopeHostsHostCatalogsHostCatalogHostSetsHostSetHosts
    * @type {Array.<object>}
    */
   get columns() {
+    const commonColumns = [
+      {
+        label: this.intl.t('form.type.label'),
+      },
+      {
+        label: this.intl.t('form.id.label'),
+      },
+    ];
+
+    const staticColumns = [
+      {
+        label: this.intl.t('form.name.label'),
+      },
+      ...commonColumns,
+      {
+        label: this.intl.t('titles.actions'),
+      },
+    ];
+
+    const dynamicColumns = [
+      {
+        label: this.intl.t('form.external_name.label'),
+      },
+      ...commonColumns,
+      {
+        label: '',
+      },
+    ];
+
     if (this.model.hostSet.isStatic) {
-      return [
-        {
-          label: this.intl.t('form.name.label'),
-        },
-        {
-          label: this.intl.t('form.type.label'),
-        },
-        {
-          label: this.intl.t('form.id.label'),
-        },
-        {
-          label: this.intl.t('titles.actions'),
-        },
-      ];
+      return staticColumns;
     } else {
-      return [
-        {
-          label: this.intl.t('form.external_name.label'),
-        },
-        {
-          label: this.intl.t('form.type.label'),
-        },
-        {
-          label: this.intl.t('form.id.label'),
-        },
-      ];
+      return dynamicColumns;
     }
   }
 }
