b79eebf73a8b4ce9ab3f213217acc761f6dbc7fe
Dharini Jeeva <dharini@hashicorp.com>
2024-01-08 15:12:49 -0800

    feat:  storage policies list view (#2071)
    policies list view

---
+++ addons/core/tests/integration/helpers/format-day-year-test.js
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import { setupIntl } from 'ember-intl/test-support';
+
+module('Integration | Helper | format-day-year', function (hooks) {
+  setupRenderingTest(hooks);
+  setupIntl(hooks);
+
+  test('it renders correctly when it is 1 year', async function (assert) {
+    this.set('inputValue', 365);
+    await render(hbs`{{format-day-year this.inputValue}}`);
+    assert.strictEqual(this.element.textContent.trim(), '1 year');
+  });
+
+  test('it renders correctly when it is more than 1 year', async function (assert) {
+    this.set('inputValue', 730);
+    await render(hbs`{{format-day-year this.inputValue}}`);
+    assert.strictEqual(this.element.textContent.trim(), '2 years');
+  });
+
+  test('it renders correctly when it is not a whole year', async function (assert) {
+    this.set('inputValue', 800);
+    await render(hbs`{{format-day-year this.inputValue}}`);
+    assert.strictEqual(this.element.textContent.trim(), '800 days');
+  });
+  test('it renders when null', async function (assert) {
+    this.set('inputValue', null);
+    await render(hbs`{{format-date-iso this.inputValue}}`);
+    assert.strictEqual(this.element.textContent.trim(), '');
+  });
+});
--- ui/admin/app/routes/scopes/scope/policies/index.js
+++ ui/admin/app/routes/scopes/scope/policies/index.js
@@ -5,4 +5,12 @@
 
 import Route from '@ember/routing/route';
 
-export default class ScopesScopePoliciesIndexRoute extends Route {}
+export default class ScopesScopePoliciesIndexRoute extends Route {
+  // =methods
+
+  setupController(controller) {
+    const scope = this.modelFor('scopes.scope');
+    super.setupController(...arguments);
+    controller.setProperties({ scope });
+  }
+}
