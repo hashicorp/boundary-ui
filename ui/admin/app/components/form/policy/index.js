b5641e459972c1c36bbf6ed4a8dc163a2a1e3216
dharini <dharini@hashicorp.com>
2024-01-11 12:56:49 -0800

    refactor: 💡 move common funcs to generic comp

--- ui/admin/app/components/form/policy/index.js
+++ ui/admin/app/components/form/policy/index.js
@@ -4,8 +4,6 @@
  */
 
 import Component from '@glimmer/component';
-import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
 
 const RETENTION_POLICY = {
   forever: -1,
@@ -17,11 +15,6 @@ const DELETION_POLICY = { do_not_delete: 0, custom: 1 };
 
 export default class FormPolicyComponent extends Component {
   //attributes
-
-  @tracked retainForOverridable = this.args.model.retain_for?.overridable;
-  @tracked deleteAfterOverridable = this.args.model.delete_after?.overridable;
-  @tracked isCustomRetentionSelected = this.args.model.retain_for?.days > 0;
-  @tracked isCustomDeletionSelected = this.args.model.delete_after?.days > 0;
 
   //methods
 
@@ -66,46 +59,5 @@ export default class FormPolicyComponent extends Component {
    */
   get selectDeletePolicyType() {
     return this.args.model.delete_after?.days > 0 ? 'custom' : 'do_not_delete';
-  }
-
-  //actions
-
-  /**
-   * Show custom text field when custom option is selected
-   */
-  @action
-  handlePolicyTypeSelection({ target: { value: selectedVal, name: policy } }) {
-    if (policy === 'retention_policy') {
-      this.args.model.retain_for = {
-        ...this.args.model.retain_for,
-        days: selectedVal,
-      };
-      if (selectedVal < 0) {
-        this.args.model.delete_after = {
-          ...this.args.model.delete_after,
-          days: 0,
-        };
-        this.isCustomDeletionSelected = false;
-        this.isCustomRetentionSelected = false;
-      }
-
-      if (selectedVal > 0) {
-        this.isCustomRetentionSelected = true;
-      } else {
-        this.isCustomRetentionSelected = false;
-      }
-    }
-
-    if (policy === 'deletion_policy') {
-      this.args.model.delete_after = {
-        ...this.args.model.delete_after,
-        days: selectedVal,
-      };
-      if (selectedVal > 0) {
-        this.isCustomDeletionSelected = true;
-      } else {
-        this.isCustomDeletionSelected = false;
-      }
-    }
   }
 }
--- ui/admin/app/components/form/policy/policy-selection/index.js
+++ ui/admin/app/components/form/policy/policy-selection/index.js
@@ -1,7 +1,39 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class FormPolicySelectionComponent extends Component {
+  //attributes
+  @tracked isCustomRetentionSelected = this.args.model.retain_for?.days > 0;
+  @tracked isCustomDeletionSelected = this.args.model.delete_after?.days > 0;
+  @tracked retainForOverridable = this.args.model.retain_for?.overridable;
+  @tracked deleteAfterOverridable = this.args.model.delete_after?.overridable;
+
+  //methods
+  /**
+   * This is used to show/hide the custom input field
+   * @type {boolean}
+   */
+  get showCustomInput() {
+    if (this.args.name === 'retention_policy') {
+      return this.isCustomRetentionSelected;
+    } else if (
+      this.args.name === 'deletion_policy' &&
+      this.args.model.retain_for?.days > 0
+    ) {
+      return this.isCustomDeletionSelected;
+    } else {
+      return null;
+    }
+  }
+
+  /**
+   * Returns true if the toggle is on
+   * @type {boolean}
+   */
+  get isOverridable() {
+    return this.args.model[this.args.customInputName]?.overridable;
+  }
   //actions
   /**
    * Handles custom input changes
@@ -23,5 +55,45 @@ export default class FormPolicySelectionComponent extends Component {
       ...this.args.model[field],
       overridable: !this.args.model[field].overridable,
     };
+  }
+
+  /**
+   * Show custom text field when custom option is selected
+   */
+  @action
+  handlePolicyTypeSelection({ target: { value: selectedVal, name: policy } }) {
+    if (policy === 'retention_policy') {
+      this.args.model.retain_for = {
+        ...this.args.model.retain_for,
+        days: selectedVal,
+      };
+      if (selectedVal < 0) {
+        this.args.model.delete_after = {
+          ...this.args.model.delete_after,
+          days: 0,
+        };
+
+        this.isCustomDeletionSelected = false;
+        this.isCustomRetentionSelected = false;
+      }
+
+      if (selectedVal > 0) {
+        this.isCustomRetentionSelected = true;
+      } else {
+        this.isCustomRetentionSelected = false;
+      }
+    }
+
+    if (policy === 'deletion_policy') {
+      this.args.model.delete_after = {
+        ...this.args.model.delete_after,
+        days: selectedVal,
+      };
+      if (selectedVal > 0) {
+        this.isCustomDeletionSelected = true;
+      } else {
+        this.isCustomDeletionSelected = false;
+      }
+    }
   }
 }
