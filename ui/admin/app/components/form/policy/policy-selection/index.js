85241e6967d160b3d478af80d510159a725386e8
dharini <dharini@hashicorp.com>
2024-01-10 22:01:26 -0800

    fix: 🐛 remove log, refactor

--- ui/admin/app/components/form/policy/policy-selection/index.js
+++ ui/admin/app/components/form/policy/policy-selection/index.js
@@ -1,20 +1,7 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
 
 export default class FormPolicySelectionComponent extends Component {
-  //attributes
-  @tracked selUnit;
-
-  //methods
-  /**
-   * Force the delete After select list to have `do not delete` option selected
-   * @type {boolean}
-   */
-  get disableDeleteOptions() {
-    return this.args.model.retain_for?.days < 0 ? true : false;
-  }
-
   //actions
   /**
    * Handles custom input changes
