5e4770f1a11894479f25d4fc54f725d2dea21c1c
Dharini Jeeva <dharini@hashicorp.com>
2024-01-23 10:06:57 -0800

    Update ui/admin/app/routes/scopes/scope/add-storage-policy/index.js

--- ui/admin/app/routes/scopes/scope/add-storage-policy/index.js
+++ ui/admin/app/routes/scopes/scope/add-storage-policy/index.js
@@ -26,7 +26,7 @@ export default class ScopesScopeAddStoragePolicyIndexRoute extends Route {
       scope_id,
     });
     if (scope_id === 'global') {
-      //Global scope should only list policies from its scope
+      // Global scope should only list policies from its scope
       this.policyList = currentScope;
     } else {
       //Org scope should list both global and org scope policies
