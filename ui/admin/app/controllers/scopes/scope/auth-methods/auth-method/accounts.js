4f717cf168f0ee6200a3b769f7d72a40d7726f8b
Lisbet Alvarez <lisbet.alvarez@hashicorp.com>
2024-02-09 11:36:16 -0800

    fix: 🐛 await refresh
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12616

--- ui/admin/app/controllers/scopes/scope/auth-methods/auth-method/accounts.js
+++ ui/admin/app/controllers/scopes/scope/auth-methods/auth-method/accounts.js
@@ -55,7 +55,7 @@ export default class ScopesScopeAuthMethodsAuthMethodAccountsController extends 
         'scopes.scope.auth-methods.auth-method.accounts',
       );
     }
-    this.router.refresh();
+    await this.router.refresh();
   }
 
   /**
@@ -72,6 +72,6 @@ export default class ScopesScopeAuthMethodsAuthMethodAccountsController extends 
     await this.router.replaceWith(
       'scopes.scope.auth-methods.auth-method.accounts',
     );
-    this.router.refresh();
+    await this.router.refresh();
   }
 }
