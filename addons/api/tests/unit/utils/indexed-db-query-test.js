83bea9cc8da56e0c43d6b94860bf0dd9c68b654c
Lisbet Alvarez <lisbet.alvarez@hashicorp.com>
2024-03-07 14:05:03 -0800

    test: 💍 add additional tests for indexedDB parser
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12164

--- addons/api/tests/unit/utils/indexed-db-query-test.js
+++ addons/api/tests/unit/utils/indexed-db-query-test.js
@@ -60,4 +60,195 @@ module('Unit | Utility | indexed-db-query', function (hooks) {
 
     assert.strictEqual(result.length, 5);
   });
+
+  test('it filters by single field and specify logicalOperator type', async function (assert) {
+    const resource = 'target';
+    const scope = this.server.create('scope', { id: 'p_123', type: 'project' });
+    this.server.createList(resource, 5, {
+      type: 'ssh',
+      scopeId: scope.id,
+    });
+    this.server.createList(resource, 5, {
+      type: 'tcp',
+      scopeId: scope.id,
+    });
+    await seedIndexDb(resource, store, indexedDb, this.server);
+    const query = {
+      filters: {
+        type: {
+          logicalOperator: 'and',
+          values: [{ equals: 'ssh' }],
+        },
+      },
+    };
+    const result = await queryIndexedDb(indexedDb.db, resource, query);
+
+    assert.strictEqual(result.length, 5);
+  });
+
+  test('it filters by single field and specify notEquals', async function (assert) {
+    const resource = 'target';
+    const scope = this.server.create('scope', { id: 'p_123', type: 'project' });
+    this.server.createList(resource, 5, {
+      type: 'ssh',
+      scopeId: scope.id,
+    });
+    this.server.createList(resource, 5, {
+      type: 'tcp',
+      scopeId: scope.id,
+    });
+    await seedIndexDb(resource, store, indexedDb, this.server);
+    const query = {
+      filters: {
+        type: [{ equals: 'ssh' }, { notEquals: 'tcp' }],
+      },
+    };
+    const result = await queryIndexedDb(indexedDb.db, resource, query);
+
+    assert.strictEqual(result.length, 5);
+  });
+
+  test('it filters by single field with logicalOperator of "OR"', async function (assert) {
+    const resource = 'target';
+    const scope = this.server.create('scope', { id: 'p_123', type: 'project' });
+    this.server.createList(resource, 5, {
+      type: 'ssh',
+      scopeId: scope.id,
+    });
+    this.server.createList(resource, 5, {
+      type: 'tcp',
+      scopeId: scope.id,
+    });
+    await seedIndexDb(resource, store, indexedDb, this.server);
+    const query = {
+      filters: {
+        type: {
+          logicalOperator: 'or',
+          values: [{ equals: 'ssh' }, { equals: 'tcp' }],
+        },
+      },
+    };
+    const result = await queryIndexedDb(indexedDb.db, resource, query);
+
+    assert.strictEqual(result.length, 10);
+  });
+
+  test('it filters by multiple fields', async function (assert) {
+    const resource = 'target';
+    const scope1 = this.server.create('scope', {
+      id: 'p_123',
+      type: 'project',
+    });
+    const scope2 = this.server.create('scope', {
+      id: 'p_456',
+      type: 'project',
+    });
+    this.server.createList(resource, 5, {
+      type: 'ssh',
+      scopeId: scope1.id,
+    });
+    this.server.createList(resource, 5, {
+      type: 'tcp',
+      scopeId: scope2.id,
+    });
+    await seedIndexDb(resource, store, indexedDb, this.server);
+    const query = {
+      filters: {
+        type: [{ equals: 'ssh' }],
+        scope_id: [{ equals: scope1.id }],
+      },
+    };
+    const result = await queryIndexedDb(indexedDb.db, resource, query);
+
+    assert.strictEqual(result.length, 5);
+  });
+
+  test('it filters by multiple fields and uses logicalOperator "OR"', async function (assert) {
+    const resource = 'target';
+    const scope1 = this.server.create('scope', {
+      id: 'p_123',
+      type: 'project',
+    });
+    const scope2 = this.server.create('scope', {
+      id: 'p_456',
+      type: 'project',
+    });
+    this.server.createList(resource, 5, {
+      type: 'ssh',
+      scopeId: scope1.id,
+    });
+    this.server.createList(resource, 5, {
+      type: 'tcp',
+      scopeId: scope2.id,
+    });
+    this.server.create(resource, { type: 'tcp', scopeId: scope1.id });
+    await seedIndexDb(resource, store, indexedDb, this.server);
+    const query = {
+      filters: {
+        type: {
+          logicalOperator: 'or',
+          values: [{ equals: 'ssh' }, { equals: 'tcp' }],
+        },
+        scope_id: [{ equals: scope1.id }],
+      },
+    };
+    const result = await queryIndexedDb(indexedDb.db, resource, query);
+
+    assert.strictEqual(result.length, 6);
+  });
+
+  test('it filters by multiple fields and uses two logicalOperator "OR"s', async function (assert) {
+    const resource = 'target';
+    const scope1 = this.server.create('scope', {
+      id: 'p_123',
+      type: 'project',
+    });
+    const scope2 = this.server.create('scope', {
+      id: 'p_456',
+      type: 'project',
+    });
+    this.server.createList(resource, 5, {
+      type: 'ssh',
+      scopeId: scope1.id,
+    });
+    this.server.createList(resource, 5, {
+      type: 'tcp',
+      scopeId: scope2.id,
+    });
+    await seedIndexDb(resource, store, indexedDb, this.server);
+    const query = {
+      filters: {
+        type: {
+          logicalOperator: 'or',
+          values: [{ equals: 'ssh' }, { equals: 'tcp' }],
+        },
+        scope_id: {
+          logicalOperator: 'or',
+          values: [{ equals: scope1.id }, { equals: scope2.id }],
+        },
+      },
+    };
+    const result = await queryIndexedDb(indexedDb.db, resource, query);
+
+    assert.strictEqual(result.length, 10);
+  });
+
+  test('it filters by single field and returns zero results', async function (assert) {
+    const resource = 'target';
+    const scope = this.server.create('scope', { id: 'p_123', type: 'project' });
+    this.server.createList(resource, 5, {
+      type: 'tcp',
+      scopeId: scope.id,
+    });
+    await seedIndexDb(resource, store, indexedDb, this.server);
+    const query = {
+      filters: {
+        type: [{ equals: 'ssh' }],
+      },
+    };
+
+    const result = await queryIndexedDb(indexedDb.db, resource, query);
+
+    assert.strictEqual(result.length, 0);
+  });
 });
