a26662eca5a9f36c370712a4ae08b0ab2bf259e0
Zhihe Li <zhihe.li@hashicorp.com>
2024-03-18 14:50:50 -0400

    feat: 🎸 Add filtering/pagination for admin ui targets (#2072)
    * feat: 🎸 Add filtering/pagination for admin ui targets
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12193
    
    * test: 💍 add acceptance test cases
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12193
    
    * test: 💍 cleanup tests and add controller tests
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12193
    
    ---------
    
    Co-authored-by: Lisbet Alvarez <lisbet.alvarez@hashicorp.com>

--- ui/admin/tests/acceptance/targets/list-test.js
+++ ui/admin/tests/acceptance/targets/list-test.js
@@ -4,19 +4,35 @@
  */
 
 import { module, test } from 'qunit';
-import { visit, click } from '@ember/test-helpers';
+import { visit, click, fillIn, waitUntil, findAll } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+import { setupIndexedDb } from 'api/test-support/helpers/indexed-db';
 import {
   authenticateSession,
   // These are left here intentionally for future reference.
   //currentSession,
   //invalidateSession,
 } from 'ember-simple-auth/test-support';
+import { TYPE_TARGET_TCP, TYPE_TARGET_SSH } from 'api/models/target';
 
 module('Acceptance | targets | list', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
+  setupIndexedDb(hooks);
+
+  const SEARCH_INPUT_SELECTOR = '.search-filtering [type="search"]';
+  const NO_RESULTS_MSG_SELECTOR = '[data-test-no-target-results]';
+  const TYPE_FILTER_DROPDOWN_SELECTOR =
+    '.search-filtering [name="Type"] button';
+  const TCP_TYPE_FILTER_OPTION_SELECTOR =
+    '.search-filtering [name="Type"] [data-test-checkbox="tcp"]';
+  const FILTER_APPLY_BUTTON_SELECTOR =
+    '.search-filtering [data-test-dropdown-apply-button]';
+  const ACTIVE_SESSIONS_FILTER_DROPDOWN_SELECTOR =
+    '.search-filtering [name="Active sessions"] button';
+  const YES_FILTER_OPTION_SELECTOR =
+    '.search-filtering [name="Active sessions"] [data-test-checkbox="yes"]';
 
   const instances = {
     scopes: {
@@ -24,13 +40,16 @@ module('Acceptance | targets | list', function (hooks) {
       org: null,
       project: null,
     },
-    target: null,
+    tcpTarget: null,
+    sshTarget: null,
   };
 
   const urls = {
     orgScope: null,
     projectScope: null,
     targets: null,
+    tcpTarget: null,
+    sshTarget: null,
   };
 
   hooks.beforeEach(function () {
@@ -43,12 +62,23 @@ module('Acceptance | targets | list', function (hooks) {
       type: 'project',
       scope: { id: instances.scopes.org.id, type: 'org' },
     });
-    instances.target = this.server.create('target', {
+    instances.tcpTarget = this.server.create('target', {
+      type: TYPE_TARGET_TCP,
+      scope: instances.scopes.project,
+    });
+    instances.sshTarget = this.server.create('target', {
+      id: 'target-1',
+      type: TYPE_TARGET_SSH,
       scope: instances.scopes.project,
     });
     urls.orgScope = `/scopes/${instances.scopes.org.id}/scopes`;
     urls.projectScope = `/scopes/${instances.scopes.project.id}`;
     urls.targets = `${urls.projectScope}/targets`;
+    urls.tcpTarget = `${urls.targets}/${instances.tcpTarget.id}`;
+    urls.sshTarget = `${urls.targets}/${instances.sshTarget.id}`;
+
+    const featuresService = this.owner.lookup('service:features');
+    featuresService.enable('ssh-target');
     authenticateSession({});
   });
 
@@ -133,5 +163,71 @@ module('Acceptance | targets | list', function (hooks) {
       ),
     );
     assert.dom(`[href="${urls.targets}"]`).exists();
+  });
+
+  test('user can search for a specifc target by id', async function (assert) {
+    await visit(urls.projectScope);
+
+    await click(`[href="${urls.targets}"]`);
+
+    assert.dom(`[href="${urls.tcpTarget}"]`).exists();
+    assert.dom(`[href="${urls.sshTarget}"]`).exists();
+
+    await fillIn(SEARCH_INPUT_SELECTOR, instances.sshTarget.id);
+    await waitUntil(() => findAll(`[href="${urls.tcpTarget}"]`).length === 0);
+
+    assert.dom(`[href="${urls.sshTarget}"]`).exists();
+    assert.dom(`[href="${urls.tcpTarget}"]`).doesNotExist();
+  });
+
+  test('user can search for targets and get no results', async function (assert) {
+    await visit(urls.projectScope);
+
+    await click(`[href="${urls.targets}"]`);
+
+    assert.dom(`[href="${urls.tcpTarget}"]`).exists();
+    assert.dom(`[href="${urls.sshTarget}"]`).exists();
+
+    await fillIn(SEARCH_INPUT_SELECTOR, 'fake target that does not exist');
+    await waitUntil(() => findAll(NO_RESULTS_MSG_SELECTOR).length === 1);
+
+    assert.dom(`[href="${urls.sshTarget}"]`).doesNotExist();
+    assert.dom(`[href="${urls.tcpTarget}"]`).doesNotExist();
+    assert.dom(NO_RESULTS_MSG_SELECTOR).includesText('No results found');
+  });
+
+  test('user can filter for targets by type', async function (assert) {
+    await visit(urls.projectScope);
+
+    await click(`[href="${urls.targets}"]`);
+
+    assert.dom(`[href="${urls.tcpTarget}"]`).exists();
+    assert.dom(`[href="${urls.sshTarget}"]`).exists();
+
+    await click(TYPE_FILTER_DROPDOWN_SELECTOR);
+    await click(TCP_TYPE_FILTER_OPTION_SELECTOR);
+    await click(FILTER_APPLY_BUTTON_SELECTOR);
+    await waitUntil(() => findAll(`[href="${urls.sshTarget}"]`).length === 0);
+
+    assert.dom(`[href="${urls.sshTarget}"]`).doesNotExist();
+    assert.dom(`[href="${urls.tcpTarget}"]`).exists();
+  });
+
+  test('user can filter for targets by active sessions', async function (assert) {
+    await visit(urls.projectScope);
+
+    await click(`[href="${urls.targets}"]`);
+
+    assert.dom(`[href="${urls.tcpTarget}"]`).exists();
+    assert.dom(`[href="${urls.sshTarget}"]`).exists();
+
+    await click(ACTIVE_SESSIONS_FILTER_DROPDOWN_SELECTOR);
+    await click(YES_FILTER_OPTION_SELECTOR);
+    await click(FILTER_APPLY_BUTTON_SELECTOR);
+    await waitUntil(() => findAll(NO_RESULTS_MSG_SELECTOR).length === 1);
+
+    assert.dom(`[href="${urls.sshTarget}"]`).doesNotExist();
+    assert.dom(`[href="${urls.tcpTarget}"]`).doesNotExist();
+    assert.dom(NO_RESULTS_MSG_SELECTOR).includesText('No results found');
   });
 });
