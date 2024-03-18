98170ed01e2e9637e327b3c2f417dd093b15e185
Lisbet Alvarez <107949262+lisbet-alvarez@users.noreply.github.com>
2024-03-18 10:34:54 -0700

    refactor: 💡 Remove route-action usage for managed-groups (#2168)
    * refactor: 💡 Remove route-action usage for managed-groups
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12619

---
+++ ui/admin/app/controllers/scopes/scope/auth-methods/auth-method/managed-groups/managed-group/members.js
@@ -0,0 +1,6 @@
+import Controller, { inject as controller } from '@ember/controller';
+
+export default class ScopesScopeAuthMethodsAuthMethodManagedGroupsManagedGroupMembersController extends Controller {
+  @controller('scopes/scope/auth-methods/auth-method/managed-groups/index')
+  managedGroups;
+}
