453f1bb0827cbdb523f2edd8aa58597502f3451a
Lisbet Alvarez <lisbet.alvarez@hashicorp.com>
2024-03-20 16:48:08 -0700

    feat: 🎸 add search & pagination support for users
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12589

--- addons/api/addon/services/indexed-db.js
+++ addons/api/addon/services/indexed-db.js
@@ -12,6 +12,7 @@ export const modelIndexes = {
     '&id, attributes.created_time, attributes.type, attributes.name, attributes.description, attributes.address, attributes.scope.scope_id',
   session:
     '&id, attributes.created_time, attributes.type, attributes.status, attributes.endpoint, attributes.target_id, attributes.user_id, attributes.scope.scope_id',
+  user: '&id, attributes.created_time, attributes.name, attributes.description, attributes.scope.scope_id',
 };
 
 export const formatDbName = (userId, clusterUrl) =>
