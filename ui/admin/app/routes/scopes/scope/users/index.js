83248d35e8428b533b10112d6e290b8477c3aaac
Lisbet Alvarez <107949262+lisbet-alvarez@users.noreply.github.com>
2024-03-22 13:35:23 -0700

    feat: 🎸 add search & pagination support for users (#2195)
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12589

--- ui/admin/app/routes/scopes/scope/users/index.js
+++ ui/admin/app/routes/scopes/scope/users/index.js
@@ -4,9 +4,76 @@
  */
 
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class ScopesScopeUsersIndexRoute extends Route {
+  // =attributes
+
+  queryParams = {
+    search: {
+      refreshModel: true,
+      replace: true,
+    },
+    page: {
+      refreshModel: true,
+    },
+    pageSize: {
+      refreshModel: true,
+    },
+  };
+
+  // =services
+
+  @service store;
+  @service can;
+
   // =methods
+
+  /**
+   * Load all users under current scope.
+   * @return {Promise{[UserModel]}}
+   */
+  async model({ search, page, pageSize }) {
+    const scope = this.modelFor('scopes.scope');
+    const { id: scope_id } = scope;
+
+    const filters = {
+      scope_id: [{ equals: scope_id }],
+    };
+
+    await this.getUsersExist(scope_id);
+
+    let users;
+    let totalItems = 0;
+    if (this.can.can('list model', scope, { collection: 'users' })) {
+      users = await this.store.query('user', {
+        query: { search, filters },
+        page,
+        pageSize,
+      });
+      totalItems = users.meta?.totalItems;
+    }
+
+    return { users, usersExist: this.usersExist, totalItems };
+  }
+
+  async getUsersExist(scopeId) {
+    const options = { pushToStore: false };
+    const user = await this.store.query(
+      'user',
+      {
+        query: {
+          filters: {
+            scope_id: [{ equals: scopeId }],
+          },
+        },
+        page: 1,
+        pageSize: 1,
+      },
+      options,
+    );
+    this.usersExist = user.length > 0;
+  }
 
   setupController(controller) {
     const scope = this.modelFor('scopes.scope');
