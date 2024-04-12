ded8bdadd1b0d96e8828b77f9464727d0f6ead05
Lisbet Alvarez <107949262+lisbet-alvarez@users.noreply.github.com>
2024-04-12 10:31:32 -0700

    Icu 12592 add filtering pagination support for auth methods (#2209)
    * feat: 🎸 add filtering & pagination to auth methods
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12592
    
    ---------
    
    Co-authored-by: Zhihe Li <zhihe.li@hashicorp.com>
    Co-authored-by: Cameron Perera <cameron.perera@hashicorp.com>

--- ui/admin/app/routes/scopes/scope/users/user/add-accounts.js
+++ ui/admin/app/routes/scopes/scope/users/user/add-accounts.js
@@ -6,7 +6,6 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
-import { all } from 'rsvp';
 import { loading } from 'ember-loading';
 import { notifySuccess, notifyError } from 'core/decorators/notify';
 
@@ -33,8 +32,11 @@ export default class ScopesScopeUsersUserAddAccountsRoute extends Route {
   async model() {
     const user = this.modelFor('scopes.scope.users.user');
     const { id: scope_id } = this.modelFor('scopes.scope');
-    const authMethods = await this.store.query('auth-method', { scope_id });
-    await all(
+    const authMethods = await this.store.query('auth-method', {
+      scope_id,
+      query: { filters: { scope_id: [{ equals: scope_id }] } },
+    });
+    await Promise.all(
       authMethods.map(({ id: auth_method_id }) =>
         this.store.query('account', { auth_method_id }),
       ),
