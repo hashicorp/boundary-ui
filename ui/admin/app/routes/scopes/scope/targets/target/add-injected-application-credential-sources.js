3e9a06a5b58c729129a0a0ccbcfdf5c20b49f536
Zhihe Li <zhihe.li@hashicorp.com>
2024-04-23 10:49:33 -0400

    fix: 🐛 Fix adding host set error in targets (#2267)

--- ui/admin/app/routes/scopes/scope/targets/target/add-injected-application-credential-sources.js
+++ ui/admin/app/routes/scopes/scope/targets/target/add-injected-application-credential-sources.js
@@ -37,6 +37,12 @@ export default class ScopesScopeTargetsTargetAddInjectedApplicationCredentialSou
       scope_id,
       query: { filters: { scope_id: [{ equals: scope_id }] } },
     });
+
+    // TODO: For some reason, not returning promises fixes
+    //  an ember bug similar to this reported issue:
+    //  https://github.com/emberjs/data/issues/8299.
+    //  This is a temporary fix until we can find a better solution or
+    //  we upgrade ember data to try to fix the issue.
     await Promise.all(
       credentialStores.map(({ id: credential_store_id, isStatic }) => {
         if (isStatic) {
