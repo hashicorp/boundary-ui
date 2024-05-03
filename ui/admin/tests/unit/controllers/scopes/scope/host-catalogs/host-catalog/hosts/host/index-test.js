0dab74bea5572bf5a1bf82f09c723dbc0358ba00
Lisbet Alvarez <lisbet.alvarez@hashicorp.com>
2024-05-03 09:50:18 -0700

    style: 💄 remove unecessary comments
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12606

--- ui/admin/tests/unit/controllers/scopes/scope/host-catalogs/host-catalog/hosts/host/index-test.js
+++ ui/admin/tests/unit/controllers/scopes/scope/host-catalogs/host-catalog/hosts/host/index-test.js
@@ -6,7 +6,6 @@ module(
   function (hooks) {
     setupTest(hooks);
 
-    // TODO: Replace this with your real tests.
     test('it exists', function (assert) {
       let controller = this.owner.lookup(
         'controller:scopes/scope/host-catalogs/host-catalog/hosts/host/index',
