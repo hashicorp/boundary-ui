94b93d24638ad2e0f4c975103391cb39bcf9d60c
Cameron Perera <cameron.perera@hashicorp.com>
2024-06-04 13:32:29 -0500

    Icu 12707 update role mirage (#2319)
    * feat: 🎸 add grant_scope_ids to role mirage
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12707
    
    * feat: 🎸 set grant_scope_ids in onboarding workflow
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12707
    
    * refactor: 💡 set grant_scope_ids when calling updateGrantScopeID
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12707
    
    * refactor: 💡 add TODO for removal of grant_scope_id
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12707

--- ui/admin/app/components/form/role/index.js
+++ ui/admin/app/components/form/role/index.js
@@ -11,6 +11,8 @@ export default class FormRoleGrantsComponent extends Component {
 
   @action
   updateGrantScopeID(id) {
+    // TODO: all grant_scope_id instances will be removed in future PR
     this.args.model.grant_scope_id = id;
+    this.args.model.grant_scope_ids = [id];
   }
 }
