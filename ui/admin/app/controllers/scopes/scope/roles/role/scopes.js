cc41c4e9f7fa4c039231359804eab0d5a2557a94
cameronperera <cameron.perera@hashicorp.com>
2024-06-07 10:42:07 -0500

    feat: 🎸 add table to list grant scopes on a role
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12708

--- ui/admin/app/controllers/scopes/scope/roles/role/scopes.js
+++ ui/admin/app/controllers/scopes/scope/roles/role/scopes.js
@@ -4,7 +4,28 @@
  */
 
 import Controller, { inject as controller } from '@ember/controller';
+import { action } from '@ember/object';
+import {
+  GRANT_SCOPE_THIS,
+  GRANT_SCOPE_CHILDREN,
+  GRANT_SCOPE_DESCENDANTS,
+} from 'api/models/role';
 
 export default class ScopesScopeRolesRoleScopesController extends Controller {
   @controller('scopes/scope/roles/index') roles;
+
+  @action
+  isKeywordThis(id) {
+    return id === GRANT_SCOPE_THIS;
+  }
+
+  @action
+  isKeywordChildren(id) {
+    return id === GRANT_SCOPE_CHILDREN;
+  }
+
+  @action
+  isKeywordDescendants(id) {
+    return id === GRANT_SCOPE_DESCENDANTS;
+  }
 }
