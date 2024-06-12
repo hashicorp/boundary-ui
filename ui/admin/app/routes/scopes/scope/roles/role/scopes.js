2bc314d2711b2013b67cb1743e3f9bb5886b8574
Cameron Perera <cameron.perera@hashicorp.com>
2024-06-12 11:57:06 -0500

    Icu 12708 add scopes tab route in role route (#2322)
    * feat: 🎸 add roles/role/scopes route
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12708
    
    * refactor: 💡 update role and scope mirage factories
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12708
    
    * feat: 🎸 add grantScopes to role model
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12708
    
    * feat: 🎸 fetch all scopes recursively
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12708
    
    * feat: 🎸 add new role grant scopes translations
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12708
    
    * feat: 🎸 add table to list grant scopes on a role
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12708
    
    * test: 💍 acceptance test for grant scope table and controller
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12708
    
    * test: 💍 add unit tests for grantScopes method on role model
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12708
    
    * refactor: 💡 small updates from PR feedback
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12708
    
    * refactor: 💡 update grantScopes to use sort
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12708
    
    * feat: 🎸 update role scope description for each scope type
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12708
    
    * refactor: 💡 make grant_scope_ids sort better
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12708
    
    * refactor: 💡 update global scope to show as Global vs global
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12708
    
    * refactor: 💡 update logic for display grant scope keywords
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12708
    
    * refactor: 💡 small improvement in role.grantScopes
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12708
    
    * refactor: 💡 move scope query to roles/scope route
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12708
    
    * refactor: 💡 remove console.log
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-12708

---
+++ ui/admin/app/routes/scopes/scope/roles/role/scopes.js
@@ -0,0 +1,20 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import { GRANT_SCOPE_KEYWORDS } from 'api/models/role';
+
+export default class ScopesScopeRolesRoleScopesRoute extends Route {
+  // =services
+
+  @service store;
+
+  async model() {
+    const role = this.modelFor('scopes.scope.roles.role');
+    if (!GRANT_SCOPE_KEYWORDS.some((id) => role.grant_scope_ids.includes(id))) {
+      await this.store.query('scope', {
+        scope_id: role.scope.id,
+        recursive: true,
+      });
+    }
+    return role;
+  }
+}
