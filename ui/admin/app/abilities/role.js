612379bad8fd7d67e0dd50c9a9aebe4a3b3d50d2
Lisbet Alvarez <lisbet.alvarez@hashicorp.com>
2024-06-13 16:17:47 -0700

    refactor: 💡 clean-up
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-13880

--- ui/admin/app/abilities/role.js
+++ ui/admin/app/abilities/role.js
@@ -4,18 +4,13 @@
  */
 
 import RoleAbility from 'api/abilities/role';
-import { inject as service } from '@ember/service';
 
 export default class OverrideRoleAbility extends RoleAbility {
-  //service
-
-  @service features;
-
   /**
-   * These override abilities are temporary and just part of Phase 1
-   * of LDAP auth method implementation.
+   * This override ensures that a user can only set grant scopes
+   * on global and org level roles.
+   * @type {boolean}
    */
-
   get canSetGrantScopes() {
     return !this.model.scope.isProject ? super.canSetGrantScopes : false;
   }
