3273f5981799f310b80d666159f39f1338591e8f
Lisbet Alvarez <107949262+lisbet-alvarez@users.noreply.github.com>
2024-06-17 14:33:48 -0700

    Icu 13880 create manage scopes route and view with keyword values (#2342)
    * feat: 🎸 manage scopes route and page
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-13880
    
    * Update addons/core/translations/resources/en-us.yaml
    
    Co-authored-by: Zhihe Li <zhihe.li@hashicorp.com>
    
    * Update ui/admin/app/abilities/role.js
    
    Co-authored-by: Zhihe Li <zhihe.li@hashicorp.com>
    
    * Update ui/admin/app/components/form/role/manage-scopes/index.hbs
    
    Co-authored-by: Zhihe Li <zhihe.li@hashicorp.com>
    ---------
    
    Co-authored-by: Zhihe Li <zhihe.li@hashicorp.com>

--- ui/admin/tests/acceptance/roles/scopes-test.js
+++ ui/admin/tests/acceptance/roles/scopes-test.js
@@ -4,17 +4,26 @@
  */
 
 import { module, test } from 'qunit';
-import { visit, currentURL, click } from '@ember/test-helpers';
+import { visit, currentURL, click, findAll } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import { setupIndexedDb } from 'api/test-support/helpers/indexed-db';
 import a11yAudit from 'ember-a11y-testing/test-support/audit';
 import { authenticateSession } from 'ember-simple-auth/test-support';
+import { GRANT_SCOPE_THIS } from 'api/models/role';
 
 module('Acceptance | roles | scopes', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
   setupIndexedDb(hooks);
+
+  const SCOPE_TOGGLE_SELECTOR = (name) =>
+    `.hds-form-toggle input[name="${name}"]`;
+  const TABLE_ROW_SELECTOR = 'tbody tr';
+  const SAVE_BTN_SELECTOR = 'form [type="submit"]';
+  const CANCEL_BTN_SELECTOR = '.rose-form-actions [type="button"]';
+  const MANAGE_SCOPES_SELECTOR = '[data-test-manage-dropdown-scopes]';
+  const TOAST_SELECTOR = '[role="alert"]';
 
   const instances = {
     scopes: {
@@ -28,6 +37,7 @@ module('Acceptance | roles | scopes', function (hooks) {
     roles: null,
     role: null,
     roleScopes: null,
+    manageScopes: null,
   };
 
   hooks.beforeEach(function () {
@@ -47,6 +57,7 @@ module('Acceptance | roles | scopes', function (hooks) {
     urls.roles = `/scopes/${instances.scopes.org.id}/roles`;
     urls.role = `${urls.roles}/${instances.role.id}`;
     urls.roleScopes = `${urls.role}/scopes`;
+    urls.manageScopes = `${urls.role}/manage-scopes`;
   });
 
   test('visiting role scopes', async function (assert) {
@@ -54,7 +65,7 @@ module('Acceptance | roles | scopes', function (hooks) {
     await a11yAudit();
     assert.strictEqual(currentURL(), urls.roleScopes);
     assert
-      .dom('tbody tr')
+      .dom(TABLE_ROW_SELECTOR)
       .exists({ count: instances.role.grant_scope_ids.length });
   });
 
@@ -92,5 +103,72 @@ module('Acceptance | roles | scopes', function (hooks) {
     });
     await visit(urls.roleScopes);
     assert.dom('.role-grant-scopes div div:nth-child(3) a').doesNotExist();
+  });
+
+  test('toggle and save scope keywords to add', async function (assert) {
+    instances.role.update({ grant_scope_ids: [] });
+    await visit(urls.role);
+
+    await click(`[href="${urls.roleScopes}"]`);
+
+    assert.strictEqual(findAll(TABLE_ROW_SELECTOR).length, 0);
+
+    await click(MANAGE_SCOPES_SELECTOR);
+
+    assert.strictEqual(currentURL(), urls.manageScopes);
+
+    // Click three times to select, unselect, then reselect (for coverage)
+    await click(SCOPE_TOGGLE_SELECTOR(GRANT_SCOPE_THIS));
+    await click(SCOPE_TOGGLE_SELECTOR(GRANT_SCOPE_THIS));
+    await click(SCOPE_TOGGLE_SELECTOR(GRANT_SCOPE_THIS));
+    await click(SAVE_BTN_SELECTOR);
+
+    assert.strictEqual(currentURL(), urls.roleScopes);
+    assert.strictEqual(findAll(TABLE_ROW_SELECTOR).length, 1);
+  });
+
+  test('toggle and cancel scope keywords to add', async function (assert) {
+    instances.role.update({ grant_scope_ids: [] });
+    await visit(urls.role);
+
+    await click(`[href="${urls.roleScopes}"]`);
+
+    assert.strictEqual(findAll(TABLE_ROW_SELECTOR).length, 0);
+
+    await click(MANAGE_SCOPES_SELECTOR);
+
+    assert.strictEqual(currentURL(), urls.manageScopes);
+
+    // Click three times to select, unselect, then reselect (for coverage)
+    await click(SCOPE_TOGGLE_SELECTOR(GRANT_SCOPE_THIS));
+    await click(SCOPE_TOGGLE_SELECTOR(GRANT_SCOPE_THIS));
+    await click(SCOPE_TOGGLE_SELECTOR(GRANT_SCOPE_THIS));
+    await click(CANCEL_BTN_SELECTOR);
+
+    assert.strictEqual(currentURL(), urls.roleScopes);
+    assert.strictEqual(findAll(TABLE_ROW_SELECTOR).length, 0);
+  });
+
+  test('shows error message on scope save', async function (assert) {
+    this.server.post('/roles/:idMethod', () => {
+      return new Response(
+        400,
+        {},
+        {
+          status: 400,
+          code: 'invalid_argument',
+          message: 'The request was invalid.',
+          details: {},
+        },
+      );
+    });
+    instances.role.update({ grant_scope_ids: [] });
+    await visit(urls.role);
+
+    await click(MANAGE_SCOPES_SELECTOR);
+    await click(SCOPE_TOGGLE_SELECTOR(GRANT_SCOPE_THIS));
+    await click(SAVE_BTN_SELECTOR);
+
+    assert.dom(TOAST_SELECTOR).isVisible();
   });
 });
