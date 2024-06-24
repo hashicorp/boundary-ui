a3abbfc25965a9a37e571c4192931f6a80a74610
cameronperera <cameron.perera@hashicorp.com>
2024-06-24 11:46:53 -0500

    feat: 🎸 create workers/worker/actions component
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-13892

---
+++ ui/admin/tests/integration/components/workers/worker/actions/index-test.js
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'admin/tests/helpers';
+import { setupIntl } from 'ember-intl/test-support';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module(
+  'Integration | Component | workers/worker/actions/index',
+  function (hooks) {
+    setupRenderingTest(hooks);
+    setupIntl(hooks);
+
+    test('it renders', async function (assert) {
+      this.set('model', { id: 'w_123', authorized_actions: ['delete'] });
+      this.set('delete', () => {});
+
+      await render(
+        hbs`<Workers::Worker::Actions @model={{this.model}} @delete={{this.delete}} />`,
+      );
+
+      assert.dom(this.element).includesText('Remove Worker');
+    });
+  },
+);
