acac36cfef0953edd066e35d3cc7d3094117dafe
cameronperera <cameron.perera@hashicorp.com>
2024-06-26 16:38:01 -0500

    feat: 🎸 update worker serializer for adding api_tags
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-13895

--- addons/api/addon/serializers/worker.js
+++ addons/api/addon/serializers/worker.js
@@ -19,11 +19,16 @@ export default class WorkerSerializer extends ApplicationSerializer {
     let serialized = super.serialize(...arguments);
     const workerGeneratedAuthToken =
       snapshot?.adapterOptions?.workerGeneratedAuthToken;
-    if (workerGeneratedAuthToken)
+    if (workerGeneratedAuthToken) {
       serialized = this.serializeWithGeneratedToken(
         snapshot,
         workerGeneratedAuthToken,
       );
+    }
+    const apiTags = snapshot?.adapterOptions?.apiTags;
+    if (apiTags) {
+      serialized = this.serializeWithApiTags(snapshot, apiTags);
+    }
     return serialized;
   }
 
@@ -37,6 +42,19 @@ export default class WorkerSerializer extends ApplicationSerializer {
     return {
       scope_id: snapshot.attr('scope').scope_id,
       worker_generated_auth_token: workerGeneratedAuthToken,
+    };
+  }
+
+  /**
+   * Returns a payload with api_tags and version serialized.
+   * @param {Snapshot} snapshot
+   * @param {object} apiTags
+   * @return {object}
+   */
+  serializeWithApiTags(snapshot, apiTags) {
+    return {
+      version: snapshot.attr('version'),
+      api_tags: apiTags,
     };
   }
 }
