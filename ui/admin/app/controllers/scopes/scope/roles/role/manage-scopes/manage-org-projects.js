0a92d6a9c87632ce5642d4d5cd2bcfe3405bcc02
Lisbet Alvarez <107949262+lisbet-alvarez@users.noreply.github.com>
2024-06-28 15:11:34 -0700

    feat: 🎸 create manage projects in org route and view (#2365)
    * feat: 🎸 create manage projects in org route and view
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-13882
    
    ---------
    
    Co-authored-by: Cameron Perera <cameron.perera@hashicorp.com>
    Co-authored-by: Zhihe Li <zhihe.li@hashicorp.com>

---
+++ ui/admin/app/controllers/scopes/scope/roles/role/manage-scopes/manage-org-projects.js
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+import { loading } from 'ember-loading';
+import { tracked } from '@glimmer/tracking';
+import { debounce } from 'core/decorators/debounce';
+import { notifySuccess, notifyError } from 'core/decorators/notify';
+
+export default class ScopesScopeRolesRoleManageScopesManageOrgProjectsController extends Controller {
+  // =services
+
+  @service router;
+
+  // =attributes
+
+  queryParams = ['search', 'page', 'pageSize'];
+
+  @tracked search = '';
+  @tracked page = 1;
+  @tracked pageSize = 10;
+
+  // =actions
+
+  /**
+   * Handles input on each keystroke and the search queryParam
+   * @param {object} event
+   */
+  @action
+  @debounce(250)
+  handleSearchInput(event) {
+    const { value } = event.target;
+    this.search = value;
+    this.page = 1;
+  }
+
+  /**
+   * Save grant scope IDs to current role via the API.
+   * @param {RoleModel} role
+   * @param {[string]} grantScopeIDs
+   */
+  @action
+  @loading
+  @notifyError(({ message }) => message, { catch: true })
+  @notifySuccess('notifications.add-success')
+  async setGrantScopes(role, grantScopeIDs) {
+    await role.setGrantScopes(grantScopeIDs);
+    if (role.scope.isGlobal) {
+      this.router.replaceWith(
+        'scopes.scope.roles.role.manage-scopes.manage-custom-scopes',
+      );
+    } else {
+      this.router.replaceWith('scopes.scope.roles.role.manage-scopes');
+    }
+  }
+
+  /**
+   * Redirect to previous as if nothing ever happened.
+   */
+  @action
+  cancel(role) {
+    if (role.scope.isGlobal) {
+      this.router.replaceWith(
+        'scopes.scope.roles.role.manage-scopes.manage-custom-scopes',
+      );
+    } else {
+      this.router.replaceWith('scopes.scope.roles.role.manage-scopes');
+    }
+  }
+}
--- ui/admin/app/routes/scopes/scope/roles/role/scopes.js
+++ ui/admin/app/routes/scopes/scope/roles/role/scopes.js
@@ -1,23 +1,9 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
-import { GRANT_SCOPE_KEYWORDS } from 'api/models/role';
 
 export default class ScopesScopeRolesRoleScopesRoute extends Route {
-  // =services
+  // =methods
 
-  @service store;
-
-  async model() {
-    const role = this.modelFor('scopes.scope.roles.role');
-    if (
-      role.grant_scope_ids &&
-      !GRANT_SCOPE_KEYWORDS.some((id) => role.grant_scope_ids.includes(id))
-    ) {
-      await this.store.query('scope', {
-        scope_id: role.scope.id,
-        recursive: true,
-      });
-    }
-    return role;
+  model() {
+    return this.modelFor('scopes.scope.roles.role');
   }
 }
