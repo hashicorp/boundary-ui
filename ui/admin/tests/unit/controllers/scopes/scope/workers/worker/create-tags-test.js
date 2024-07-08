c4340d5154f79a746a8cc8be7295d4c267799d6a
cameronperera <cameron.perera@hashicorp.com>
2024-07-08 17:31:58 -0500

    test: 💍 add missing controller tests
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-13895

--- ui/admin/tests/unit/controllers/scopes/scope/workers/worker/create-tags-test.js
+++ ui/admin/tests/unit/controllers/scopes/scope/workers/worker/create-tags-test.js
@@ -5,17 +5,71 @@
 
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import { currentURL, visit } from '@ember/test-helpers';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+import { authenticateSession } from 'ember-simple-auth/test-support';
 
 module(
   'Unit | Controller | scopes/scope/workers/worker/create-tags',
   function (hooks) {
     setupTest(hooks);
+    setupMirage(hooks);
 
-    test('it exists', function (assert) {
-      let controller = this.owner.lookup(
+    let store;
+    let controller;
+
+    const instances = {
+      scopes: {
+        global: null,
+      },
+      worker: null,
+    };
+
+    const urls = {
+      workers: null,
+      tags: null,
+      createTags: null,
+    };
+
+    hooks.beforeEach(function () {
+      authenticateSession({});
+      store = this.owner.lookup('service:store');
+      controller = this.owner.lookup(
         'controller:scopes/scope/workers/worker/create-tags',
       );
+
+      instances.scopes.global = this.server.create('scope', { id: 'global' });
+      instances.worker = this.server.create('worker', {
+        scope: instances.scopes.global,
+      });
+
+      urls.workers = `/scopes/global/workers`;
+      urls.tags = `${urls.workers}/${instances.worker.id}/tags`;
+      urls.createTags = `${urls.workers}/${instances.worker.id}/create-tags`;
+    });
+
+    test('it exists', function (assert) {
       assert.ok(controller);
+    });
+
+    test('save action sets api tags for a worker', async function (assert) {
+      await visit(urls.createTags);
+      const worker = await store.findRecord('worker', instances.worker.id);
+      const apiTags = { key: ['value'] };
+
+      await controller.save(apiTags);
+
+      assert.deepEqual(worker.api_tags, apiTags);
+    });
+
+    test('cancel action clears api tags for a worker', async function (assert) {
+      await visit(urls.createTags);
+      controller.apiTags = [{ key: 'key', value: 'value' }];
+
+      await controller.cancel();
+
+      assert.deepEqual(controller.apiTags, []);
+      assert.strictEqual(currentURL(), urls.tags);
     });
   },
 );
