2317272a36933b22a33233a7e9ced8183414fa1d
cameronperera <cameron.perera@hashicorp.com>
2024-07-09 12:27:00 -0500

    refactor: 💡 add tracked to Tag class
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-13895

--- ui/admin/app/components/form/worker/tag.js
+++ ui/admin/app/components/form/worker/tag.js
@@ -1,4 +1,14 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import { tracked } from '@glimmer/tracking';
+
 export default class Tag {
+  @tracked key;
+  @tracked value;
+
   constructor(key, value) {
     this.key = key;
     this.value = value;
