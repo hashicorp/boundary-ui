8cb16dc46ce2f1af902e1e1f844e74a9f2314388
Cameron Perera <cameron.perera@hashicorp.com>
2024-07-16 12:54:44 -0500

    Icu 13894 create edit tags modal (#2384)
    * feat: 🎸 add "no tags" state for worker tags
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-13894
    
    * feat: 🎸 add edit workflow for worker tags
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-13894
    
    * refactor: 💡 update tag action permissions logic
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-13894
    
    * refactor: 💡 small change to updateApiTags
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-13894
    
    * feat: 🎸 show different tooltip for hcp managed worker tags
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-13894
    
    * refactor: 💡 address PR feedback
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-13894
    
    * refactor: 💡 address more PR feedback
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-13894

--- ui/admin/tests/acceptance/workers/tags-test.js
+++ ui/admin/tests/acceptance/workers/tags-test.js
@@ -4,11 +4,19 @@
  */
 
 import { module, test } from 'qunit';
-import { visit, currentURL, click, focus, fillIn } from '@ember/test-helpers';
+import {
+  visit,
+  currentURL,
+  click,
+  focus,
+  fillIn,
+  find,
+} from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import a11yAudit from 'ember-a11y-testing/test-support/audit';
 import { authenticateSession } from 'ember-simple-auth/test-support';
+import { HCP_MANAGED_KEY } from 'api/models/worker';
 
 module('Acceptance | workers | worker | tags', function (hooks) {
   setupApplicationTest(hooks);
@@ -18,15 +26,30 @@ module('Acceptance | workers | worker | tags', function (hooks) {
     'tbody tr:first-child td:nth-child(3) button';
   const CONFIG_TAG_TOOLTIP_TEXT_SELECTOR =
     'tbody tr:first-child td:nth-child(3) > div';
+  const API_TAG_KEY_SELECTOR = (row) =>
+    `tbody tr:nth-child(${row}) td:first-child pre`;
+  const API_TAG_VALUE_SELECTOR = (row) =>
+    `tbody tr:nth-child(${row}) td:nth-child(2) pre`;
   const API_TAG_ACTION_SELECTOR =
     'tbody tr:nth-child(4) td:nth-child(4) button';
-  const API_TAG_REMOVE_ACTION_SELECTOR =
-    'tbody tr:nth-child(4) td:nth-child(4) ul li button';
-  const CONFIRMATION_MODAL_INPUT_SELECTOR = '.confirmation-modal input';
+  const API_TAG_FIRST_BUTTON_SELECTOR =
+    'tbody tr:nth-child(4) td:nth-child(4) ul li:first-child button';
+  const API_TAG_LAST_BUTTON_SELECTOR =
+    'tbody tr:nth-child(4) td:nth-child(4) ul li:nth-child(2) button';
+  const EDIT_TAG_KEY_INPUT_SELECTOR =
+    '[data-test-edit-modal] [name="edit-tag-key"]';
+  const EDIT_TAG_VALUE_INPUT_SELECTOR =
+    '[data-test-edit-modal] [name="edit-tag-value"]';
+  const EDIT_TAG_CONFIRM_BUTTON = '[data-test-edit-modal] button:first-child';
+  const EDIT_TAG_CANCEL_BUTTON = '[data-test-edit-modal] button:last-child';
+  const CONFIRMATION_MODAL_INPUT_SELECTOR =
+    '[data-test-confirmation-modal] input';
   const CONFIRMATION_MODAL_REMOVE_BUTTON_SELECTOR =
-    '.confirmation-modal button:first-child';
+    '[data-test-confirmation-modal] button:first-child';
   const CONFIRMATION_MODAL_CANCEL_BUTTON_SELECTOR =
-    '.confirmation-modal button:last-child';
+    '[data-test-confirmation-modal] button:last-child';
+  const NO_TAGS_STATE_TITLE = '[data-test-no-tags] div:first-child';
+  const NO_TAGS_STATE_ACTION = '[data-test-no-tags] div:nth-child(3) a';
 
   const instances = {
     scopes: {
@@ -38,6 +61,7 @@ module('Acceptance | workers | worker | tags', function (hooks) {
     workers: null,
     worker: null,
     tags: null,
+    createTags: null,
   };
 
   hooks.beforeEach(function () {
@@ -48,6 +72,7 @@ module('Acceptance | workers | worker | tags', function (hooks) {
     urls.workers = `/scopes/global/workers`;
     urls.worker = `${urls.workers}/${instances.worker.id}`;
     urls.tags = `${urls.worker}/tags`;
+    urls.createTags = `${urls.worker}/create-tags`;
     authenticateSession({});
   });
 
@@ -60,9 +85,21 @@ module('Acceptance | workers | worker | tags', function (hooks) {
     assert.dom('tbody tr').exists({ count: 11 });
   });
 
-  test('config tags display a tooltip', async function (assert) {
+  test('config tags display a tooltip for HCP managed tag', async function (assert) {
+    instances.worker.config_tags = { [HCP_MANAGED_KEY]: ['true'] };
     await visit(urls.tags);
-    await a11yAudit();
+
+    await focus(CONFIG_TAG_TOOLTIP_SELECTOR);
+
+    assert
+      .dom(CONFIG_TAG_TOOLTIP_TEXT_SELECTOR)
+      .hasText(
+        'This is a HCP managed worker, you cannot edit or delete this worker’s tags.',
+      );
+  });
+
+  test('config tags display a tooltip self managed tag', async function (assert) {
+    await visit(urls.tags);
 
     await focus(CONFIG_TAG_TOOLTIP_SELECTOR);
 
@@ -77,7 +114,7 @@ module('Acceptance | workers | worker | tags', function (hooks) {
     assert.dom('tbody tr').exists({ count: 11 });
 
     await click(API_TAG_ACTION_SELECTOR);
-    await click(API_TAG_REMOVE_ACTION_SELECTOR);
+    await click(API_TAG_LAST_BUTTON_SELECTOR);
 
     assert.dom(CONFIRMATION_MODAL_REMOVE_BUTTON_SELECTOR).isDisabled();
 
@@ -96,7 +133,7 @@ module('Acceptance | workers | worker | tags', function (hooks) {
     assert.dom('tbody tr').exists({ count: 11 });
 
     await click(API_TAG_ACTION_SELECTOR);
-    await click(API_TAG_REMOVE_ACTION_SELECTOR);
+    await click(API_TAG_LAST_BUTTON_SELECTOR);
 
     await click(CONFIRMATION_MODAL_CANCEL_BUTTON_SELECTOR);
 
@@ -114,6 +151,84 @@ module('Acceptance | workers | worker | tags', function (hooks) {
 
     await click(API_TAG_ACTION_SELECTOR);
 
-    assert.dom(API_TAG_REMOVE_ACTION_SELECTOR).doesNotExist();
+    // Asserts first button is for editing and there is no second button
+    assert.dom(API_TAG_FIRST_BUTTON_SELECTOR).hasText('Edit tag');
+    assert.dom(API_TAG_LAST_BUTTON_SELECTOR).doesNotExist();
+  });
+
+  test('users can edit a specific tag', async function (assert) {
+    await visit(urls.tags);
+    const key = find(API_TAG_KEY_SELECTOR(4)).textContent.trim();
+    const value = find(API_TAG_VALUE_SELECTOR(4)).textContent.trim();
+
+    assert.dom(API_TAG_KEY_SELECTOR(4)).hasText(key);
+    assert.dom(API_TAG_VALUE_SELECTOR(4)).hasText(value);
+
+    await click(API_TAG_ACTION_SELECTOR);
+    await click(API_TAG_FIRST_BUTTON_SELECTOR);
+
+    await fillIn(EDIT_TAG_KEY_INPUT_SELECTOR, 'red');
+    await fillIn(EDIT_TAG_VALUE_INPUT_SELECTOR, 'blue');
+
+    await click(EDIT_TAG_CONFIRM_BUTTON);
+
+    assert.dom(API_TAG_KEY_SELECTOR(11)).hasText('red');
+    assert.dom(API_TAG_VALUE_SELECTOR(11)).hasText('blue');
+  });
+
+  test('users can cancel editing a tag', async function (assert) {
+    await visit(urls.tags);
+    const key = find(API_TAG_KEY_SELECTOR(4)).textContent.trim();
+    const value = find(API_TAG_VALUE_SELECTOR(4)).textContent.trim();
+
+    assert.dom(API_TAG_KEY_SELECTOR(4)).hasText(key);
+    assert.dom(API_TAG_VALUE_SELECTOR(4)).hasText(value);
+
+    await click(API_TAG_ACTION_SELECTOR);
+    await click(API_TAG_FIRST_BUTTON_SELECTOR);
+
+    await fillIn(EDIT_TAG_KEY_INPUT_SELECTOR, 'red');
+    await fillIn(EDIT_TAG_VALUE_INPUT_SELECTOR, 'blue');
+
+    await click(EDIT_TAG_CANCEL_BUTTON);
+
+    assert.dom(API_TAG_KEY_SELECTOR(4)).hasText(key);
+    assert.dom(API_TAG_VALUE_SELECTOR(4)).hasText(value);
+  });
+
+  test('user cannot edit a tag without proper permission', async function (assert) {
+    instances.worker.authorized_actions =
+      instances.worker.authorized_actions.filter(
+        (item) => item !== 'set-worker-tags',
+      );
+    await visit(urls.tags);
+
+    await click(API_TAG_ACTION_SELECTOR);
+
+    // Asserts first button is for removing and there is no second button
+    assert.dom(API_TAG_FIRST_BUTTON_SELECTOR).hasText('Remove tag');
+    assert.dom(API_TAG_LAST_BUTTON_SELECTOR).doesNotExist();
+  });
+
+  test('user does not see tag action dropdown if they cannot edit or remove tags', async function (assert) {
+    instances.worker.authorized_actions =
+      instances.worker.authorized_actions.filter(
+        (item) => item !== 'set-worker-tags' && item !== 'remove-worker-tags',
+      );
+
+    await visit(urls.tags);
+
+    assert.dom(API_TAG_ACTION_SELECTOR).doesNotExist();
+  });
+
+  test('shows "No tags added" message when there are no worker tags', async function (assert) {
+    instances.worker.api_tags = {};
+    instances.worker.config_tags = {};
+    instances.worker.canonical_tags = {};
+
+    await visit(urls.tags);
+
+    assert.dom(NO_TAGS_STATE_TITLE).hasText('No tags added');
+    assert.dom(NO_TAGS_STATE_ACTION).hasAttribute('href', urls.createTags);
   });
 });
