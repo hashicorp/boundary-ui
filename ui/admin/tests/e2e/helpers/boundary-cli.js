0499e0d316b25dce1f8cc119081b5b692b34a0da
Carlos Alcaide Corvo <ccorvo@hashicorp.com>
2024-07-19 14:08:08 -0700

    Test add headers automated + auto commit

--- ui/admin/tests/e2e/helpers/boundary-cli.js
+++ ui/admin/tests/e2e/helpers/boundary-cli.js
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: BUSL-1.1
- */
-
 /* eslint-disable no-undef */
 const { execSync, exec } = require('child_process');
 const { nanoid } = require('nanoid');
