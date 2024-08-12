0542218537d25761f08b92692c082731c803377f
Cameron Perera <cameron.perera@hashicorp.com>
2024-08-12 10:41:27 -0500

    Icu 14033 storage buckets read test (#2414)
    * test: 💍 update storage-bucket read test selectors
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-14033
    
    * test: 💍 add common selectors in tests/helpers
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-14033
    
    * refactor: 💡 update selector name and value
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-14033
    
    * refactor: 💡 update selector name
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-14033

---
+++ ui/admin/tests/helpers/selectors.js
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+export const SAVE_BTN = '[type=submit]';
+export const CANCEL_BTN = '.rose-form-actions [type=button]';
+export const EDIT_BTN = '.rose-form-actions [type=button]';
+
+export const FIELD_NAME = '[name=name]';
+export const FIELD_NAME_VALUE = 'Random name';
+
+export const TABLE_FIRST_ROW_RESOURCE_LINK = 'tbody tr:first-child a';
+
+export const RESOURCE_NOT_FOUND_SUBTITLE = '.rose-message-subtitle';
+export const RESOURCE_NOT_FOUND_VALUE = 'Error 404';
+
+export const HREF = (url) => `[href="${url}"]`;
