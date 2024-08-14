5d5a0acd2083e822f03cb879aebd89c0ca067d64
Zhihe Li <zhihe.li@hashicorp.com>
2024-08-14 14:32:56 -0400

    feat: 🎸 Show cache daemon errors from status (#2437)
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-14816

--- ui/desktop/app/routes/scopes/scope/projects/settings/index.js
+++ ui/desktop/app/routes/scopes/scope/projects/settings/index.js
@@ -7,14 +7,17 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
 export default class ScopesScopeProjectsSettingsIndexRoute extends Route {
+  // =services
   @service ipc;
+  @service session;
 
+  // =methods
   async model() {
     let cacheDaemonStatus,
       formattedCacheVersion,
       formattedCliVersion,
       formattedDesktopVersion,
-      error;
+      cacheDaemonErrors = [];
 
     const { versionNumber: cliVersion } =
       await this.ipc.invoke('getCliVersion');
@@ -26,16 +29,28 @@ export default class ScopesScopeProjectsSettingsIndexRoute extends Route {
     try {
       cacheDaemonStatus = await this.ipc.invoke('cacheDaemonStatus');
     } catch (e) {
-      error = e;
+      cacheDaemonErrors.push(e);
     }
-    const { version } = cacheDaemonStatus ?? {};
+    const { version, users } = cacheDaemonStatus ?? {};
     formattedCacheVersion = version?.match(/v\d+\.\d+\.\d+/)?.[0];
+
+    // Grab the errors from each resource
+    const userId = this.session.data.authenticated.user_id;
+    const resourceErrors =
+      users
+        ?.find((user) => user.id === userId)
+        ?.resources.filter((resource) => resource?.last_error?.error)
+        ?.map((resource) => ({
+          message: resource.last_error.error,
+          name: resource.name,
+        })) ?? [];
+    cacheDaemonErrors = [...cacheDaemonErrors, ...resourceErrors];
 
     const logLevel = await this.ipc.invoke('getLogLevel');
     const logPath = await this.ipc.invoke('getLogPath');
 
     return {
-      error,
+      cacheDaemonErrors,
       formattedCliVersion,
       formattedCacheVersion,
       formattedDesktopVersion,
