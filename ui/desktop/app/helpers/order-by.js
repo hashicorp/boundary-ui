553905aa13fd07af5a64b185b3d43c1fa2257abf
Dharini <dharini@hashicorp.com>
2024-09-17 18:31:10 -0700

    fix: 🐛 show username first
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-15105

---
+++ ui/desktop/app/helpers/order-by.js
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import { helper } from '@ember/component/helper';
+
+/**
+ * This helper accepts an array of objects and orders them by a specified key.
+ * @param {object[]} arr - The array of objects to be sorted
+ * @param {string} keyToShowFirst - The key to be shown first in the sorting order
+ * @returns {object[]} - The sorted array of objects
+ */
+
+export default helper(function orderBy([arr, keyToShowFirst]) {
+  return arr.sort((a, b) => {
+    if (a.key === keyToShowFirst && b.key !== keyToShowFirst) {
+      return -1;
+    }
+    if (a.key !== keyToShowFirst && b.key === keyToShowFirst) {
+      return 1;
+    }
+    return 0; // Maintain original order if both values are the same or neither matches
+  });
+});
---
+++ ui/desktop/tests/integration/helpers/order-by-test.js
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, findAll } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Helper | order-by', function (hooks) {
+  setupRenderingTest(hooks);
+
+  // TODO: Replace this with your real tests.
+  test('it renders', async function (assert) {
+    this.set('inputValue', [
+      { key: 'username', value: 1 },
+      { key: 'password', value: 2 },
+      { key: 'none', value: 3 },
+    ]);
+
+    await render(hbs`
+      <ul>
+        {{#each (order-by this.inputValue 'none') as |secret|}}
+          <li>{{secret.key}}</li>
+        {{/each}}
+      </ul>
+    `);
+
+    assert.strictEqual(findAll('li').length, 3);
+    assert.strictEqual(
+      this.element.textContent.replace(/\s+/g, ' ').trim(),
+      'none username password',
+    );
+  });
+});
