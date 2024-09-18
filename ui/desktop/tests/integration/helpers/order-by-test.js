56f74f86dadb6a667f02158224f400109c7d6db5
Dharini <dharini@hashicorp.com>
2024-09-18 12:43:00 -0700

    fix: 🐛 remove unused helper method
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-15105

--- ui/desktop/tests/integration/helpers/order-by-test.js
+++ ui/desktop/tests/integration/helpers/order-by-test.js
@@ -5,7 +5,7 @@
 
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, findAll } from '@ember/test-helpers';
+import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Helper | order-by', function (hooks) {
