31cfdbcbc694e2afccb08015b13630ef1a71451b
Dharini <dharini@hashicorp.com>
2024-09-19 11:35:29 -0700

    test: 💍 more test cases
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-15105

--- ui/desktop/tests/integration/helpers/order-by-test.js
+++ ui/desktop/tests/integration/helpers/order-by-test.js
@@ -34,4 +34,53 @@ module('Integration | Helper | order-by', function (hooks) {
       'username none password',
     );
   });
+
+  test('maintains order if there are no matching keys in the list', async function (assert) {
+    this.set('inputValue', [
+      { key: 'username', value: 1 },
+      { key: 'password', value: 2 },
+      { key: 'none', value: 3 },
+    ]);
+
+    this.set('customOrder', ['one', 'two', 'three']);
+
+    await render(hbs`
+      <ul>
+        {{#each (order-by this.inputValue this.customOrder) as |secret|}}
+          <li>{{secret.key}}</li>
+        {{/each}}
+      </ul>
+    `);
+
+    assert.dom('li').exists({ count: 3 });
+    assert.strictEqual(
+      this.element.textContent.replace(/\s+/g, ' ').trim(),
+      'username password none',
+    );
+  });
+
+  test('maintains order if there are only some matching keys in the list', async function (assert) {
+    this.set('inputValue', [
+      { key: 'username', value: 1 },
+      { key: 'password', value: 2 },
+      { key: 'email', value: 3 },
+      { key: 'none', value: 3 },
+    ]);
+
+    this.set('customOrder', ['none', 'email']);
+
+    await render(hbs`
+      <ul>
+        {{#each (order-by this.inputValue this.customOrder) as |secret|}}
+          <li>{{secret.key}}</li>
+        {{/each}}
+      </ul>
+    `);
+
+    assert.dom('li').exists({ count: 4 });
+    assert.strictEqual(
+      this.element.textContent.replace(/\s+/g, ' ').trim(),
+      'none email username password',
+    );
+  });
 });
