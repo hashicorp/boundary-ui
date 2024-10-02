6e1cb921a2b0bd80664b7e80e458293d46afe546
Dharini <dharini@hashicorp.com>
2024-10-02 11:00:14 -0700

    fix: 🐛 windows no cli build crash

--- ui/desktop/electron-app/src/cli/path.js
+++ ui/desktop/electron-app/src/cli/path.js
@@ -7,6 +7,8 @@ const path = require('path');
 const { isWindows } = require('../helpers/platform.js');
 const isDev = require('electron-is-dev');
 const { existsSync } = require('node:fs');
+const which = require('which');
+const log = require('electron-log/main');
 
 const binaryName = isWindows() ? 'boundary.exe' : 'boundary';
 const builtInCliPath = isDev
@@ -19,8 +21,17 @@ const isBuiltInCli = existsSync(builtInCliPath);
 /**
  * Returns Boundary CLI path if the CLI is built in or the Boundary binary name
  * if not, so we assume boundary is available within user $PATH.
+ * For Windows, we filter out the path that is within the current working directory.
  */
-const pathBoundary = isBuiltInCli ? builtInCliPath : binaryName;
+const pathBoundary = isBuiltInCli
+  ? builtInCliPath
+  : isWindows()
+    ? which.sync(binaryName, { nothrow: true, all: true })
+      .filter((binary) => !binary.startsWith(process.cwd()))[0]
+    : binaryName;
+
+log.info('pathBoundary', pathBoundary)
+console.log(pathBoundary)
 
 module.exports = {
   path: pathBoundary,
