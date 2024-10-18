98d19ef5912cdf0c4143218e866f03844010922f
Cameron Perera <cameron.perera@hashicorp.com>
2024-10-18 09:56:31 -0500

    test: 💍 add new selectors for accounts acceptance tests (#2525)
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-14036

---
+++ ui/admin/tests/acceptance/accounts/selectors.js
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+export const APP_HEADER_DROPDOWN =
+  '.rose-header-utilities details:first-child summary';
+
+export const FIELD_CURRENT_PASSWORD = '[name=currentPassword]';
+export const FIELD_CURRENT_PASSWORD_VALUE = 'current password';
+
+export const FIELD_NEW_PASSWORD = '[name=newPassword]';
+export const FIELD_NEW_PASSWORD_VALUE = 'new password';
