b31ffcedf06be97929588ecc4930f0474d8ce898
Lisbet Alvarez <lisbet.alvarez@hashicorp.com>
2024-10-18 17:55:55 -0700

    refactor: 💡 remove route-action usage for targets and sessions
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-15040

--- ui/desktop/app/controllers/scopes/scope/projects/targets/target.js
+++ ui/desktop/app/controllers/scopes/scope/projects/targets/target.js
@@ -3,21 +3,90 @@
  * SPDX-License-Identifier: BUSL-1.1
  */
 
-import Controller from '@ember/controller';
+import Controller, { inject as controller } from '@ember/controller';
+import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class ScopesScopeProjectsTargetsTargetController extends Controller {
+  @controller('scopes/scope/projects/targets/index') targets;
+
+  // =services
+
+  @service store;
+  @service confirm;
+
   // =attributes
 
   queryParams = [{ isConnecting: { type: 'boolean' } }];
 
   @tracked isConnecting = false;
+  isConnectionError = false;
 
   // =methods
 
   @action
   toggleModal(value) {
     this.isConnecting = value;
+  }
+
+  /**
+   * Connect method that calls parent connect method and handles
+   * connection errors unique to this route
+   * @param {TargetModel} target
+   * @param {HostModel} host
+   */
+  @action
+  async connect(target, host) {
+    try {
+      await this.targets.connect(target, host);
+    } catch (error) {
+      this.isConnectionError = true;
+      this.confirm
+        .confirm(error.message, { isConnectError: true })
+        // Retry
+        .then(() => this.connect(target, host))
+        .catch(() => {
+          // Reset the flag as this was user initiated and we're not
+          // in a transition or have a host modal open
+          this.isConnectionError = false;
+        });
+    }
+  }
+
+  /**
+   * Establish a session to current target.
+   * @param {TargetModel} target
+   * @param {HostModel} host
+   * hostConnect is only called when making a connection with a host and ensures that the host modal is automatically closed in the case of a connection error.
+   */
+  @action
+  async hostConnect(target, host) {
+    await this.connect(target, host);
+
+    if (this.isConnectionError) {
+      this.toggleModal(false);
+      this.isConnectionError = false;
+    }
+  }
+
+  /**
+   * Determine if we show host modal or quick connect based on target attributes.
+   * @param {TargetModel} model
+   * @param {function} toggleModal
+   */
+  @action
+  async preConnect(model) {
+    /**
+     * if hosts length is 1 or less we will try to
+     * connect, even if there is no address on the target and
+     * rely on the CLI to give the user the proper error or if
+     * there are 2 or more hosts we show the modal for host selection
+     */
+    if (model.hosts.length <= 1) {
+      await this.connect(model.target);
+    } else {
+      this.toggleModal(true);
+    }
   }
 }
---
+++ ui/desktop/tests/unit/controllers/scopes/scope/projects/sessions/session-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module(
+  'Unit | Controller | scopes/scope/projects/sessions/session',
+  function (hooks) {
+    setupTest(hooks);
+
+    test('it exists', function (assert) {
+      let controller = this.owner.lookup(
+        'controller:scopes/scope/projects/sessions/session',
+      );
+      assert.ok(controller);
+      assert.ok(controller.sessions);
+    });
+  },
+);
