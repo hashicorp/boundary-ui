1b31188d5ed1c6e85c706f3b315903ad524dd631
Cameron Perera <cameron.perera@hashicorp.com>
2024-10-21 17:12:00 -0500

    test: 💍 selectors refactor for accounts create (#2527)
    * test: 💍 selectors refactor for accounts create
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-14037
    
    * refactor: 💡 remove name & description duplicate selectors
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-14037

--- ui/admin/tests/acceptance/accounts/selectors.js
+++ ui/admin/tests/acceptance/accounts/selectors.js
@@ -6,8 +6,19 @@
 export const APP_HEADER_DROPDOWN =
   '.rose-header-utilities details:first-child summary';
 
+export const MANAGE_DROPDOWN =
+  '[data-test-manage-auth-methods-dropdown] button';
+export const MANAGE_DROPDOWN_NEW_ACCOUNT = (url) =>
+  `[data-test-manage-auth-methods-dropdown] a[href="${url}"]`;
+
 export const FIELD_CURRENT_PASSWORD = '[name=currentPassword]';
 export const FIELD_CURRENT_PASSWORD_VALUE = 'current password';
 
 export const FIELD_NEW_PASSWORD = '[name=newPassword]';
 export const FIELD_NEW_PASSWORD_VALUE = 'new password';
+
+export const FIELD_LOGIN_NAME = '[name=login_name]';
+export const FIELD_LOGIN_NAME_VALUE = 'username';
+
+export const FIELD_PASSWORD = '[name=password]';
+export const FIELD_PASSWORD_VALUE = 'password';
