7c1814118d159abd6653584200248c293b372b1a
Zhihe Li <zhihe.li@hashicorp.com>
2024-11-12 11:21:37 -0500

    refactor: 💡 Move e2e tests to new folder (#2546)
    * refactor: 💡 Move e2e tests to new folder
    
    * refactor: 💡 Convert desktop tests to mjs and use es6 imports
    
    * refactor: 💡 Use module setting for e2e directory and remove mjs
    
    * refactor: 💡 Remove "e2e" in all scripts in package.json

---
+++ e2e-tests/desktop/fixtures/authenticateTest.js
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import { electronTest } from './electronTest.js';
+import LoginPage from '../pages/loginPage.js';
+
+export const authenticateTest = electronTest.extend({
+  // TODO: Should we put these in a config file?
+  clusterUrl: ({}, use) => {
+    use(process.env.BOUNDARY_ADDR ?? 'http://localhost:9200');
+  },
+  username: ({}, use) => {
+    use(process.env.E2E_PASSWORD_ADMIN_LOGIN_NAME ?? 'admin');
+  },
+  password: ({}, use) => {
+    use(process.env.E2E_PASSWORD_ADMIN_PASSWORD ?? 'password');
+  },
+  authedPage: async ({ electronPage, clusterUrl, username, password }, use) => {
+    const loginPage = new LoginPage(electronPage);
+    await loginPage.setClusterUrl(clusterUrl);
+    await loginPage.logInWithPassword(username, password);
+    use(electronPage);
+  },
+});
