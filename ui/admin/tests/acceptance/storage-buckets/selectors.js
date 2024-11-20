b709ec3d81c1fcd22ccea279d9cb3591c97edb1b
Cameron Perera <cameron.perera@hashicorp.com>
2024-11-20 11:13:51 -0600

    feat: 🎸 update worker filter field to use code editor (#2578)
    * feat: 🎸 update worker filter field to use code editor
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-15737
    
    * test: 💍 fix failing storage bucket tests
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-15737

--- ui/admin/tests/acceptance/storage-buckets/selectors.js
+++ ui/admin/tests/acceptance/storage-buckets/selectors.js
@@ -29,6 +29,10 @@ export const FIELD_SECRET_KEY_EDIT_BTN =
   '[data-test-secret-access-key] [type=button]';
 export const FIELD_WORKER_FILTER = '[name=worker_filter]';
 export const FIELD_WORKER_FILTER_ERROR = '[data-test-worker-filter-error]';
+export const EDITOR_WORKER_FILTER =
+  '[data-test-code-editor-field-editor] textarea';
+export const EDITOR_WORKER_FILTER_VALUE = '"dev" in "/tags/env"';
+export const TOAST = '[role=alert] div';
 export const TOAST_SUCCESSFUL_VALUE = 'Saved successfully.';
 
 export const TABLE_FIRST_ROW_ACTION_DROPDOWN =
