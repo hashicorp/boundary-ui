a694275c594c0316e670e1df2a215f3f7d8e936f
calcaide <ccorvo@hashicorp.com>
2024-11-29 13:27:42 -0800

    Clean up auto-generated TODO comments

--- addons/core/tests/unit/services/clock-tick-test.js
+++ addons/core/tests/unit/services/clock-tick-test.js
@@ -9,7 +9,6 @@ import { setupTest } from 'ember-qunit';
 module('Unit | Service | clock-tick', function (hooks) {
   setupTest(hooks);
 
-  // TODO: Replace this with your real tests.
   test('it exists', function (assert) {
     let service = this.owner.lookup('service:clock-tick');
     assert.ok(service);
