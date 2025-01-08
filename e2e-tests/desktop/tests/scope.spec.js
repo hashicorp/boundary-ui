301a1b36b1db5516b5da42ac9c7627d8f061fcf2
hashicc <191911133+hashicc@users.noreply.github.com>
2025-01-08 11:52:21 -0500

    refactor(e2e): move test hooks and associated variables to top-level
    Move beforeEach and afterEach and the associated variables setup in
    hooks to top-level within scope.spec.js. This matches the patterns
    used in other tests.

--- e2e-tests/desktop/tests/scope.spec.js
+++ e2e-tests/desktop/tests/scope.spec.js
@@ -6,47 +6,47 @@
 import { expect, test } from '../fixtures/baseTest.js';
 import * as boundaryHttp from '../../helpers/boundary-http.js';
 
+let orgA;
+let projectA;
+let targetA;
+
+let orgB;
+let projectB;
+let targetB;
+
+test.beforeEach(async ({ request, targetAddress, targetPort }) => {
+  // Group A resources
+  orgA = await boundaryHttp.createOrg(request);
+  projectA = await boundaryHttp.createProject(request, orgA.id);
+  targetA = await boundaryHttp.createTarget(request, {
+    scopeId: projectA.id,
+    type: 'tcp',
+    port: targetPort,
+    address: targetAddress,
+  });
+
+  // Group B resources
+  orgB = await boundaryHttp.createOrg(request);
+  projectB = await boundaryHttp.createProject(request, orgB.id);
+  targetB = await boundaryHttp.createTarget(request, {
+    scopeId: projectB.id,
+    type: 'tcp',
+    port: targetPort,
+    address: targetAddress,
+  });
+});
+
+test.afterEach(async ({ request }) => {
+  if (orgA) {
+    await boundaryHttp.deleteOrg(request, orgA.id);
+  }
+
+  if (orgB) {
+    await boundaryHttp.deleteOrg(request, orgB.id);
+  }
+});
+
 test.describe('Scope tests', async () => {
-  let orgA;
-  let projectA;
-  let targetA;
-
-  let orgB;
-  let projectB;
-  let targetB;
-
-  test.beforeEach(async ({ request, targetAddress, targetPort }) => {
-    // Group A resources
-    orgA = await boundaryHttp.createOrg(request);
-    projectA = await boundaryHttp.createProject(request, orgA.id);
-    targetA = await boundaryHttp.createTarget(request, {
-      scopeId: projectA.id,
-      type: 'tcp',
-      port: targetPort,
-      address: targetAddress,
-    });
-
-    // Group B resources
-    orgB = await boundaryHttp.createOrg(request);
-    projectB = await boundaryHttp.createProject(request, orgB.id);
-    targetB = await boundaryHttp.createTarget(request, {
-      scopeId: projectB.id,
-      type: 'tcp',
-      port: targetPort,
-      address: targetAddress,
-    });
-  });
-
-  test.afterEach(async ({ request }) => {
-    if (orgA) {
-      await boundaryHttp.deleteOrg(request, orgA.id);
-    }
-
-    if (orgB) {
-      await boundaryHttp.deleteOrg(request, orgB.id);
-    }
-  });
-
   async function assertSelectedHeaderNav(
     headerNavLocator,
     selectedScopeString,
