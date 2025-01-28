9742d824c44581661cf91363f435d6e0dc9d3744
Michael Li <michael.li@hashicorp.com>
2025-01-28 11:04:46 -0500

    test(e2e/desktop): Address failures in session tests (#2664)
    * test(e2e): Address failures in session tests
    
    * CR

---
+++ e2e-tests/desktop/pages/basePage.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import { expect } from '../fixtures/baseTest.js';
+
+export default class BasePage {
+  constructor(page) {
+    this.page = page;
+  }
+
+  async dismissSuccessAlert() {
+    await expect(
+      this.page.getByRole('alert').getByText('Success', { exact: true }),
+    ).toBeVisible();
+    await this.page
+      .getByRole('alert')
+      .getByRole('button', { name: 'Dismiss', exact: true })
+      .click();
+  }
+}
---
+++ e2e-tests/desktop/pages/sessionPage.js
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import { expect } from '../fixtures/baseTest.js';
+import BasePage from './basePage.js';
+
+export default class SessionPage extends BasePage {
+  async cancelAllSessions() {
+    await expect
+      .poll(
+        async () => {
+          await this.page
+            .getByRole('row')
+            .filter({ hasNot: this.page.getByRole('columnheader') })
+            .getByLabel('Cancel')
+            .first()
+            .click();
+
+          await this.dismissSuccessAlert();
+
+          return await this.page
+            .getByRole('row')
+            .filter({ hasNot: this.page.getByRole('columnheader') })
+            .getByLabel('Cancel')
+            .count();
+        },
+        {
+          message: 'Ensure all sessions are cancelled',
+        },
+      )
+      .toBe(0);
+  }
+}
