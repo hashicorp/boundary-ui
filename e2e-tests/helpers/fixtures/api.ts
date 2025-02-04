888739432bffc5506a68edaafb7a0ed5e95fa592
hashicc <hashicc@users.noreply.github.com>
2025-02-04 18:25:48 +0000

    Add missing copyright headers

--- e2e-tests/helpers/fixtures/api.ts
+++ e2e-tests/helpers/fixtures/api.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
 import { test as base } from '@playwright/test';
 import { Configuration, TargetServiceApi } from '../../api-client';
 
