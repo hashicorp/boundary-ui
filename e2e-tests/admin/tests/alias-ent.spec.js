f3232f34c203a342de3559ea70addb07fcab53f7
Michael Li <michael.li@hashicorp.com>
2025-02-12 17:24:51 -0500

    test(e2e): Use new tags syntax (#2686)
    * test(e2e): Use new tag syntax
    
    * chore: Update yarn lock

--- e2e-tests/admin/tests/alias-ent.spec.js
+++ e2e-tests/admin/tests/alias-ent.spec.js
@@ -20,178 +20,277 @@ test.beforeAll(async () => {
 });
 
 test.describe('Aliases (Enterprise)', () => {
-  test('Set up alias from target details page @ent @aws @docker', async ({
-    page,
-    baseURL,
-    adminAuthMethodId,
-    adminLoginName,
-    adminPassword,
-    sshUser,
-    sshKeyPath,
-    targetAddress,
-    targetPort,
-  }) => {
-    await page.goto('/');
-    let orgName;
-    let alias;
-    let connect;
-    const nanoid = customAlphabet('1234567890abcdefghijklmnopqrstuvwxyz', 10);
-    try {
-      const orgsPage = new OrgsPage(page);
-      orgName = await orgsPage.createOrg();
-      const projectsPage = new ProjectsPage(page);
-      await projectsPage.createProject();
+  test(
+    'Set up alias from target details page',
+    { tag: ['@ent', '@aws', '@docker'] },
+    async ({
+      page,
+      baseURL,
+      adminAuthMethodId,
+      adminLoginName,
+      adminPassword,
+      sshUser,
+      sshKeyPath,
+      targetAddress,
+      targetPort,
+    }) => {
+      await page.goto('/');
+      let orgName;
+      let alias;
+      let connect;
+      const nanoid = customAlphabet('1234567890abcdefghijklmnopqrstuvwxyz', 10);
+      try {
+        const orgsPage = new OrgsPage(page);
+        orgName = await orgsPage.createOrg();
+        const projectsPage = new ProjectsPage(page);
+        await projectsPage.createProject();
 
-      const targetsPage = new TargetsPage(page);
-      const targetName = await targetsPage.createSshTargetWithAddressEnt(
-        targetAddress,
-        targetPort,
-      );
-      const credentialStoresPage = new CredentialStoresPage(page);
-      await credentialStoresPage.createStaticCredentialStore();
-      const credentialName =
-        await credentialStoresPage.createStaticCredentialKeyPair(
-          sshUser,
-          sshKeyPath,
+        const targetsPage = new TargetsPage(page);
+        const targetName = await targetsPage.createSshTargetWithAddressEnt(
+          targetAddress,
+          targetPort,
         );
-      await targetsPage.addInjectedCredentialsToTarget(
-        targetName,
-        credentialName,
-      );
+        const credentialStoresPage = new CredentialStoresPage(page);
+        await credentialStoresPage.createStaticCredentialStore();
+        const credentialName =
+          await credentialStoresPage.createStaticCredentialKeyPair(
+            sshUser,
+            sshKeyPath,
+          );
+        await targetsPage.addInjectedCredentialsToTarget(
+          targetName,
+          credentialName,
+        );
 
-      // Create alias for target
-      await page
-        .getByRole('navigation', { name: 'breadcrumbs' })
-        .getByText(targetName)
-        .click();
-      const aliasName = 'Alias ' + nanoid();
-      alias = 'example.alias.' + nanoid();
-      await page.getByRole('link', { name: 'Add an alias' }).click();
-      await page.getByLabel('Name').fill(aliasName);
-      await page.getByLabel('Description').fill('This is an automated test');
-      await page.getByLabel('Alias Value').fill(alias);
-      await page.getByRole('button', { name: 'Save' }).click();
-      await expect(
-        page.getByRole('alert').getByText('Success', { exact: true }),
-      ).toBeVisible();
-      await page.getByRole('button', { name: 'Dismiss' }).click();
+        // Create alias for target
+        await page
+          .getByRole('navigation', { name: 'breadcrumbs' })
+          .getByText(targetName)
+          .click();
+        const aliasName = 'Alias ' + nanoid();
+        alias = 'example.alias.' + nanoid();
+        await page.getByRole('link', { name: 'Add an alias' }).click();
+        await page.getByLabel('Name').fill(aliasName);
+        await page.getByLabel('Description').fill('This is an automated test');
+        await page.getByLabel('Alias Value').fill(alias);
+        await page.getByRole('button', { name: 'Save' }).click();
+        await expect(
+          page.getByRole('alert').getByText('Success', { exact: true }),
+        ).toBeVisible();
+        await page.getByRole('button', { name: 'Dismiss' }).click();
 
-      // Connect to target using alias
-      await boundaryCli.authenticateBoundary(
-        baseURL,
-        adminAuthMethodId,
-        adminLoginName,
-        adminPassword,
-      );
-      connect = await boundaryCli.connectSshToAlias(alias);
-      const sessionsPage = new SessionsPage(page);
-      await sessionsPage.waitForSessionToBeVisible(targetName);
+        // Connect to target using alias
+        await boundaryCli.authenticateBoundary(
+          baseURL,
+          adminAuthMethodId,
+          adminLoginName,
+          adminPassword,
+        );
+        connect = await boundaryCli.connectSshToAlias(alias);
+        const sessionsPage = new SessionsPage(page);
+        await sessionsPage.waitForSessionToBeVisible(targetName);
 
-      // Clear destination from alias
-      await page
-        .getByRole('navigation', { name: 'Resources' })
-        .getByRole('link', { name: 'Targets' })
-        .click();
-      await page.getByRole('link', { name: targetName }).click();
-      await page.getByRole('link', { name: alias }).click();
-      // Note: On the Target details page, there is a section with the header
-      // "Aliases". The extra check here is to ensure that we are on the Alias
-      // details page and not the Target details page.
-      await expect(page.getByRole('heading', { name: 'Alias' })).not.toHaveText(
-        'Aliases',
-      );
-      await page.getByRole('button', { name: 'Manage' }).click();
-      await page.getByText('Clear', { exact: true }).click();
-      await page.getByText('OK', { exact: true }).click();
-      await expect(
-        page.getByRole('alert').getByText('Success', { exact: true }),
-      ).toBeVisible();
-      await page.getByRole('button', { name: 'Dismiss' }).click();
-    } finally {
-      if (connect) {
-        connect.kill('SIGTERM');
-      }
-      await boundaryCli.authenticateBoundary(
-        baseURL,
-        adminAuthMethodId,
-        adminLoginName,
-        adminPassword,
-      );
-      if (orgName) {
-        const orgId = await boundaryCli.getOrgIdFromName(orgName);
-        if (orgId) {
-          await boundaryCli.deleteScope(orgId);
+        // Clear destination from alias
+        await page
+          .getByRole('navigation', { name: 'Resources' })
+          .getByRole('link', { name: 'Targets' })
+          .click();
+        await page.getByRole('link', { name: targetName }).click();
+        await page.getByRole('link', { name: alias }).click();
+        // Note: On the Target details page, there is a section with the header
+        // "Aliases". The extra check here is to ensure that we are on the Alias
+        // details page and not the Target details page.
+        await expect(
+          page.getByRole('heading', { name: 'Alias' }),
+        ).not.toHaveText('Aliases');
+        await page.getByRole('button', { name: 'Manage' }).click();
+        await page.getByText('Clear', { exact: true }).click();
+        await page.getByText('OK', { exact: true }).click();
+        await expect(
+          page.getByRole('alert').getByText('Success', { exact: true }),
+        ).toBeVisible();
+        await page.getByRole('button', { name: 'Dismiss' }).click();
+      } finally {
+        if (connect) {
+          connect.kill('SIGTERM');
+        }
+        await boundaryCli.authenticateBoundary(
+          baseURL,
+          adminAuthMethodId,
+          adminLoginName,
+          adminPassword,
+        );
+        if (orgName) {
+          const orgId = await boundaryCli.getOrgIdFromName(orgName);
+          if (orgId) {
+            await boundaryCli.deleteScope(orgId);
+          }
+        }
+        if (alias) {
+          await boundaryCli.deleteAlias(alias);
         }
       }
-      if (alias) {
-        await boundaryCli.deleteAlias(alias);
-      }
-    }
-  });
+    },
+  );
 
-  test('Set up alias from new target page @ent @aws @docker', async ({
-    page,
-    baseURL,
-    adminAuthMethodId,
-    adminLoginName,
-    adminPassword,
-    sshUser,
-    sshKeyPath,
-    targetAddress,
-    targetPort,
-  }) => {
-    await page.goto('/');
-    let orgName;
-    let alias;
-    let connect;
-    const nanoid = customAlphabet('1234567890abcdefghijklmnopqrstuvwxyz', 10);
-    try {
-      const orgsPage = new OrgsPage(page);
-      orgName = await orgsPage.createOrg();
-      await boundaryCli.authenticateBoundary(
-        baseURL,
-        adminAuthMethodId,
-        adminLoginName,
-        adminPassword,
-      );
-      const projectsPage = new ProjectsPage(page);
-      await projectsPage.createProject();
-      alias = 'example.alias.' + nanoid();
-      const targetsPage = new TargetsPage(page);
-      const targetName = await targetsPage.createTargetWithAddressAndAlias(
-        targetAddress,
-        targetPort,
-        alias,
-      );
-      const credentialStoresPage = new CredentialStoresPage(page);
-      await credentialStoresPage.createStaticCredentialStore();
-      const credentialName =
-        await credentialStoresPage.createStaticCredentialKeyPair(
-          sshUser,
-          sshKeyPath,
+  test(
+    'Set up alias from new target page',
+    { tag: ['@ent', '@aws', '@docker'] },
+    async ({
+      page,
+      baseURL,
+      adminAuthMethodId,
+      adminLoginName,
+      adminPassword,
+      sshUser,
+      sshKeyPath,
+      targetAddress,
+      targetPort,
+    }) => {
+      await page.goto('/');
+      let orgName;
+      let alias;
+      let connect;
+      const nanoid = customAlphabet('1234567890abcdefghijklmnopqrstuvwxyz', 10);
+      try {
+        const orgsPage = new OrgsPage(page);
+        orgName = await orgsPage.createOrg();
+        await boundaryCli.authenticateBoundary(
+          baseURL,
+          adminAuthMethodId,
+          adminLoginName,
+          adminPassword,
         );
-      await targetsPage.addInjectedCredentialsToTarget(
-        targetName,
-        credentialName,
-      );
+        const projectsPage = new ProjectsPage(page);
+        await projectsPage.createProject();
+        alias = 'example.alias.' + nanoid();
+        const targetsPage = new TargetsPage(page);
+        const targetName = await targetsPage.createTargetWithAddressAndAlias(
+          targetAddress,
+          targetPort,
+          alias,
+        );
+        const credentialStoresPage = new CredentialStoresPage(page);
+        await credentialStoresPage.createStaticCredentialStore();
+        const credentialName =
+          await credentialStoresPage.createStaticCredentialKeyPair(
+            sshUser,
+            sshKeyPath,
+          );
+        await targetsPage.addInjectedCredentialsToTarget(
+          targetName,
+          credentialName,
+        );
 
-      // Connect to target using alias
-      connect = await boundaryCli.connectSshToAlias(alias);
-      const sessionsPage = new SessionsPage(page);
-      await sessionsPage.waitForSessionToBeVisible(targetName);
-    } finally {
-      if (connect) {
-        connect.kill('SIGTERM');
+        // Connect to target using alias
+        connect = await boundaryCli.connectSshToAlias(alias);
+        const sessionsPage = new SessionsPage(page);
+        await sessionsPage.waitForSessionToBeVisible(targetName);
+      } finally {
+        if (connect) {
+          connect.kill('SIGTERM');
+        }
+        await boundaryCli.authenticateBoundary(
+          baseURL,
+          adminAuthMethodId,
+          adminLoginName,
+          adminPassword,
+        );
+
+        if (orgName) {
+          const orgId = await boundaryCli.getOrgIdFromName(orgName);
+          if (orgId) {
+            await boundaryCli.deleteScope(orgId);
+          }
+          if (alias) {
+            await boundaryCli.deleteAlias(alias);
+          }
+        }
       }
-      await boundaryCli.authenticateBoundary(
-        baseURL,
-        adminAuthMethodId,
-        adminLoginName,
-        adminPassword,
-      );
+    },
+  );
 
-      if (orgName) {
-        const orgId = await boundaryCli.getOrgIdFromName(orgName);
+  test(
+    'Set up alias from aliases page',
+    { tag: ['@ent', '@aws', '@docker'] },
+    async ({
+      page,
+      baseURL,
+      adminAuthMethodId,
+      adminLoginName,
+      adminPassword,
+      sshUser,
+      sshKeyPath,
+      targetAddress,
+      targetPort,
+    }) => {
+      await page.goto('/');
+      const nanoid = customAlphabet('1234567890abcdefghijklmnopqrstuvwxyz', 10);
+      let orgId;
+      let alias;
+      let connect;
+      try {
+        const orgsPage = new OrgsPage(page);
+        const orgName = await orgsPage.createOrg();
+        const projectsPage = new ProjectsPage(page);
+        const projectName = await projectsPage.createProject();
+        const targetsPage = new TargetsPage(page);
+        const targetName = await targetsPage.createSshTargetWithAddressEnt(
+          targetAddress,
+          targetPort,
+        );
+        const credentialStoresPage = new CredentialStoresPage(page);
+        await credentialStoresPage.createStaticCredentialStore();
+        const credentialName =
+          await credentialStoresPage.createStaticCredentialKeyPair(
+            sshUser,
+            sshKeyPath,
+          );
+        await targetsPage.addInjectedCredentialsToTarget(
+          targetName,
+          credentialName,
+        );
+
+        // Create new alias from scope page
+        await boundaryCli.authenticateBoundary(
+          baseURL,
+          adminAuthMethodId,
+          adminLoginName,
+          adminPassword,
+        );
+        orgId = await boundaryCli.getOrgIdFromName(orgName);
+        const projectId = await boundaryCli.getProjectIdFromName(
+          orgId,
+          projectName,
+        );
+        const targetId = await boundaryCli.getTargetIdFromName(
+          projectId,
+          targetName,
+        );
+
+        alias = 'example.alias.' + nanoid();
+        const aliasesPage = new AliasesPage(page);
+        await aliasesPage.createAliasForTarget(alias, targetId);
+        connect = await boundaryCli.connectSshToAlias(alias);
+        await page
+          .getByRole('navigation', { name: 'General' })
+          .getByRole('link', { name: 'Orgs' })
+          .click();
+        await page.getByRole('link', { name: orgName }).click();
+        await page.getByRole('link', { name: projectName }).click();
+        const sessionsPage = new SessionsPage(page);
+        await sessionsPage.waitForSessionToBeVisible(targetName);
+      } finally {
+        if (connect) {
+          connect.kill('SIGTERM');
+        }
+        await boundaryCli.authenticateBoundary(
+          baseURL,
+          adminAuthMethodId,
+          adminLoginName,
+          adminPassword,
+        );
+
         if (orgId) {
           await boundaryCli.deleteScope(orgId);
         }
@@ -199,93 +298,6 @@ test.describe('Aliases (Enterprise)', () => {
           await boundaryCli.deleteAlias(alias);
         }
       }
-    }
-  });
-
-  test('Set up alias from aliases page @ent @aws @docker', async ({
-    page,
-    baseURL,
-    adminAuthMethodId,
-    adminLoginName,
-    adminPassword,
-    sshUser,
-    sshKeyPath,
-    targetAddress,
-    targetPort,
-  }) => {
-    await page.goto('/');
-    const nanoid = customAlphabet('1234567890abcdefghijklmnopqrstuvwxyz', 10);
-    let orgId;
-    let alias;
-    let connect;
-    try {
-      const orgsPage = new OrgsPage(page);
-      const orgName = await orgsPage.createOrg();
-      const projectsPage = new ProjectsPage(page);
-      const projectName = await projectsPage.createProject();
-      const targetsPage = new TargetsPage(page);
-      const targetName = await targetsPage.createSshTargetWithAddressEnt(
-        targetAddress,
-        targetPort,
-      );
-      const credentialStoresPage = new CredentialStoresPage(page);
-      await credentialStoresPage.createStaticCredentialStore();
-      const credentialName =
-        await credentialStoresPage.createStaticCredentialKeyPair(
-          sshUser,
-          sshKeyPath,
-        );
-      await targetsPage.addInjectedCredentialsToTarget(
-        targetName,
-        credentialName,
-      );
-
-      // Create new alias from scope page
-      await boundaryCli.authenticateBoundary(
-        baseURL,
-        adminAuthMethodId,
-        adminLoginName,
-        adminPassword,
-      );
-      orgId = await boundaryCli.getOrgIdFromName(orgName);
-      const projectId = await boundaryCli.getProjectIdFromName(
-        orgId,
-        projectName,
-      );
-      const targetId = await boundaryCli.getTargetIdFromName(
-        projectId,
-        targetName,
-      );
-
-      alias = 'example.alias.' + nanoid();
-      const aliasesPage = new AliasesPage(page);
-      await aliasesPage.createAliasForTarget(alias, targetId);
-      connect = await boundaryCli.connectSshToAlias(alias);
-      await page
-        .getByRole('navigation', { name: 'General' })
-        .getByRole('link', { name: 'Orgs' })
-        .click();
-      await page.getByRole('link', { name: orgName }).click();
-      await page.getByRole('link', { name: projectName }).click();
-      const sessionsPage = new SessionsPage(page);
-      await sessionsPage.waitForSessionToBeVisible(targetName);
-    } finally {
-      if (connect) {
-        connect.kill('SIGTERM');
-      }
-      await boundaryCli.authenticateBoundary(
-        baseURL,
-        adminAuthMethodId,
-        adminLoginName,
-        adminPassword,
-      );
-
-      if (orgId) {
-        await boundaryCli.deleteScope(orgId);
-      }
-      if (alias) {
-        await boundaryCli.deleteAlias(alias);
-      }
-    }
-  });
+    },
+  );
 });
--- e2e-tests/admin/tests/auth-method-password.spec.js
+++ e2e-tests/admin/tests/auth-method-password.spec.js
@@ -20,118 +20,124 @@ test.beforeAll(async () => {
   await boundaryCli.checkBoundaryCli();
 });
 
-test('Verify new auth-method can be created and assigned to users @ce @ent @aws @docker', async ({
-  page,
-  baseURL,
-  adminAuthMethodId,
-  adminLoginName,
-  adminPassword,
-}) => {
-  await page.goto('/');
-  let orgName;
-  let authMethodName;
-  try {
-    // Log in
-    const loginPage = new LoginPage(page);
-    await loginPage.login(adminLoginName, adminPassword);
-    await expect(
-      page.getByRole('navigation', { name: 'breadcrumbs' }).getByText('Orgs'),
-    ).toBeVisible();
+test(
+  'Verify new auth-method can be created and assigned to users',
+  { tag: ['@ce', '@ent', '@aws', '@docker'] },
+  async ({
+    page,
+    baseURL,
+    adminAuthMethodId,
+    adminLoginName,
+    adminPassword,
+  }) => {
+    await page.goto('/');
+    let orgName;
+    let authMethodName;
+    try {
+      // Log in
+      const loginPage = new LoginPage(page);
+      await loginPage.login(adminLoginName, adminPassword);
+      await expect(
+        page.getByRole('navigation', { name: 'breadcrumbs' }).getByText('Orgs'),
+      ).toBeVisible();
 
-    // Create a new password auth method and account
-    const orgsPage = new OrgsPage(page);
-    orgName = await orgsPage.createOrg();
+      // Create a new password auth method and account
+      const orgsPage = new OrgsPage(page);
+      orgName = await orgsPage.createOrg();
 
-    const authMethodsPage = new AuthMethodsPage(page);
-    authMethodName = await authMethodsPage.createPasswordAuthMethod();
-    const username = 'test-user';
-    const password = 'password';
+      const authMethodsPage = new AuthMethodsPage(page);
+      authMethodName = await authMethodsPage.createPasswordAuthMethod();
+      const username = 'test-user';
+      const password = 'password';
 
-    await authMethodsPage.createPasswordAccount(username, password);
-    await authMethodsPage.makeAuthMethodPrimary();
-    const usersPage = new UsersPage(page);
-    await usersPage.createUser();
-    await usersPage.addAccountToUser(username);
+      await authMethodsPage.createPasswordAccount(username, password);
+      await authMethodsPage.makeAuthMethodPrimary();
+      const usersPage = new UsersPage(page);
+      await usersPage.createUser();
+      await usersPage.addAccountToUser(username);
 
-    // Log in using new account
-    await loginPage.logout(adminLoginName);
-    await page.getByText('Choose a different scope').click();
-    await page.getByRole('link', { name: orgName }).click();
-    await page.getByRole('link', { name: authMethodName }).click();
+      // Log in using new account
+      await loginPage.logout(adminLoginName);
+      await page.getByText('Choose a different scope').click();
+      await page.getByRole('link', { name: orgName }).click();
+      await page.getByRole('link', { name: authMethodName }).click();
 
-    await loginPage.login(username, password);
-    await expect(
-      page
-        .getByRole('navigation', { name: 'breadcrumbs' })
-        .getByText('Projects'),
-    ).toBeVisible();
+      await loginPage.login(username, password);
+      await expect(
+        page
+          .getByRole('navigation', { name: 'breadcrumbs' })
+          .getByText('Projects'),
+      ).toBeVisible();
 
-    // Log back in as admin
-    await loginPage.logout(username);
-    await loginPage.login(adminLoginName, adminPassword);
+      // Log back in as admin
+      await loginPage.logout(username);
+      await loginPage.login(adminLoginName, adminPassword);
 
-    // Set a new password on the account
-    await page
-      .getByRole('navigation', { name: 'General' })
-      .getByRole('link', { name: 'Orgs' })
-      .click();
-    await page.getByRole('link', { name: orgName }).click();
-    await expect(
-      page.getByRole('navigation', { name: 'breadcrumbs' }).getByText(orgName),
-    ).toBeVisible();
-    await page
-      .getByRole('navigation', { name: 'IAM' })
-      .getByRole('link', { name: 'Auth Methods' })
-      .click();
-    await page.getByRole('link', { name: authMethodName }).click();
-    await page.getByRole('link', { name: 'Accounts', exact: true }).click();
-    await page.getByRole('link', { name: username }).click();
-    const newPassword = 'new-password';
-    await authMethodsPage.setPasswordToAccount(newPassword);
+      // Set a new password on the account
+      await page
+        .getByRole('navigation', { name: 'General' })
+        .getByRole('link', { name: 'Orgs' })
+        .click();
+      await page.getByRole('link', { name: orgName }).click();
+      await expect(
+        page
+          .getByRole('navigation', { name: 'breadcrumbs' })
+          .getByText(orgName),
+      ).toBeVisible();
+      await page
+        .getByRole('navigation', { name: 'IAM' })
+        .getByRole('link', { name: 'Auth Methods' })
+        .click();
+      await page.getByRole('link', { name: authMethodName }).click();
+      await page.getByRole('link', { name: 'Accounts', exact: true }).click();
+      await page.getByRole('link', { name: username }).click();
+      const newPassword = 'new-password';
+      await authMethodsPage.setPasswordToAccount(newPassword);
 
-    // Log in using new password
-    await loginPage.logout(adminLoginName);
+      // Log in using new password
+      await loginPage.logout(adminLoginName);
 
-    await page.getByText('Choose a different scope').click();
-    await page.getByRole('link', { name: orgName }).click();
-    await page.getByRole('link', { name: authMethodName }).click();
-    await loginPage.login(username, newPassword);
-    await expect(
-      page
-        .getByRole('navigation', { name: 'breadcrumbs' })
-        .getByText('Projects'),
-    ).toBeVisible();
-  } finally {
-    await boundaryCli.authenticateBoundary(
-      baseURL,
-      adminAuthMethodId,
-      adminLoginName,
-      adminPassword,
-    );
-
-    // There is an issue with deleting an org that has an auth method unless you
-    // delete the auth method first
-    if (authMethodName) {
-      const authMethods = JSON.parse(
-        execSync('boundary auth-methods list --recursive -format json'),
+      await page.getByText('Choose a different scope').click();
+      await page.getByRole('link', { name: orgName }).click();
+      await page.getByRole('link', { name: authMethodName }).click();
+      await loginPage.login(username, newPassword);
+      await expect(
+        page
+          .getByRole('navigation', { name: 'breadcrumbs' })
+          .getByText('Projects'),
+      ).toBeVisible();
+    } finally {
+      await boundaryCli.authenticateBoundary(
+        baseURL,
+        adminAuthMethodId,
+        adminLoginName,
+        adminPassword,
       );
-      const authMethod = authMethods.items.filter(
-        (obj) => obj.name == authMethodName,
-      )[0];
-      if (authMethod) {
-        try {
-          execSync('boundary auth-methods delete -id=' + authMethod.id);
-        } catch (e) {
-          console.log(`${e.stderr}`);
+
+      // There is an issue with deleting an org that has an auth method unless you
+      // delete the auth method first
+      if (authMethodName) {
+        const authMethods = JSON.parse(
+          execSync('boundary auth-methods list --recursive -format json'),
+        );
+        const authMethod = authMethods.items.filter(
+          (obj) => obj.name == authMethodName,
+        )[0];
+        if (authMethod) {
+          try {
+            execSync('boundary auth-methods delete -id=' + authMethod.id);
+          } catch (e) {
+            console.log(`${e.stderr}`);
+          }
+        }
+      }
+
+      if (orgName) {
+        const orgId = await boundaryCli.getOrgIdFromName(orgName);
+        if (orgId) {
+          await boundaryCli.deleteScope(orgId);
         }
       }
     }
-
-    if (orgName) {
-      const orgId = await boundaryCli.getOrgIdFromName(orgName);
-      if (orgId) {
-        await boundaryCli.deleteScope(orgId);
-      }
-    }
-  }
-});
+  },
+);
