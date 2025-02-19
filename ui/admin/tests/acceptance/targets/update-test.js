f6a624b1250756ac55589761063a1ec8e382b128
Lauren <lauren.burwell@hashicorp.com>
2025-02-18 20:24:19 -0500

    revert targets changes

--- ui/admin/tests/acceptance/targets/update-test.js
+++ ui/admin/tests/acceptance/targets/update-test.js
@@ -119,7 +119,7 @@ module('Acceptance | targets | update', function (hooks) {
     assert
       .dom(commonSelectors.ALERT_TOAST_BODY)
       .hasText('The request was invalid.');
-    assert.dom(commonSelectors.FIELD_TEXT_ERROR).hasText('Name is required.');
+    assert.dom('.hds-form-error__message').hasText('Name is required.');
   });
 
   test('can discard unsaved target changes via dialog', async function (assert) {
