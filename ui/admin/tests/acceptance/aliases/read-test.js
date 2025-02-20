4b0ba14731a18b43995ef686a3578886d7fba21c
Lauren <lauren.burwell@hashicorp.com>
2025-02-20 10:09:47 -0500

    fix test failure

--- ui/admin/tests/acceptance/aliases/read-test.js
+++ ui/admin/tests/acceptance/aliases/read-test.js
@@ -49,7 +49,7 @@ module('Acceptance | aliases | read', function (hooks) {
     await authenticateSession({ username: 'admin' });
   });
 
-  test('visiting a alias', async function (assert) {
+  test('visiting an alias', async function (assert) {
     await visit(urls.globalScope);
     await a11yAudit();
 
@@ -61,14 +61,14 @@ module('Acceptance | aliases | read', function (hooks) {
     assert.strictEqual(currentURL(), urls.alias);
   });
 
-  test('cannot navigate to a alias without proper authorization', async function (assert) {
+  test('cannot navigate to an alias without proper authorization', async function (assert) {
     await visit(urls.globalScope);
     instances.alias.authorized_actions =
       instances.alias.authorized_actions.filter((item) => item !== 'read');
 
     await click(commonSelectors.HREF(urls.aliases));
 
-    assert.dom(commonSelectors.TABLE_RESOURCE_LINK).doesNotExist();
+    assert.dom(commonSelectors.TABLE_RESOURCE_LINK(urls.alias)).doesNotExist();
   });
 
   test('visiting an unknown alias displays 404 message', async function (assert) {
