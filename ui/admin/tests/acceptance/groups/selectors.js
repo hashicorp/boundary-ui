e2c77bc5db2e5baddb2a82e3f3c6c040fac3ca7d
Dharini <dharini@hashicorp.com>
2025-03-07 14:44:51 -0800

    test: 💍 use test selectors for groups

---
+++ ui/admin/tests/acceptance/groups/selectors.js
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+export const MANAGE_DROPDOWN_SELECTOR =
+  '[data-test-manage-group-dropdown] button:first-child';
+export const DELETE_ACTION_SELECTOR =
+  '[data-test-manage-group-dropdown] ul li button';
