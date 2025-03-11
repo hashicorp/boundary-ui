6d6fa6333dbeb41a255db7e3b48e4a872153b09f
Dharini <dharini@hashicorp.com>
2025-03-10 17:25:21 -0700

    refactor: 💡 use test selector in groups/members

--- ui/admin/tests/acceptance/groups/selectors.js
+++ ui/admin/tests/acceptance/groups/selectors.js
@@ -6,3 +6,10 @@
 export const MANAGE_DROPDOWN =
   '[data-test-manage-group-dropdown] button:first-child';
 export const DELETE_ACTION = '[data-test-manage-group-dropdown] ul li button';
+
+// Members
+export const MEMBERS_MANAGE_DROPDOWN =
+  '[data-test-manage-groups-members] button:first-child';
+export const ADD_MEMBERS_ACTION = '[data-test-manage-group-dropdown] ul li a';
+export const DELETE_MEMBER_ACTION =
+  '[data-test-manage-groups-members] ul li button';
