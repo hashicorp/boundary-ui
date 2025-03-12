d212cf7c40f699b3139a60fb662d869f400f33d6
Priya Patel <priya.patel@hashicorp.com>
2025-03-12 16:49:38 -0700

    Refactor: 💡 User acceptance tests - create, delete and read (#2718)
    * refactor: 💡 user - create, delete and read tests
    
    * fix: 🐛 removed 'this' usage, updated user create test

---
+++ ui/admin/tests/acceptance/users/selectors.js
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+export const MANAGE_DROPDOWN_USER_DELETE =
+  '[data-test-manage-user-dropdown] ul li button';
+export const MANAGE_DROPDOWN_USER =
+  '[data-test-manage-user-dropdown] button:first-child';
