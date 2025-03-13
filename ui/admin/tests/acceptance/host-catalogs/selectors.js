90c67259bd5cbea31cbd6ece8d3b43dba4185a91
Dharini <dharini@hashicorp.com>
2025-03-13 09:56:30 -0700

    refactor: 💡 selectors

---
+++ ui/admin/tests/acceptance/host-catalogs/selectors.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+export const FIELD_DESCRIPTION = '[name="description"]';
+export const FIELD_DESCRIPTION_VALUE = 'random description';
+export const FIELD_TYPE = '[name="Type"]';
+export const FIELD_WORKER_FILTER = '[name=worker_filter]';
+export const FIELD_ZONE = '[name="zone"]';
+export const FIELD_ZONE_VALUE = 'us-west-1';
+export const FIELD_PROJECT = '[name="project_id"]';
+export const FIELD_PROJECT_VALUE = 'project ID';
+export const FIELD_CLIENT_EMAIL = '[name="client_email"]';
+export const FIELD_CLIENT_EMAIL_VALUE = 'email';
