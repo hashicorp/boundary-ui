2d3ee26c3f218e04e61d033ec12398e1510d635c
Zhihe Li <zhihe.li@hashicorp.com>
2025-03-14 17:25:18 -0400

    feat: 🎸 Use task for aliases page

--- ui/admin/app/routes/scopes/scope/aliases/index.js
+++ ui/admin/app/routes/scopes/scope/aliases/index.js
@@ -6,6 +6,7 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 import { hash } from 'rsvp';
+import { restartableTask, timeout } from 'ember-concurrency';
 
 export default class ScopesScopeAliasesIndexRoute extends Route {
   // =services
@@ -34,45 +35,57 @@ export default class ScopesScopeAliasesIndexRoute extends Route {
    * Loads queried aliases and the number of aliases under current scope.
    * @returns {Promise<{totalItems: number, aliases: [object], aliasesExist: boolean}>}
    */
-  async model({ search, page, pageSize }) {
-    const scope = this.modelFor('scopes.scope');
-    const { id: scope_id } = scope;
-
-    let aliases;
-    let doAliasesExist = false;
-    let totalItems = 0;
-
-    if (
-      this.can.can('list model', scope, {
-        collection: 'aliases',
-      })
-    ) {
-      aliases = await this.store.query('alias', {
-        scope_id,
-        query: { search },
-        page,
-        pageSize,
-      });
-      totalItems = aliases.meta?.totalItems;
-      // since we don't receive target info from aliases list API,
-      // we query the store to fetch target information based on the destination id
-      aliases = await Promise.all(
-        aliases.map((alias) =>
-          hash({
-            alias,
-            target: alias.destination_id
-              ? this.store.findRecord('target', alias.destination_id, {
-                  backgroundReload: false,
-                })
-              : null,
-          }),
-        ),
-      );
-      doAliasesExist = await this.getDoAliasesExist(scope_id, totalItems);
-    }
-
-    return { aliases, doAliasesExist, totalItems };
+  async model(params) {
+    const useDebounce =
+      this.retrieveData?.lastPerformed?.args?.[0].search !== params.search;
+    return this.retrieveData.perform({ ...params, useDebounce });
   }
+
+  retrieveData = restartableTask(
+    async ({ search, page, pageSize, useDebounce }) => {
+      if (useDebounce) {
+        await timeout(250);
+      }
+
+      const scope = this.modelFor('scopes.scope');
+      const { id: scope_id } = scope;
+
+      let aliases;
+      let doAliasesExist = false;
+      let totalItems = 0;
+
+      if (
+        this.can.can('list model', scope, {
+          collection: 'aliases',
+        })
+      ) {
+        aliases = await this.store.query('alias', {
+          scope_id,
+          query: { search },
+          page,
+          pageSize,
+        });
+        totalItems = aliases.meta?.totalItems;
+        // since we don't receive target info from aliases list API,
+        // we query the store to fetch target information based on the destination id
+        aliases = await Promise.all(
+          aliases.map((alias) =>
+            hash({
+              alias,
+              target: alias.destination_id
+                ? this.store.findRecord('target', alias.destination_id, {
+                    backgroundReload: false,
+                  })
+                : null,
+            }),
+          ),
+        );
+        doAliasesExist = await this.getDoAliasesExist(scope_id, totalItems);
+      }
+
+      return { aliases, doAliasesExist, totalItems };
+    },
+  );
 
   /**
    * Sets doAliasesExist to true if there are any aliases.
