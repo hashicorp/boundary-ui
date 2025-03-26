76b6769a0221d4a4d8e04a6b63a6b6a97d9db0e5
Lisbet Alvarez <lisbet.alvarez@hashicorp.com>
2025-03-26 14:21:09 -0700

    fix: 🐛 unique record id error bug fix using store.push
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-16544

--- ui/admin/app/routes/scopes/scope.js
+++ ui/admin/app/routes/scopes/scope.js
@@ -6,6 +6,7 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 import { A } from '@ember/array';
+import { TYPE_SCOPE_GLOBAL, TYPE_SCOPE_ORG } from 'api/models/scope';
 
 export default class ScopesScopeRoute extends Route {
   // =services
@@ -28,7 +29,14 @@ export default class ScopesScopeRoute extends Route {
   async model({ scope_id: id }) {
     // Since only global and org scopes are authenticatable, we can infer type
     // from ID because global has a fixed ID.
-    return this.store.findRecord('scope', id);
+    return this.store.findRecord('scope', id).catch(() => {
+      const type = id === 'global' ? TYPE_SCOPE_GLOBAL : TYPE_SCOPE_ORG;
+      const maybeExistingScope = this.store.peekRecord('scope', id);
+      return (
+        maybeExistingScope ||
+        this.store.push({ data: { id, type: 'scope', attributes: { type } } })
+      );
+    });
   }
 
   /**
