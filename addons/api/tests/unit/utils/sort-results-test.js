872259887242e1d93ced670dfb170c1840376084
calcaide <9775006+calcaide@users.noreply.github.com>
2025-05-02 14:01:34 +0000

    Add missing copyright headers

--- addons/api/tests/unit/utils/sort-results-test.js
+++ addons/api/tests/unit/utils/sort-results-test.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
 import { module, test } from 'qunit';
 import { sortResults } from 'api/utils/sort-results';
 import { faker } from '@faker-js/faker';
