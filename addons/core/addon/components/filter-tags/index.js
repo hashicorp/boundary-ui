b5685a06f931d92ef2e8559beeece1081bb1089b
Dharini <dharini@hashicorp.com>
2025-05-09 14:36:47 -0700

    undo filter change

--- addons/core/addon/components/filter-tags/index.js
+++ addons/core/addon/components/filter-tags/index.js
@@ -15,20 +15,13 @@ export default class FilterTagsIndexComponent extends Component {
 
   // =attributes
 
-
-  /**
-   * Get the filters based on the selected filters and all filters.
-   * @returns {Array} An array of filter objects with id, name, and type properties.
-   * if isCustomFilter is true, it returns the selected filters directly for the customer to process further.
-   */
   get filters() {
     const { allFilters, selectedFilters } = this.args.filters;
-
     return Object.entries(allFilters).flatMap(([key, value]) => {
       assert(`Tags must be an array for key ${key}`, Array.isArray(value));
-
       const paramsSet = new Set(selectedFilters[key]);
       const filters = value.filter((item) => paramsSet.has(item.id));
+
       return filters.map((item) => ({
         id: item.id,
         name: item.name,
