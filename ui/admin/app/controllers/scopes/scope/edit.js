c9e9328300c311dd36df2b295971fddb82449a8f
Priya Patel <priya.patel@hashicorp.com>
2025-05-15 13:02:33 -0500

    Refactor: Rose::Page::Layout - header, actions and breadcrumbs (#2813)
    * refactor: 💡 page::layout- header,breadcrumb and actions
    
    * refactor: 💡 added new class for the page layout, fixed a11y
    
    * refactor: 💡 addressed comments
    
    * refactor: 💡 moved copy snippet to PH.Generic
    
    * refactor: 💡 addressed comments

--- ui/admin/app/controllers/scopes/scope/edit.js
+++ ui/admin/app/controllers/scopes/scope/edit.js
@@ -7,4 +7,33 @@ import Controller, { inject as controller } from '@ember/controller';
 
 export default class ScopesScopeEditController extends Controller {
   @controller('scopes/scope/index') scopes;
+
+  get scopeTypes() {
+    return [
+      {
+        key: 'isGlobal',
+        title: 'resources.global.title',
+        description: 'resources.global.description',
+        doc: 'global',
+      },
+      {
+        key: 'isOrg',
+        title: 'resources.org.title',
+        description: 'resources.org.description',
+        doc: 'org',
+      },
+      {
+        key: 'isProject',
+        title: 'resources.project.title',
+        description: 'resources.project.description',
+        doc: 'project',
+      },
+      {
+        key: 'isScope',
+        title: 'resources.scope.title',
+        description: 'resources.scope.description',
+        doc: 'scope',
+      },
+    ];
+  }
 }
