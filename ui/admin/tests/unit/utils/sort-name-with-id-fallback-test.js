a714a0eee9c45d0f251696f8ba04d7e57c23bc76
lisbet-alvarez <107949262+lisbet-alvarez@users.noreply.github.com>
2025-06-02 16:51:56 +0000

    Add missing copyright headers

--- ui/admin/tests/unit/utils/sort-name-with-id-fallback-test.js
+++ ui/admin/tests/unit/utils/sort-name-with-id-fallback-test.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
 import sortNameWithIdFallback from 'admin/utils/sort-name-with-id-fallback';
 import { module, test } from 'qunit';
 
