ae9e2d42dcdbb7a0a1054a6b2437aa255e22799a
calcaide <ccorvo@hashicorp.com>
2025-06-10 09:21:50 -0700

    Add sortNameWithIdFallback to auth methods

--- ui/admin/app/routes/scopes/scope/auth-methods/index.js
+++ ui/admin/app/routes/scopes/scope/auth-methods/index.js
@@ -6,6 +6,7 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 import { restartableTask, timeout } from 'ember-concurrency';
+import { sortNameWithIdFallback } from 'admin/utils/sort-name-with-id-fallback';
 
 export default class ScopesScopeAuthMethodsIndexRoute extends Route {
   // =services
@@ -80,10 +81,13 @@ export default class ScopesScopeAuthMethodsIndexRoute extends Route {
         is_primary: [],
       };
 
-      const sort = {
-        attribute: sortAttribute,
-        direction: sortDirection,
-      };
+      const sort =
+        sortAttribute === 'name'
+          ? { sortFunction: sortNameWithIdFallback, direction: sortDirection }
+          : { attribute: sortAttribute, direction: sortDirection };
+
+      console.log('sortAttribute: ', sortAttribute);
+      console.log(sort);
 
       types.forEach((type) => {
         filters.type.push({ equals: type });
