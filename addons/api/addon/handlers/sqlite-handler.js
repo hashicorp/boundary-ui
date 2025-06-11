b137d066b451fed3215d6a178d491dbeb375d6db
Zhihe Li <zhihe.li@hashicorp.com>
2025-06-11 17:33:52 -0400

    feat: 🎸 Add initial sqlite handler

--- addons/api/addon/handlers/sqlite-handler.js
+++ addons/api/addon/handlers/sqlite-handler.js
@@ -1,6 +1,15 @@
 import { getOwner, setOwner } from '@ember/owner';
+import { service } from '@ember/service';
+import { modelMapping } from 'api/services/sqlite-db';
+import { get } from '@ember/object';
+import { typeOf } from '@ember/utils';
+import { hashCode } from '../utils/hash-code';
 
 export default class SqliteHandler {
+  @service sqliteDb;
+
+  batchLimit = 10_000;
+
   constructor(context) {
     setOwner(this, getOwner(context));
   }
@@ -8,10 +17,148 @@ export default class SqliteHandler {
   async request(context, next) {
     switch (context.request.op) {
       case 'query': {
-        return next(context.request);
+        const { store, data } = context.request;
+        const {
+          type,
+          query,
+          options: {
+            pushToStore = true,
+            // peekIndexedDB = false,
+            storeToken = true,
+          } = {},
+        } = data;
+        const supportedModels = Object.keys(modelMapping);
+
+        // Go through normal flow if we don't yet support the model
+        // or if we don't have an indexedDb instance
+        if (!supportedModels.includes(type) || !this.sqliteDb) {
+          return next(context.request);
+        }
+
+        const adapter = store.adapterFor(type);
+        const schema = store.modelFor(type);
+        const serializer = store.serializerFor(type);
+
+        let { page, pageSize, /* query: queryObj,*/ ...remainingQuery } = query;
+        let payload, listToken, writeToDbPromise;
+        const tokenKey = `${type}-${hashCode(remainingQuery)}`;
+
+        if (storeToken) {
+          const [tokenObj] = await this.sqliteDb.fetchResource('token');
+          listToken = tokenObj?.token;
+        }
+
+        do {
+          try {
+            payload = await adapter.query(store, schema, {
+              ...remainingQuery,
+              list_token: listToken,
+              batchLimit: this.batchLimit,
+            });
+
+            // await the previous writeToDbPromise before writing to db again
+            if (writeToDbPromise) {
+              await writeToDbPromise;
+            }
+          } catch (err) {
+            console.error('Error retrieving data:', err);
+            throw err;
+          }
+          listToken = payload.list_token;
+
+          writeToDbPromise = this.writeToDb(
+            payload,
+            storeToken,
+            tokenKey,
+            serializer,
+            store,
+            schema,
+            type,
+          );
+        } while (payload.response_type === 'delta');
+
+        await writeToDbPromise;
+
+        console.time('SQLite fetch');
+
+        const rowsPromise = this.sqliteDb.fetchResource(type, {
+          select: 'data',
+          page,
+          pageSize,
+        });
+        const countPromise = this.sqliteDb.fetchResource('target', {
+          select: 'count(*) as total',
+        });
+        const [rows, count] = await Promise.all([rowsPromise, countPromise]);
+
+        const results = rows.map((item) => JSON.parse(item.data));
+
+        // If we are not pushing to the store, use the raw data with id property
+        const records = pushToStore
+          ? store.push({ data: results })
+          : results.map((record) => ({
+              ...record.attributes,
+              id: record.id,
+            }));
+
+        // Set a meta property on the array to store the total items of the results.
+        // This isn't conventional but is better than returning an ArrayProxy
+        // or EmberArray since the ember store query method asserts it has to be an array
+        // so we can't just return an object.
+        console.timeEnd('SQLite fetch');
+        records.meta = { totalItems: count[0].total };
+        return records;
       }
       default:
         return next(context.request);
     }
+  }
+
+  async writeToDb(
+    payload,
+    storeToken,
+    tokenKey,
+    serializer,
+    store,
+    schema,
+    type,
+  ) {
+    if (payload.list_token && storeToken) {
+      await this.sqliteDb.insertResource('token', [
+        [tokenKey, payload.list_token],
+      ]);
+    }
+
+    // Remove any records from the DB if the API indicates they've been deleted
+    if (payload.removed_ids?.length > 0) {
+      await this.sqliteDb.deleteResource(type, payload.removed_ids);
+    }
+
+    const normalizedPayload = serializer.normalizeResponse(
+      store,
+      schema,
+      payload,
+      null,
+      'query',
+    );
+
+    const { data: payloadData } = normalizedPayload;
+
+    // Store the new data we just got back from the API refresh
+    const items = payloadData.map((datum) => {
+      const params = modelMapping[type].map((key) => {
+        const value = get(datum, `attributes.${key}`);
+        if (typeOf(value) === 'date') {
+          return value.toISOString();
+        }
+        return value;
+      });
+
+      return [datum.id, ...params, JSON.stringify(datum)];
+    });
+
+    if (items.length > 0) console.time(`SQLite insert`);
+    await this.sqliteDb.insertResource(type, items);
+    if (items.length > 0) console.timeEnd(`SQLite insert`);
   }
 }
