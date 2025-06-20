c33ab5d5223e6cd39c1e8e7711cc75baf84468fe
calcaide <ccorvo@hashicorp.com>
2025-06-20 13:08:30 -0700

    Refactor list test selectors

--- ui/admin/tests/acceptance/aliases/selectors.js
+++ ui/admin/tests/acceptance/aliases/selectors.js
@@ -5,3 +5,8 @@
 
 export const FIELD_DESTINATION_ID = '[name=destination_id]';
 export const FIELD_HOST_ID = '[name=authorize_session_arguments]';
+
+export const TABLE_ROW_ID_ACTION_DROPDOWN = (id) =>
+  `tbody [data-test-alias-row="${id}"] td:last-child button`;
+export const TABLE_ROW_ID_ACTION_DROPDOWN_ITEM_LINK = (id) =>
+  `tbody [data-test-alias-row="${id}"] td:last-child ul li a`;
