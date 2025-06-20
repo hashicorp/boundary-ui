a24ad31b89fa866b6546df938d37a5776471ac53
calcaide <ccorvo@hashicorp.com>
2025-06-20 11:56:20 -0700

    Refactor delete test selectors

--- ui/admin/tests/acceptance/aliases/selectors.js
+++ ui/admin/tests/acceptance/aliases/selectors.js
@@ -3,5 +3,5 @@
  * SPDX-License-Identifier: BUSL-1.1
  */
 
-export const FIELD_DESTINATION_ID = '[name="destination_id"]';
-export const FIELD_HOST_ID = '[name="authorize_session_arguments"]';
+export const FIELD_DESTINATION_ID = '[name=destination_id]';
+export const FIELD_HOST_ID = '[name=authorize_session_arguments]';
