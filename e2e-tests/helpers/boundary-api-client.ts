116fb06c75577cb42a3e7d5c7a56375af585356f
hashicc <191911133+hashicc@users.noreply.github.com>
2025-06-23 18:52:51 +0000

    Add missing copyright headers

--- e2e-tests/helpers/boundary-api-client.ts
+++ e2e-tests/helpers/boundary-api-client.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
 import { test as base } from '@playwright/test';
 import * as BoundaryApiClient from '../api-client';
 import { InitOverrideFunction } from '../api-client';
