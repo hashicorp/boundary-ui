43727afead572dda4e83a6cdb53ca89ff07e315c
hashicc <191911133+hashicc@users.noreply.github.com>
2025-07-02 13:58:00 -0400

    refactor: 💡 clean up and refactor boundary-api-client fixture

--- e2e-tests/helpers/boundary-api-client.ts
+++ e2e-tests/helpers/boundary-api-client.ts
@@ -5,20 +5,83 @@
 
 import { test as base } from '@playwright/test';
 import * as BoundaryApiClient from '../api-client';
-import { InitOverrideFunction } from '../api-client';
 
 const BoundaryE2EResourceCleanupHeader = 'X-Boundary-E2E-Resource-Cleanup';
 
-class BoundaryApi {
-  createdResources: Record<string, { id: string }[]> = {};
-  skipCleanupResources: { id: string }[] = [];
+// These constants represent assumptions around the boundary api
+const BoundaryApiStatusCodes = Object.freeze({
+  created: 200,
+  deleted: 204,
+  notFound: 404,
+});
+const BOUNDARY_API_RESOURCE_CREATED_HTTP_CONTENT_TYPE = 'application/json';
 
-  constructor(
-    private readonly controllerAddr: string
-  ) {}
+// the openapi spec generated incorrectly specifies that DELETE responses will be a 200 with a json body,
+// but they actually return a 204 "No Content" status with no body. The generated api client does not handle
+// this correctly, so we need to patch it here when it tries to access the `json` method on the response
+const patchEmptyResponsesMiddleware = {
+  async post(context: BoundaryApiClient.ResponseContext) {
+    const isBoundaryApiDeleteResponse =
+      context.response.status ===
+        BoundaryApiStatusCodes.deleted &&
+      context.response.body === null;
+
+    if (isBoundaryApiDeleteResponse) {
+      context.response.json = async () => undefined;
+    }
+
+    return context.response;
+  },
+};
+
+const betterErrorHandlingMiddleware = {
+  async post(context: BoundaryApiClient.ResponseContext) {
+    // skip logging of cleanup requests because it is common for the resource to already
+    // be cleaned up and the result of trying again will be a 404. In general, we only
+    // want to log requests that fail as part of test or test setup so that they can
+    // be debugged
+    if (context.init.headers?.[BoundaryE2EResourceCleanupHeader] === 'true') {
+      return;
+    }
+
+    const responseStatusCodeIsInErrorRange = context.response.status >= 400;
+    if (responseStatusCodeIsInErrorRange) {
+      let errorResponseMessage;
+
+      // a response can only be read once, but needs to be read by the generated api client,
+      // instead the response is cloned and then read via `text`
+      const clonedResponse = context.response.clone();
+      try {
+        errorResponseMessage = await clonedResponse.text();
+      } finally {
+        console.error(
+          `Error response from Boundary API, status code: ${clonedResponse.status}, with method: ${context.init.method}, on ${clonedResponse.url}${errorResponseMessage ? `:\n${errorResponseMessage}` : ''}`,
+        );
+      }
+    }
+    return context.response;
+  },
+};
+
+function extractDomainResourceFromUrl(url: string): string | undefined {
+  const match = new URL(url).pathname.match(/\/v1\/([a-z]+)(|\/.*)/);
+  if (!match) return undefined;
+
+  const [_, type] = match;
+  return type;
+}
+
+class BoundaryApi {
+  readonly createdResources: Record<string, { id: string }[]> = {};
+  readonly skipCleanupResources: { id: string }[] = [];
+
+  constructor(readonly controllerAddr: string) {}
 
   get clients() {
     const { openapiConfiguration } = this;
+
+    // these could might be able to be built dynamically, but by doing it this way the type annotations are
+    // preserved better for each client, and the "@link" jsdoc appear in the editor
     const clients = {
       /** {@link https://developer.hashicorp.com/boundary/docs/concepts/domain-model/roles Roles Documentation} */
       roles: new BoundaryApiClient.RoleServiceApi(openapiConfiguration),
@@ -33,28 +96,40 @@ class BoundaryApi {
     return clients;
   }
 
+  skipCleanup(identifiable: { id: string }): void {
+    this.skipCleanupResources.push(identifiable);
+  }
+
   private get openapiConfiguration() {
     const { controllerAddr, createdResources: resources } = this;
 
     const captureCreatedResourcesMiddleware = {
       async post(context) {
-        if (
-          context.response.status !== 200 ||
-          context.response.headers.get('content-type') !== 'application/json'
-        ) {
+        const resourceWasCreated =
+          context.response.status ===
+            BoundaryApiStatusCodes.created &&
+          context.response.headers.get('content-type') ===
+            BOUNDARY_API_RESOURCE_CREATED_HTTP_CONTENT_TYPE;
+
+        // if the respronse doesn't represent a created resource, return early
+        if (!resourceWasCreated) {
           return context.response;
         }
 
         const type = extractDomainResourceFromUrl(context.response.url);
+        // the response `json` can only be read once, and needs to be read later by the generated api client,
+        // instead we can clone the response and this allows us to read the `json` on the cloned response only
         const json = await context.response.clone().json();
-        if (type && typeof json?.id === 'string') {
-          if (!resources[type]) {
-            resources[type] = [];
-          }
 
+        // it's assume that json response has an `id` in order for it to be a created resource
+        if (type && typeof json?.id === 'string') {
+          resources[type] ??= [];
+          // capture the created json payload, all that's really needed is the `id` and the `type`
+          // in order to delete it later
           resources[type].push(json);
         }
 
+        // return original response
         return context.response;
       },
     };
@@ -71,97 +146,34 @@ class BoundaryApi {
 
     return openapiConfiguration;
   }
-
-  deleteMethod(
-    resourceType: string,
-  ):
-    | ((
-        args: { id: string },
-        initOverrides?: RequestInit | InitOverrideFunction,
-      ) => Promise<object>)
-    | undefined {
-    const { clients } = this;
-
-    const deleteMethods = {
-      roles: clients.roles.roleServiceDeleteRole.bind(clients.roles),
-      scopes: clients.scopes.scopeServiceDeleteScope.bind(clients.scopes),
-      targets: clients.targets.targetServiceDeleteTarget.bind(clients.targets),
-    };
-
-    return deleteMethods[resourceType];
-  }
-
-  skipCleanup(identifiable: { id: string }): void {
-    this.skipCleanupResources.push(identifiable);
-  }
-
-  shouldSkipCleanup(identifiable: { id: string }): boolean {
-    return this.skipCleanupResources.some(
-      (resource) => resource.id === identifiable.id,
-    );
-  }
-}
-
-// the openapi spec generated incorrectly specifies that POST responses will have a json body,
-// but they actually return a 204 "No Content" status with no body. The generated api client
-// does not handle this correctly, so we need to patch it here when it tries to access the `json`
-// method on the response
-const patchEmptyResponsesMiddleware = {
-  async post(context: BoundaryApiClient.ResponseContext) {
-    if (context.response.status === 204 && context.response.body === null) {
-      context.response.json = async () => undefined;
-    }
-
-    return context.response;
-  },
-};
-
-const betterErrorHandlingMiddleware = {
-  async post(context: BoundaryApiClient.ResponseContext) {
-    // skip logging of cleanup requests
-    if (context.init.headers?.[BoundaryE2EResourceCleanupHeader] === 'true') {
-      return;
-    }
-
-    if (context.response.status >= 400 && context.response.status < 600) {
-      let errorResponseMessage;
-      const clonedResponse = context.response.clone();
-      try {
-        errorResponseMessage = await clonedResponse.text();
-      } finally {
-        console.error(
-          `Received status code: ${clonedResponse.status}, with method: ${context.init.method}, on ${clonedResponse.url}${errorResponseMessage ? `:\n${errorResponseMessage}` : ''}`,
-        );
-      }
-    }
-    return context.response;
-  },
-};
-
-function extractDomainResourceFromUrl(url: string): string | undefined {
-  const match = new URL(url).pathname.match(/\/v1\/([a-z]+)(|\/.*)/);
-  if (!match) return undefined;
-
-  const [_, type] = match;
-  return type;
 }
 
 export const boundaryApiClientTest = base.extend<{
   apiClient: BoundaryApi;
   controllerAddr?: string;
 }>({
-  apiClient: async ({ request, controllerAddr }, use) => {
+  apiClient: async ({ controllerAddr }, use) => {
     if (!controllerAddr) {
       throw new Error('`controllerAddr` must be set in the base fixture');
     }
 
     const boundaryApi = new BoundaryApi(controllerAddr);
+
+    // what is passed to `use` is what is provided by the fixture
     await use(boundaryApi);
+    // anything after awaiting `use` is ran after the test that uses the fixture has ran
+
+    const { clients } = boundaryApi;
+    const deleteMethods = {
+      roles: clients.roles.roleServiceDeleteRole.bind(clients.roles),
+      scopes: clients.scopes.scopeServiceDeleteScope.bind(clients.scopes),
+      targets: clients.targets.targetServiceDeleteTarget.bind(clients.targets),
+    };
 
     for (const [resourceType, resources] of Object.entries(
       boundaryApi.createdResources,
     )) {
-      const resouceDeleteMethod = boundaryApi.deleteMethod(resourceType);
+      const resouceDeleteMethod = deleteMethods[resourceType];
 
       if (!resouceDeleteMethod) {
         throw new Error(
@@ -171,11 +183,15 @@ export const boundaryApiClientTest = base.extend<{
 
       await Promise.all(
         resources.map((resource) => {
-          if (boundaryApi.shouldSkipCleanup(resource)) {
+          const shouldSkipResource = boundaryApi.skipCleanupResources.some(
+            (r) => r.id === resource.id,
+          );
+
+          if (shouldSkipResource) {
             return;
           }
 
-          const initOverrides = ({ init }) => {
+          const initOverride = ({ init }) => {
             return {
               ...init,
               headers: {
@@ -185,16 +201,16 @@ export const boundaryApiClientTest = base.extend<{
             };
           };
 
-          return resouceDeleteMethod({ id: resource.id }, initOverrides).catch(
-            (e) => {
+          return resouceDeleteMethod({ id: resource.id }, initOverride).catch(
+            (error) => {
               // if the resource is not found it was likely already deleted and the error can be ignored
-              if (e.response?.status === 404) {
+              if (error.response?.status === BoundaryApiStatusCodes.notFound) {
                 return;
               }
 
               console.warn(
                 `Failed to clean up resource of type ${resourceType} with id ${resource.id}:`,
-                e,
+                error,
               );
             },
           );
