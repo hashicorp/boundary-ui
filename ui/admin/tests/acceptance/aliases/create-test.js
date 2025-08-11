ce336e7d34c37878fd96906f19ef257a9eacae43
Zhihe Li <zhihe.li@hashicorp.com>
2025-08-11 15:57:58 -0400

    feat: 🎸 Add sqlite support for aliases (#2957)
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-17420

--- ui/admin/tests/acceptance/aliases/create-test.js
+++ ui/admin/tests/acceptance/aliases/create-test.js
@@ -4,19 +4,21 @@
  */
 
 import { module, test } from 'qunit';
-import { visit, currentURL, click, fillIn } from '@ember/test-helpers';
+import { click, currentURL, fillIn, visit } from '@ember/test-helpers';
 import { setupApplicationTest } from 'admin/tests/helpers';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import a11yAudit from 'ember-a11y-testing/test-support/audit';
 import { Response } from 'miragejs';
 import { authenticateSession } from 'ember-simple-auth/test-support';
 import { setupIndexedDb } from 'api/test-support/helpers/indexed-db';
+import { setupSqlite } from 'api/test-support/helpers/sqlite';
 import * as commonSelectors from 'admin/tests/helpers/selectors';
 import * as selectors from './selectors';
 
 module('Acceptance | aliases | create', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
+  setupSqlite(hooks);
   setupIndexedDb(hooks);
 
   let features;
