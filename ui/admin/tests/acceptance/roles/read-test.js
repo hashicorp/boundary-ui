826d9885de20efc0d4b8da856b12400e286257f4
Zhihe Li <zhihe.li@hashicorp.com>
2025-08-15 14:54:03 -0400

    feat: 🎸 Add sqlite support for roles
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-17414

--- ui/admin/tests/acceptance/roles/read-test.js
+++ ui/admin/tests/acceptance/roles/read-test.js
@@ -8,6 +8,7 @@ import { visit, currentURL, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'admin/tests/helpers';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import { setupIndexedDb } from 'api/test-support/helpers/indexed-db';
+import { setupSqlite } from 'api/test-support/helpers/sqlite';
 import a11yAudit from 'ember-a11y-testing/test-support/audit';
 import { authenticateSession } from 'ember-simple-auth/test-support';
 import * as commonSelectors from 'admin/tests/helpers/selectors';
@@ -15,6 +16,7 @@ import * as commonSelectors from 'admin/tests/helpers/selectors';
 module('Acceptance | roles | read', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
+  setupSqlite(hooks);
   setupIndexedDb(hooks);
 
   const instances = {
