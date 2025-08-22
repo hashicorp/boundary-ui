48d9aa6b08b8da2db94f4a5bb3f08151d9b4ff4f
Zhihe Li <zhihe.li@hashicorp.com>
2025-08-22 11:38:12 -0400

    feat: 🎸 Add sqlite support for credential stores (#2967)
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-17416

--- addons/api/addon/services/sqlite.js
+++ addons/api/addon/services/sqlite.js
@@ -49,10 +49,24 @@ export const modelMapping = {
     scope_id: 'scope.scope_id',
     created_time: 'created_time',
   },
+  'credential-store': {
+    id: 'id',
+    type: 'type',
+    name: 'name',
+    description: 'description',
+    scope_id: 'scope.scope_id',
+    created_time: 'created_time',
+  },
 };
 
 // A list of tables that we support searching using FTS5 in SQLite.
-export const searchTables = new Set(['target', 'alias', 'role', 'user']);
+export const searchTables = new Set([
+  'target',
+  'alias',
+  'role',
+  'user',
+  'credential-store',
+]);
 
 export default class SqliteDbService extends Service {
   // =attributes
