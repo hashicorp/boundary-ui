d2bff04915c6a5f4895908677940305c997e75cf
hashicc <191911133+hashicc@users.noreply.github.com>
2025-09-29 15:58:52 -0400

    refactor: 💡 migrate away from component helper for cred library form

---
+++ ui/admin/app/components/form/credential-library/index.js
@@ -0,0 +1,28 @@
+import Component from '@glimmer/component';
+import { assert } from '@ember/debug';
+import {
+  TYPE_CREDENTIAL_LIBRARY_VAULT_GENERIC,
+  TYPE_CREDENTIAL_LIBRARY_VAULT_SSH_CERTIFICATE,
+  TYPE_CREDENTIAL_LIBRARY_VAULT_LDAP,
+} from 'api/models/credential-library';
+import VaultGenericFormComponent from './vault-generic';
+import VaultSshCertificateFormComponent from './vault-ssh-certificate';
+import VaultLdapFormComponent from './vault-ldap';
+
+const modelTypeToComponent = {
+  [TYPE_CREDENTIAL_LIBRARY_VAULT_GENERIC]: VaultGenericFormComponent,
+  [TYPE_CREDENTIAL_LIBRARY_VAULT_SSH_CERTIFICATE]:
+    VaultSshCertificateFormComponent,
+  [TYPE_CREDENTIAL_LIBRARY_VAULT_LDAP]: VaultLdapFormComponent,
+};
+
+export default class FormCredentialLibraryIndex extends Component {
+  get credentialLibraryFormComponent() {
+    const component = modelTypeToComponent[this.args.model.type];
+    assert(
+      `Mapped component must exist for credential library type: ${this.args.model.type}`,
+      component,
+    );
+    return component;
+  }
+}
