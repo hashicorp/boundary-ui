6a6d53b2c1467eb709ba2fb7cdbd7a381795351d
ZedLi <5783847+ZedLi@users.noreply.github.com>
2025-10-24 21:42:16 +0000

    Add missing copyright headers

--- addons/api/addon/services/db.js
+++ addons/api/addon/services/db.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
 import Service, { service } from '@ember/service';
 import { modelMapping } from 'api/services/sqlite';
 import { assert } from '@ember/debug';
