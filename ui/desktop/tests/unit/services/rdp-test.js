e749900980bb508c9ba93a4aad11ab31975ddde2
Priya Patel <priya.patel@hashicorp.com>
2025-10-24 12:58:50 -0500

    Create new RDP service (#3029)
    * feat: 🎸 added a new RDP service
    
    * fix: 🐛 rdp test
    
    * refactor: 💡 update error handling for fetching rdp clients
    
    * refactor: 💡 addressed comments
    
    * fix: 🐛 revert private field
    
    * refactor: 💡 addressed comments
    
    * refactor: 💡 updated rdp clients to array of strings

---
+++ ui/desktop/tests/unit/services/rdp-test.js
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import { module, test } from 'qunit';
+import { setupTest } from 'desktop/tests/helpers';
+import sinon from 'sinon';
+
+module('Unit | Service | rdp', function (hooks) {
+  setupTest(hooks);
+
+  let service, ipcService;
+
+  hooks.beforeEach(function () {
+    service = this.owner.lookup('service:rdp');
+    ipcService = this.owner.lookup('service:ipc');
+  });
+
+  test('getRdpClients sets to fallback value on error', async function (assert) {
+    sinon.stub(ipcService, 'invoke').withArgs('getRdpClients').rejects();
+    await service.getRdpClients();
+    assert.deepEqual(
+      service.rdpClients,
+      ['none'],
+      'rdpClients fallback is set correctly',
+    );
+  });
+
+  test('getPreferredRdpClient sets to fallback value on error', async function (assert) {
+    sinon
+      .stub(ipcService, 'invoke')
+      .withArgs('getPreferredRdpClient')
+      .rejects();
+    await service.getPreferredRdpClient();
+    assert.strictEqual(
+      service.preferredRdpClient,
+      'none',
+      'preferredRdpClient fallback is set correctly',
+    );
+  });
+
+  test('setPreferredRdpClient sets to fallback value on error', async function (assert) {
+    sinon
+      .stub(ipcService, 'invoke')
+      .withArgs('setPreferredRdpClient', 'mstsc')
+      .rejects();
+    await service.setPreferredRdpClient('mstsc');
+    assert.strictEqual(
+      service.preferredRdpClient,
+      'none',
+      'preferredRdpClient fallback is set correctly',
+    );
+  });
+});
