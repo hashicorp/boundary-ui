a123b6c4b444de8068a04b552fdafd2d5fe6e0f8
Lisbet Alvarez <lisbet.alvarez@hashicorp.com>
2025-10-24 15:31:56 -0700

    refactor: 💡 move scope picker to core addon

---
+++ addons/core/app/components/scope-picker/index.js
@@ -0,0 +1,1 @@
+export { default } from 'core/components/scope-picker/index';
--- addons/core/tests/unit/utils/param-value-finder-test.js
+++ addons/core/tests/unit/utils/param-value-finder-test.js
@@ -10,12 +10,6 @@ import { paramValueFinder } from 'core/utils/param-value-finder';
 module('Unit | Utility | param-value-finder', function (hooks) {
   setupTest(hooks);
 
-  let router;
-
-  hooks.beforeEach(function () {
-    router = this.owner.lookup('service:router');
-  });
-
   test('returns empty array when null or empty routeInfo object is used', function (assert) {
     let result = paramValueFinder('scopes', null);
 
@@ -27,13 +21,35 @@ module('Unit | Utility | param-value-finder', function (hooks) {
   });
 
   test('returns correct array when routeInfo object is used', function (assert) {
-    let routeInfo = router.recognize('/scopes');
-    let result = paramValueFinder('scopes', routeInfo.parent);
+    // Route being tested is '/scopes'
+    const application = { localName: 'application', params: {}, parent: null };
+    const scopes = { localName: 'scopes', params: {}, parent: application };
+    const scopesIndex = {
+      localName: 'index',
+      name: 'scopes.index',
+      parent: scopes,
+    };
+    let result = paramValueFinder('scopes', scopesIndex.parent);
 
     assert.deepEqual(result, []);
 
-    routeInfo = router.recognize('/scopes/global/auth-methods');
-    result = paramValueFinder('scopes', routeInfo.parent);
+    // Route being tested is '/scopes/global/auth-methods'
+    const scope = {
+      localName: 'scope',
+      params: { scope_id: 'global' },
+      parent: scopes,
+    };
+    const authMethods = {
+      localName: 'auth-methods',
+      params: {},
+      parent: scope,
+    };
+    const authMethodsIndex = {
+      localName: 'index',
+      name: 'scopes.scope.auth-methods.index',
+      parent: authMethods,
+    };
+    result = paramValueFinder('scopes', authMethodsIndex.parent);
     assert.deepEqual(result, ['global']);
   });
 });
