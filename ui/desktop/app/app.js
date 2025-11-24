2b272aef579bff941cc3d3ea9d04f5b26b9089ec
hashicc <191911133+hashicc@users.noreply.github.com>
2025-11-24 16:29:18 -0500

    fix: 🐛 ensure that auto mirage start doesn't happen in test env

--- ui/desktop/app/app.js
+++ ui/desktop/app/app.js
@@ -7,7 +7,12 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
-import { macroCondition, getOwnConfig, importSync } from '@embroider/macros';
+import {
+  macroCondition,
+  getOwnConfig,
+  importSync,
+  isTesting,
+} from '@embroider/macros';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
@@ -17,7 +22,7 @@ export default class App extends Application {
 
 loadInitializers(App, config.modulePrefix);
 
-if (macroCondition(getOwnConfig().startMirageWithApp)) {
+if (macroCondition(getOwnConfig().startMirageWithApp && !isTesting())) {
   const startServer = importSync('api/mirage/config').default;
   startServer({});
 }
