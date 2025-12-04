54fbb94ca0243264c0594f5758b5b1a4af1eda72
Lisbet Alvarez <107949262+lisbet-alvarez@users.noreply.github.com>
2025-12-04 09:26:46 -0800

    feat: 🎸 Add scope options component (#3092)
    * feat: 🎸 Add scope options component
    
    * Add missing copyright headers
    
    * refactor: 💡 update translations for keywords
    
    * refactor: 💡 fix broken translation
    
    * fix: 🐛 parent scope column
    
    * refactor: 💡 add missing getter
    
    * refactor: 💡 pr feedback + update refresh logic
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-18128

---
+++ ui/admin/app/components/scope-options/index.js
@@ -0,0 +1,146 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import Component from '@glimmer/component';
+import { service } from '@ember/service';
+import { action } from '@ember/object';
+import {
+  GRANT_SCOPE_THIS,
+  GRANT_SCOPE_CHILDREN,
+  GRANT_SCOPE_DESCENDANTS,
+  GRANT_SCOPE_KEYWORDS,
+} from 'api/models/role';
+
+export default class ScopeOptionsIndexComponent extends Component {
+  // =services
+
+  @service router;
+
+  // =attributes
+
+  keywords = {
+    keyThis: GRANT_SCOPE_THIS,
+    keyChildren: GRANT_SCOPE_CHILDREN,
+    keyDescendants: GRANT_SCOPE_DESCENDANTS,
+  };
+
+  /**
+   * Returns root scope displayName.
+   * @type {string}
+   */
+  get scopeDisplayName() {
+    if (this.args.model.scope.isGlobal) {
+      return 'Global';
+    }
+    return this.args.model.scope.displayName;
+  }
+
+  /**
+   * Returns selected grant scope ids.
+   * @type {array}
+   */
+  get grantScopeIds() {
+    const field = this.args.field || this.args.model;
+    return field[this.args.name];
+  }
+
+  /**
+   * Returns count of custom scopes selected.
+   * @type {number}
+   */
+  get customScopesSelectionTotal() {
+    const customScopes = this.grantScopeIds?.filter(
+      (scope) => !GRANT_SCOPE_KEYWORDS.includes(scope),
+    );
+    return customScopes?.length;
+  }
+
+  /**
+   * Returns true if global role does not have "descendants" toggled on
+   * or if org role does not have "children" toggled on.
+   * @type {boolean}
+   */
+  get allowCustomScopesSelection() {
+    return (
+      (this.args.model.scope.isGlobal &&
+        !this.grantScopeIds?.includes(GRANT_SCOPE_DESCENDANTS)) ||
+      (this.args.model.scope.isOrg &&
+        !this.grantScopeIds?.includes(GRANT_SCOPE_CHILDREN))
+    );
+  }
+
+  // =actions
+
+  /**
+   * Handles toggle event changes for selected scopes.
+   * @param {object} event
+   */
+  @action
+  async toggleField(event) {
+    const { checked, value } = event.target;
+    const field = this.args.field || this.args.model;
+    if (!field[this.args.name]) {
+      field[this.args.name] = [];
+    }
+    const removeValues = (values) => {
+      field[this.args.name] = field[this.args.name].filter(
+        (item) => !values.some((value) => item.startsWith(value)),
+      );
+    };
+    if (checked) {
+      field[this.args.name] = [...field[this.args.name], value];
+      if (value === GRANT_SCOPE_CHILDREN) {
+        if (this.args.model.scope.isGlobal) {
+          removeValues([GRANT_SCOPE_DESCENDANTS, 'o_']);
+          await this.updateDisplayedData();
+        } else {
+          removeValues(['p_']);
+        }
+      }
+      if (value === GRANT_SCOPE_DESCENDANTS) {
+        removeValues([GRANT_SCOPE_CHILDREN, 'o_', 'p_']);
+        if (this.args.model.scope.isGlobal) {
+          await this.updateDisplayedData();
+        }
+      }
+    } else {
+      removeValues([value]);
+      if (this.args.model.scope.isGlobal && value === GRANT_SCOPE_CHILDREN) {
+        await this.updateDisplayedData();
+      }
+    }
+  }
+
+  /**
+   * Handles the selection changes for the paginated table.
+   * @param {object} selectableRowsStates
+   */
+  @action
+  selectionChange({ selectableRowsStates, selectedRowsKeys }) {
+    const field = this.args.field || this.args.model;
+    selectableRowsStates.forEach((row) => {
+      const { isSelected, selectionKey: key } = row;
+      const includesId = field[this.args.name].includes(key);
+      if (isSelected && !includesId) {
+        field[this.args.name] = [...field[this.args.name], key];
+      } else if (!isSelected && includesId) {
+        field[this.args.name] = field[this.args.name].filter(
+          (item) => item !== key,
+        );
+      }
+    });
+    if (!selectedRowsKeys.length && this.args.showSelectedOnly) {
+      this.args.toggleShowSelectedOnly();
+    }
+  }
+
+  /**
+   * Refresh current route model data and reset toggle.
+   */
+  async updateDisplayedData() {
+    await this.router.refresh(this.router.currentRouteName);
+    this.args.toggleShowSelectedOnly();
+  }
+}
---
+++ ui/admin/tests/integration/components/scope-options/index-test.js
@@ -0,0 +1,127 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'admin/tests/helpers';
+import { setupIntl } from 'ember-intl/test-support';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | scope-options/index', function (hooks) {
+  setupRenderingTest(hooks);
+  setupIntl(hooks, 'en-us');
+
+  const THIS_TOGGLE = '[name="this"]';
+  const DESCENDANTS_TOGGLE = '[name="descendants"]';
+  const CHILDREN_TOGGLE = '[name="children"]';
+  const SINGLE_ALERT = '.hds-alert--type-compact';
+  const MULTIPLE_ALERTS = '.hds-alert--type-inline';
+
+  let globalScope, orgScope, projectScope, model;
+
+  hooks.beforeEach(function () {
+    globalScope = {
+      isGlobal: true,
+      id: 'global',
+      name: 'Global',
+      displayName: 'Global',
+    };
+    globalScope.scope = globalScope;
+    orgScope = {
+      isOrg: true,
+      id: 'o_123',
+      name: 'org123',
+      displayName: 'org123',
+      scope: globalScope,
+    };
+    projectScope = {
+      id: 'p_123',
+      name: 'proj123',
+      displayName: 'proj123',
+      scope: orgScope,
+    };
+    model = {
+      appToken: { id: 'atp_123', scope: globalScope },
+      scopes: [globalScope, orgScope, projectScope],
+      permission: { grant_scope_id: [] },
+      totalItems: 2,
+      page: 1,
+      pageSize: 10,
+    };
+    const filters = {
+      allFilters: { parentScopes: [globalScope, orgScope] },
+      selectedFilters: { parentScopes: [] },
+    };
+    this.set('filters', filters);
+    this.set('handleSearchInput', () => {});
+    this.set('updateDisplayedScopes', () => {});
+  });
+
+  test('it renders scope options with descendants toggle when in global scope level', async function (assert) {
+    this.set('model', model);
+
+    await render(
+      hbs`<ScopeOptions @name='grant_scope_id', @model={{this.model.appToken}} @scopes={{this.model.scopes}} @field={{this.model.permission}} @filters={{this.filters}} @totalItems={{this.model.totalItems}} @page={{this.model.page}} @pageSize={{this.model.pageSize}} />`,
+    );
+
+    assert.dom(THIS_TOGGLE).isVisible();
+    assert.dom(DESCENDANTS_TOGGLE).isVisible();
+    assert.dom(CHILDREN_TOGGLE).isVisible();
+  });
+
+  test('it renders scope options without descendants toggle when in org scope level', async function (assert) {
+    model.appToken.scope = orgScope;
+    this.set('model', model);
+
+    await render(
+      hbs`<ScopeOptions @name='grant_scope_id', @model={{this.model.appToken}} @scopes={{this.model.scopes}} @field={{this.model.permission}} @filters={{this.filters}} @totalItems={{this.model.totalItems}} @page={{this.model.page}} @pageSize={{this.model.pageSize}} />`,
+    );
+
+    assert.dom(THIS_TOGGLE).isVisible();
+    assert.dom(CHILDREN_TOGGLE).isVisible();
+    assert.dom(DESCENDANTS_TOGGLE).doesNotExist();
+  });
+
+  test('it renders single alert when children is selected in global scope level', async function (assert) {
+    model.permission.grant_scope_id = ['children'];
+    this.set('model', model);
+
+    await render(
+      hbs`<ScopeOptions @name='grant_scope_id', @model={{this.model.appToken}} @scopes={{this.model.scopes}} @field={{this.model.permission}} @filters={{this.filters}} @totalItems={{this.model.totalItems}} @page={{this.model.page}} @pageSize={{this.model.pageSize}} />`,
+    );
+
+    assert.dom(CHILDREN_TOGGLE).isChecked();
+    assert.dom(DESCENDANTS_TOGGLE).isNotChecked();
+    assert.dom(SINGLE_ALERT).isVisible();
+  });
+
+  test('it renders multiple alerts when descendants is selected in global scope level', async function (assert) {
+    model.permission.grant_scope_id = ['descendants'];
+    this.set('model', model);
+
+    await render(
+      hbs`<ScopeOptions @name='grant_scope_id', @model={{this.model.appToken}} @scopes={{this.model.scopes}} @field={{this.model.permission}} @filters={{this.filters}} @totalItems={{this.model.totalItems}} @page={{this.model.page}} @pageSize={{this.model.pageSize}} />`,
+    );
+
+    assert.dom(DESCENDANTS_TOGGLE).isChecked();
+    assert.dom(CHILDREN_TOGGLE).isNotChecked();
+    assert.dom(MULTIPLE_ALERTS).isVisible();
+  });
+
+  test('it renders no alerts when children is selected in org scope level', async function (assert) {
+    model.appToken.scope = orgScope;
+    model.permission.grant_scope_id = ['children'];
+    this.set('model', model);
+
+    await render(
+      hbs`<ScopeOptions @name='grant_scope_id', @model={{this.model.appToken}} @scopes={{this.model.scopes}} @field={{this.model.permission}} @filters={{this.filters}} @totalItems={{this.model.totalItems}} @page={{this.model.page}} @pageSize={{this.model.pageSize}} />`,
+    );
+
+    assert.dom(CHILDREN_TOGGLE).isChecked();
+    assert.dom(DESCENDANTS_TOGGLE).doesNotExist();
+    assert.dom(MULTIPLE_ALERTS).doesNotExist();
+    assert.dom(SINGLE_ALERT).doesNotExist();
+  });
+});
