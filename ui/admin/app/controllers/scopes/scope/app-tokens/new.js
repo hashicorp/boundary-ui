34910a8d2c2771d9689c54e3b6b793a3f25f6be7
Lisbet Alvarez <107949262+lisbet-alvarez@users.noreply.github.com>
2025-12-15 10:45:04 -0800

    feat: 🎸 Create new app token route and button (#3103)
    * feat: 🎸 Create new app token route and button
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-18002
    
    * Add missing copyright headers
    
    * refactor: 💡 PR feedback
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-18002
    
    * refactor: 💡 update w/ initialized object
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-18002
    
    * refactor: 💡 use tracked object w/ selected perm
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-18002
    
    * refactor: 💡 move search to new/index route + increase schema ve
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-18002
    
    * Add missing copyright headers
    
    * refactor: 💡 remove unnintentional change
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-18002
    
    * refactor: 💡 remove scope options logic from route
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-18002

---
+++ ui/admin/app/controllers/scopes/scope/app-tokens/new.js
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import Controller, { inject as controller } from '@ember/controller';
+
+export default class ScopesScopeAppTokensNewController extends Controller {
+  @controller('scopes/scope/app-tokens/index') appTokens;
+}
