e826b716f61e729decbc3f78890a2d12a826ab68
Lisbet Alvarez <107949262+lisbet-alvarez@users.noreply.github.com>
2026-01-13 12:57:20 -0800

    refactor: 💡 Add data-loading to scope options component (#3122)
    * refactor: 💡 Add data-loading to scope options component
    
    * Update ui/admin/app/components/scope-options/index.js
    
    Co-authored-by: Zhihe Li <zhihe.li@hashicorp.com>
    
    * Apply suggestion from @ZedLi
    
    Co-authored-by: Zhihe Li <zhihe.li@hashicorp.com>
    
    * refactor: 💡 fix selector
    
    ---------
    
    Co-authored-by: Zhihe Li <zhihe.li@hashicorp.com>

--- ui/admin/tests/integration/components/scope-options/index-test.js
+++ ui/admin/tests/integration/components/scope-options/index-test.js
@@ -6,11 +6,15 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'admin/tests/helpers';
 import { setupIntl } from 'ember-intl/test-support';
-import { render } from '@ember/test-helpers';
+import { setupSqlite } from 'api/test-support/helpers/sqlite';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | scope-options/index', function (hooks) {
   setupRenderingTest(hooks);
+  setupMirage(hooks);
+  setupSqlite(hooks);
   setupIntl(hooks, 'en-us');
 
   const THIS_TOGGLE = '[name="this"]';
@@ -18,52 +22,61 @@ module('Integration | Component | scope-options/index', function (hooks) {
   const CHILDREN_TOGGLE = '[name="children"]';
   const SINGLE_ALERT = '.hds-alert--type-compact';
   const MULTIPLE_ALERTS = '.hds-alert--type-inline';
+  const ACCORDION_BTN = '.hds-accordion-item__button';
+  const PARENT_SCOPES_FILTER = '[name="parent-scopes"]';
 
-  let globalScope, orgScope, projectScope, model;
+  let globalScope, orgScope, model;
 
-  hooks.beforeEach(function () {
-    globalScope = {
-      isGlobal: true,
-      id: 'global',
-      name: 'Global',
-      displayName: 'Global',
-    };
-    globalScope.scope = globalScope;
-    orgScope = {
-      isOrg: true,
-      id: 'o_123',
-      name: 'org123',
-      displayName: 'org123',
-      scope: globalScope,
-    };
-    projectScope = {
-      id: 'p_123',
-      name: 'proj123',
-      displayName: 'proj123',
-      scope: orgScope,
-    };
-    model = {
-      appToken: { id: 'atp_123', scope: globalScope, scopeModel: globalScope },
-      scopes: [globalScope, orgScope, projectScope],
-      permission: { grant_scope_id: [] },
-      totalItems: 2,
-      page: 1,
-      pageSize: 10,
-    };
-    const filters = {
-      allFilters: { parentScopes: [globalScope, orgScope] },
-      selectedFilters: { parentScopes: [] },
-    };
-    this.set('filters', filters);
-    this.set('handleSearchInput', () => {});
-    this.set('updateDisplayedScopes', () => {});
+  hooks.beforeEach(async function () {
+    const store = this.owner.lookup('service:store');
+    this.server.create(
+      'scope',
+      { id: 'global', type: 'global' },
+      'withChildren',
+    );
+    const orgScopeId = this.server.schema.scopes.where({ type: 'org' })
+      .models[0].id;
+    globalScope = await store.findRecord('scope', 'global');
+    orgScope = await store.findRecord('scope', orgScopeId);
+
+    model = store.createRecord('app-token');
+    model.permissions = [];
+    model.scopeModel = globalScope;
+  });
+
+  test('it renders parent scopes filter in global level', async function (assert) {
+    this.set('model', model);
+    this.set('permission', {});
+
+    await render(
+      hbs`<ScopeOptions @name='grant_scope_id' @model={{this.model}} @field={{this.permission}} />`,
+    );
+
+    await click(ACCORDION_BTN);
+
+    assert.dom(PARENT_SCOPES_FILTER).isVisible();
+  });
+
+  test('it does not render parent scopes filter in org level', async function (assert) {
+    model.scopeModel = orgScope;
+    this.set('model', model);
+    this.set('permission', {});
+
+    await render(
+      hbs`<ScopeOptions @name='grant_scope_id' @model={{this.model}} @field={{this.permission}} />`,
+    );
+
+    await click(ACCORDION_BTN);
+
+    assert.dom(PARENT_SCOPES_FILTER).doesNotExist();
   });
 
   test('it renders scope options with descendants toggle when in global scope level', async function (assert) {
     this.set('model', model);
+    this.set('permission', {});
 
     await render(
-      hbs`<ScopeOptions @name='grant_scope_id', @model={{this.model.appToken}} @scopes={{this.model.scopes}} @field={{this.model.permission}} @filters={{this.filters}} @totalItems={{this.model.totalItems}} @page={{this.model.page}} @pageSize={{this.model.pageSize}} />`,
+      hbs`<ScopeOptions @name='grant_scope_id', @model={{this.model}} @field={{this.permission}} />`,
     );
 
     assert.dom(THIS_TOGGLE).isVisible();
@@ -72,11 +85,12 @@ module('Integration | Component | scope-options/index', function (hooks) {
   });
 
   test('it renders scope options without descendants toggle when in org scope level', async function (assert) {
-    model.appToken.scope = orgScope;
+    model.scopeModel = orgScope;
     this.set('model', model);
+    this.set('permission', {});
 
     await render(
-      hbs`<ScopeOptions @name='grant_scope_id', @model={{this.model.appToken}} @scopes={{this.model.scopes}} @field={{this.model.permission}} @filters={{this.filters}} @totalItems={{this.model.totalItems}} @page={{this.model.page}} @pageSize={{this.model.pageSize}} />`,
+      hbs`<ScopeOptions @name='grant_scope_id', @model={{this.model}} @field={{this.permission}} />`,
     );
 
     assert.dom(THIS_TOGGLE).isVisible();
@@ -85,11 +99,11 @@ module('Integration | Component | scope-options/index', function (hooks) {
   });
 
   test('it renders single alert when children is selected in global scope level', async function (assert) {
-    model.permission.grant_scope_id = ['children'];
     this.set('model', model);
+    this.set('permission', { grant_scope_id: ['children'] });
 
     await render(
-      hbs`<ScopeOptions @name='grant_scope_id', @model={{this.model.appToken}} @scopes={{this.model.scopes}} @field={{this.model.permission}} @filters={{this.filters}} @totalItems={{this.model.totalItems}} @page={{this.model.page}} @pageSize={{this.model.pageSize}} />`,
+      hbs`<ScopeOptions @name='grant_scope_id', @model={{this.model}} @field={{this.permission}} />`,
     );
 
     assert.dom(CHILDREN_TOGGLE).isChecked();
@@ -98,11 +112,11 @@ module('Integration | Component | scope-options/index', function (hooks) {
   });
 
   test('it renders multiple alerts when descendants is selected in global scope level', async function (assert) {
-    model.permission.grant_scope_id = ['descendants'];
     this.set('model', model);
+    this.set('permission', { grant_scope_id: ['descendants'] });
 
     await render(
-      hbs`<ScopeOptions @name='grant_scope_id', @model={{this.model.appToken}} @scopes={{this.model.scopes}} @field={{this.model.permission}} @filters={{this.filters}} @totalItems={{this.model.totalItems}} @page={{this.model.page}} @pageSize={{this.model.pageSize}} />`,
+      hbs`<ScopeOptions @name='grant_scope_id', @model={{this.model}} @field={{this.permission}} />`,
     );
 
     assert.dom(DESCENDANTS_TOGGLE).isChecked();
@@ -111,12 +125,12 @@ module('Integration | Component | scope-options/index', function (hooks) {
   });
 
   test('it renders no alerts when children is selected in org scope level', async function (assert) {
-    model.appToken.scope = orgScope;
-    model.permission.grant_scope_id = ['children'];
+    model.scopeModel = orgScope;
     this.set('model', model);
+    this.set('permission', { grant_scope_id: ['children'] });
 
     await render(
-      hbs`<ScopeOptions @name='grant_scope_id', @model={{this.model.appToken}} @scopes={{this.model.scopes}} @field={{this.model.permission}} @filters={{this.filters}} @totalItems={{this.model.totalItems}} @page={{this.model.page}} @pageSize={{this.model.pageSize}} />`,
+      hbs`<ScopeOptions @name='grant_scope_id' @model={{this.model}} @field={{this.permission}} />`,
     );
 
     assert.dom(CHILDREN_TOGGLE).isChecked();
