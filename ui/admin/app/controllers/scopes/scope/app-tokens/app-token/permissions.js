e4c5f69e9585b579a9d75e7f01794e139efd52eb
lisbet-alvarez <107949262+lisbet-alvarez@users.noreply.github.com>
2026-01-15 19:31:15 +0000

    Add missing copyright headers

--- ui/admin/app/controllers/scopes/scope/app-tokens/app-token/permissions.js
+++ ui/admin/app/controllers/scopes/scope/app-tokens/app-token/permissions.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
 import Controller, { inject as controller } from '@ember/controller';
 
 export default class ScopesScopeAppTokensAppTokenPermissionsController extends Controller {
