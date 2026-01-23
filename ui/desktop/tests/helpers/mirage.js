5e9e1620646f15b204db8e60167fdfcda9be848f
hashicc <191911133+hashicc@users.noreply.github.com>
2026-01-23 18:08:17 +0000

    Add missing copyright headers

--- ui/desktop/tests/helpers/mirage.js
+++ ui/desktop/tests/helpers/mirage.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
 import startMirage from 'desktop/mirage/config';
 import { createSetupMirage } from 'api/test-support/helpers/create-mirage-helper';
 export const setupMirage = createSetupMirage(startMirage);
