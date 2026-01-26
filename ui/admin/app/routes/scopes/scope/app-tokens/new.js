494671c2ce9333bcfc784faff0f53f5961294eb4
Lisbet Alvarez <107949262+lisbet-alvarez@users.noreply.github.com>
2026-01-26 14:51:03 -0800

    feat: 🎸 Pre-fill form for cloning active app-token (#3138)
    * feat: 🎸 Prefill form for cloning active app-token
    
    * test: 💍 fix url in test case
    
    * refactor: 💡 remove logs (PR feedback)
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-18013

--- ui/admin/app/routes/scopes/scope/app-tokens/new.js
+++ ui/admin/app/routes/scopes/scope/app-tokens/new.js
@@ -32,13 +32,32 @@ export default class ScopesScopeAppTokensNewRoute extends Route {
    * Creates new app-token record.
    * @returns {AppTokenModel}
    */
-  model() {
+  async model({ cloneAppToken }) {
     const scopeModel = this.modelFor('scopes.scope');
-
     const record = this.store.createRecord('app-token');
-    record.time_to_live_seconds = 5184000; // Set default TTL
-    record.permissions = new TrackedArray([]);
     record.scopeModel = scopeModel;
+
+    if (cloneAppToken) {
+      const originalAppToken = await this.store.findRecord(
+        'app-token',
+        cloneAppToken,
+      );
+      record.name = `Clone_${originalAppToken.name}`;
+      record.description = originalAppToken.description;
+      record.time_to_live_seconds = originalAppToken.time_to_live_seconds;
+      record.time_to_stale_seconds = originalAppToken.time_to_stale_seconds;
+      record.permissions = new TrackedArray(originalAppToken.permissions);
+    } else {
+      record.time_to_live_seconds = 5184000; // Set default TTL
+      record.permissions = new TrackedArray([]);
+    }
+
     return record;
+  }
+
+  resetController(controller, isExiting) {
+    if (isExiting) {
+      controller.set('cloneAppToken', null);
+    }
   }
 }
