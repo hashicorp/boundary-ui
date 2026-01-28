b28688be20d12d047f600e4021966211b4baa6b1
cameronperera <cameron.perera@hashicorp.com>
2026-01-28 17:09:19 -0600

    fix: 🐛 update check for exitCode

--- ui/desktop/electron-app/src/models/session.js
+++ ui/desktop/electron-app/src/models/session.js
@@ -49,7 +49,7 @@ class Session {
    * @return {boolean}
    */
   get isRunning() {
-    return this.#process && !this.#process.exitCode;
+    return this.#process && !this.#process.exitCode === null;
   }
 
   /**
