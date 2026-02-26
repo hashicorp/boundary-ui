d6e8e58ca1770dce49be693eb2fc74b618f67bc8
Lisbet Alvarez <107949262+lisbet-alvarez@users.noreply.github.com>
2026-02-26 09:28:00 -0800

    feat: 🎸 Add sorting to sessions started column (#3174)
    * feat: 🎸 Add sorting to sessions started column
    
    * test: 💍 add missing a11y ignores for existing color issue
    
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-18520

--- ui/desktop/app/routes/scopes/scope/projects/sessions/index.js
+++ ui/desktop/app/routes/scopes/scope/projects/sessions/index.js
@@ -42,6 +42,14 @@ export default class ScopesScopeProjectsSessionsIndexRoute extends Route {
     pageSize: {
       refreshModel: true,
     },
+    sortAttribute: {
+      refreshModel: true,
+      replace: true,
+    },
+    sortDirection: {
+      refreshModel: true,
+      replace: true,
+    },
   };
 
   allSessions;
@@ -67,7 +75,15 @@ export default class ScopesScopeProjectsSessionsIndexRoute extends Route {
    * all targets and all projects for filtering options for the current user.
    * @return {Promise<{sessions: [SessionModel], projects: [ScopeModel], allSessions: [SessionModel], associatedTargets: [TargetModel], totalItems: number}>}
    */
-  async model({ targets, status, scopes, page, pageSize }) {
+  async model({
+    targets,
+    status,
+    scopes,
+    page,
+    pageSize,
+    sortAttribute,
+    sortDirection,
+  }) {
     const projects = this.modelFor('scopes.scope.projects');
     const orgScope = this.modelFor('scopes.scope');
     // orgFilter used to narrow down resources to only those under
@@ -90,10 +106,11 @@ export default class ScopesScopeProjectsSessionsIndexRoute extends Route {
       filters.scope_id.push({ equals: scope });
     });
 
+    const sort = { attribute: sortAttribute, direction: sortDirection };
     const queryOptions = {
       scope_id: orgScope.id,
       recursive: true,
-      query: { filters },
+      query: { filters, sort },
       page,
       pageSize,
       force_refresh: true,
