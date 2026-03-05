fb208f9fd66af20902148c3969e32deabe00dfee
lisbet-alvarez <107949262+lisbet-alvarez@users.noreply.github.com>
2026-03-05 21:07:52 +0000

    Add missing copyright headers

--- ui/admin/tests/integration/components/grant-string-formats/index-test.js
+++ ui/admin/tests/integration/components/grant-string-formats/index-test.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright IBM Corp. 2021, 2026
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'admin/tests/helpers';
 import { setupIntl } from 'ember-intl/test-support';
