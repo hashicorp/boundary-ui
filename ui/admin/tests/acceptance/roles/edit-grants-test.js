5c7b24362a8b2f940a2c9fcc374bdaf99603202d
cameronperera <cameron.perera@hashicorp.com>
2026-03-18 12:24:01 -0500

    test: 💍 add tests in edit-grants for no grantsSchema data
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-18459

--- ui/admin/tests/acceptance/roles/edit-grants-test.js
+++ ui/admin/tests/acceptance/roles/edit-grants-test.js
@@ -8,9 +8,14 @@ import { visit, click, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'admin/tests/helpers';
 import * as selectors from './selectors';
 import { setRunOptions } from 'ember-a11y-testing/test-support';
+import sinon from 'sinon';
 
 module('Acceptance | roles/edit grants', function (hooks) {
   setupApplicationTest(hooks);
+
+  hooks.afterEach(function () {
+    sinon.restore();
+  });
 
   const instances = {
     scopes: {
@@ -76,4 +81,34 @@ module('Acceptance | roles/edit grants', function (hooks) {
     assert.dom(selectors.MANAGE_DROPDOWN_EDIT_GRANTS).doesNotExist();
     assert.strictEqual(currentURL(), urls.role);
   });
+
+  test('can navigate to edit grants even when the grants schema request fails', async function (assert) {
+    setRunOptions({
+      rules: {
+        'color-contrast': {
+          // [ember-a11y-ignore]: axe rule "color-contrast" automatically ignored on 2026-02-27
+          enabled: false,
+        },
+      },
+    });
+    const grantsSchemaService = this.owner.lookup('service:grants-schema');
+    grantsSchemaService.data = null;
+    grantsSchemaService.isLoaded = false;
+    grantsSchemaService.loadError = new Error('network failure');
+    sinon.stub(grantsSchemaService, 'load').resolves(false);
+
+    await visit(urls.role);
+
+    await click(selectors.MANAGE_DROPDOWN_ROLES);
+    await click(selectors.MANAGE_DROPDOWN_EDIT_GRANTS);
+
+    assert.strictEqual(currentURL(), urls.editGrants);
+    assert
+      .dom('.grant-actions-card')
+      .includesText('Grant actions information not available.');
+  });
+
+  // TODO: add tests for grant actions card behavior once the edit grants
+  // code editor is implemented, to verify that the grant actions
+  // are displayed correctly
 });
