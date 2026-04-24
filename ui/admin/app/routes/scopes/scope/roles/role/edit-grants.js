ee00eab24197f75f40affe1029019b8acc2baca9
Zhihe Li <zhihe.li@hashicorp.com>
2026-04-24 17:31:17 -0400

    feat: 🎸 Add id auto completion
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-18506

--- ui/admin/app/routes/scopes/scope/roles/role/edit-grants.js
+++ ui/admin/app/routes/scopes/scope/roles/role/edit-grants.js
@@ -5,6 +5,16 @@
 
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import { dropTask } from 'ember-concurrency';
+import { modelMapping } from 'api/services/sqlite';
+import { notifyError } from 'core/decorators/notify';
+
+// Only resource types that have a name column are useful for ID suggestions
+// Currently this excludes session-recording and sessions as they aren't likely to be used
+// and could be expensive to load.
+export const NAMED_RESOURCE_TYPES = Object.keys(modelMapping).filter(
+  (type) => 'name' in modelMapping[type],
+);
 
 export default class ScopesScopeRolesRoleEditGrantsRoute extends Route {
   @service store;
@@ -13,15 +23,32 @@ export default class ScopesScopeRolesRoleEditGrantsRoute extends Route {
     const role = this.modelFor('scopes.scope.roles.role');
     let grantsSchema = { resource_types: [] };
 
-    try {
-      grantsSchema = await this.fetchGrantsSchema();
-    } catch {
-      // TODO: Return an error message
-    }
+    grantsSchema = await this.fetchGrantsSchema();
 
-    return { role, grantsSchema };
+    // Kick off background loading of all supported resource types into DB without awaiting.
+    this.loadResourcesTask.perform(role.scopeID);
+
+    return {
+      role,
+      grantsSchema,
+      loadResourcesTask: this.loadResourcesTask,
+      searchableResourceTypes: NAMED_RESOURCE_TYPES,
+    };
   }
 
+  /**
+   * Loads all supported resource types recursively so they are available
+   * for ID autocompletion suggestions.
+   */
+  loadResourcesTask = dropTask(async (scopeId) => {
+    await Promise.allSettled(
+      NAMED_RESOURCE_TYPES.map((type) =>
+        this.store.query(type, { scope_id: scopeId, recursive: true }),
+      ),
+    );
+  });
+
+  @notifyError(({ message }) => message, { catch: true })
   async fetchGrantsSchema() {
     const adapter = this.store.adapterFor('application');
     const url = `${adapter.host ?? ''}/grants-schema.json`;
