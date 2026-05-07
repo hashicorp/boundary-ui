f4bc3a45482a3b9f07203beb95a45f028ddd1e2f
Sohail <sohail.durrani@hashicorp.com>
2026-05-07 17:12:42 +0530

    Implemented gating logic for aliases based on alias suffix

--- ui/admin/app/controllers/scopes/scope/aliases/index.js
+++ ui/admin/app/controllers/scopes/scope/aliases/index.js
@@ -59,6 +59,15 @@ export default class ScopesScopeAliasesIndexController extends Controller {
     return this.intl.t(description, { resource });
   }
 
+  /**
+   * Project aliases are gated behind the project scope having an alias
+   * suffix configured.
+   * @type {boolean}
+   */
+  get isGated() {
+    return this.scope?.isProject && !this.scope?.hasSuffix;
+  }
+
   // =actions
 
   /**
