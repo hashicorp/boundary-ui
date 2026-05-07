928594d7784d83df0ef6e6c1734508a3de8cfcd2
Lisbet Alvarez <lisbet.alvarez@hashicorp.com>
2026-05-07 11:34:53 -0700

    refactor: 💡 grants tab to use code block
    ✅ Closes: https://hashicorp.atlassian.net/browse/ICU-18757

--- ui/admin/app/components/form/role/grants/index.js
+++ ui/admin/app/components/form/role/grants/index.js
@@ -4,16 +4,9 @@
  */
 
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
-import { computed, action } from '@ember/object';
 
 export default class FormRoleGrantsComponent extends Component {
   // =attributes
-
-  /**
-   * @type {string}
-   */
-  @tracked newGrantString = '';
 
   /**
    * Returns grants currently on model, in addition to
@@ -21,41 +14,7 @@ export default class FormRoleGrantsComponent extends Component {
    * before form submission
    * @return {[string]}
    */
-  @computed('args.model.grant_strings.[]')
   get grants() {
-    return this.args.model.grant_strings.map((value) => ({ value }));
-  }
-
-  /**
-   * Returns grants after form submission
-   * @return {[object]}
-   */
-  @computed('grants.@each.value')
-  get grantStrings() {
-    return this.grants.map((obj) => obj.value);
-  }
-
-  /**
-   * True if the grant string field is empty, false otherwise.  This is used
-   * to disable the submit button.
-   * @return {boolean}
-   */
-  @computed('newGrantString')
-  get cannotSave() {
-    return !this.newGrantString;
-  }
-
-  // =actions
-
-  /**
-   * Calls the passed function with the grant string as an argument and then
-   * clears the value of the grant string field.
-   * `@addGrant` should be passed by the context calling this component.
-   * @param {Function} addGrantFn
-   */
-  @action
-  createGrant(addGrantFn) {
-    addGrantFn(this.newGrantString);
-    this.newGrantString = '';
+    return this.args.model.grant_strings.join('\n');
   }
 }
