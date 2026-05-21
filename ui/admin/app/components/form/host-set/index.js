59b481b542d05645d22d315534ad0f97787d5f87
Dharini <dharini@hashicorp.com>
2026-05-21 16:51:16 -0700

    refactor: 💡 pr feedback

--- ui/admin/app/components/form/host-set/index.js
+++ ui/admin/app/components/form/host-set/index.js
@@ -5,7 +5,6 @@
 
 import Component from '@glimmer/component';
 import { assert } from '@ember/debug';
-import { action } from '@ember/object';
 import awsFormComponent from './aws';
 import azureFormComponent from './azure';
 import gcpFormComponent from './gcp';
@@ -30,50 +29,5 @@ export default class FormHostSetIndex extends Component {
       component,
     );
     return component;
-  }
-
-  /**
-   * Adds a new empty row to the specified field
-   * @param {string} field - The field name to add a row to
-   * @param {Array<string>} properties - Array of property names for the empty row (e.g., ['value'] or ['key', 'value'])
-   */
-  @action
-  addRow(field, properties) {
-    const emptyRow = Object.fromEntries(properties.map((prop) => [prop, '']));
-    this.args.model[field] = [...(this.args.model[field] ?? []), emptyRow];
-  }
-
-  /**
-   * Removes a row from the specified field
-   * @param {string} field - The field name to remove a row from
-   * @param {object} rowData - The row data to remove
-   */
-  @action
-  removeRow(field, rowData) {
-    let rows = (this.args.model[field] ?? []).filter(
-      (item) => item !== rowData,
-    );
-
-    if (rows.length === 0) {
-      const properties = Object.keys(rowData).length
-        ? Object.keys(rowData)
-        : ['value'];
-      rows = [Object.fromEntries(properties.map((prop) => [prop, '']))];
-    }
-
-    this.args.model[field] = rows;
-  }
-
-  /**
-   * Updates a specific property in a row
-   * @param {string} field - The field name containing the row
-   * @param {object} rowData - The row object to update
-   * @param {string} property - The property name to update
-   * @param {object} event - The DOM event containing the new value
-   */
-  @action
-  updateRow(field, rowData, property, { target: { value } }) {
-    rowData[property] = value;
-    this.args.model[field] = [...(this.args.model[field] ?? [])];
   }
 }
